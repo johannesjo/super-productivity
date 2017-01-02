72ac54092c2793de134a78d2171362281fc00395
Johannes Millan <johannes.millan@gmail.com>
2017-01-02 23:28:36 +0100

    add download backup directive

---
+++ app/scripts/download-backup/download-backup-d.js
@@ -0,0 +1,39 @@
+/**
+ * @ngdoc directive
+ * @name superProductivity.directive:downloadBackup
+ * @description
+ * # downloadBackup
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('superProductivity')
+    .directive('downloadBackup', downloadBackup);
+
+  /* @ngInject */
+  function downloadBackup($localStorage) {
+    return {
+      bindToController: true,
+      controllerAs: 'vm',
+      link: linkFn,
+      restrict: 'A'
+    };
+
+    function linkFn(scope, element) {
+      element.on('click', () => {
+        let storageObj = {
+          currentTask: $localStorage.currentTask,
+          backlogTasks: $localStorage.backlogTasks,
+          tasks: $localStorage.tasks
+        };
+
+        let dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(storageObj));
+
+        element[0].setAttribute('href', dataStr);
+        element[0].setAttribute('download', 'super-productivity-backup.json');
+      });
+    }
+  }
+})();
