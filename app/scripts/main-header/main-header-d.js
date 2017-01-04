c9ec2ce75f9575fbbc3acde4c5097a5a1f79f774
Johannes Millan <johannes.millan@gmail.com>
2017-01-04 17:54:56 +0100

    add main header as directive

---
+++ app/scripts/main-header/main-header-d.js
@@ -0,0 +1,32 @@
+/**
+ * @ngdoc directive
+ * @name superProductivity.directive:mainHeader
+ * @description
+ * # mainHeader
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('superProductivity')
+    .directive('mainHeader', mainHeader);
+
+  /* @ngInject */
+  function mainHeader() {
+    return {
+      templateUrl: 'scripts/main-header/main-header-d.html',
+      bindToController: true,
+      controller: MainHeaderCtrl,
+      controllerAs: 'vm',
+      restrict: 'E',
+      scope: {}
+    };
+  }
+
+  /* @ngInject */
+  function MainHeaderCtrl() {
+    let vm = this;
+  }
+
+})();
