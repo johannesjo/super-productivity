f89a30eb743c607c9e102c1359b451e60ee6a8d1
Johannes Millan <johannes.millan@gmail.com>
2017-01-19 22:10:41 +0100

    refactor constants into their own file

--- app/scripts/_app.js
+++ app/scripts/_app.js
@@ -34,65 +34,6 @@
       'hc.marked',
       'mwl.calendar'
     ])
-    .constant('DEFAULT_THEME', 'teal-theme')
-    .constant('LS_DEFAULTS', {
-      theme: undefined,
-      currentTask: undefined,
-      currentProject: undefined,
-      currentSession: {
-        timeWorkedWithoutBreak: undefined
-      },
-      tasks: [],
-      backlogTasks: [],
-      distractions: [],
-      projects: [],
-      git: {
-        projectDir: undefined
-      },
-      config: {
-        isTakeABreakEnabled: false,
-        takeABreakMinWorkingTime: undefined,
-        isShortSyntaxEnabled: true
-      },
-      jiraSettings: {
-        isFirstLogin: true,
-        isWorklogEnabled: true,
-        isAutoWorklog: false,
-        isUpdateIssueFromLocal: false,
-        isAddWorklogOnSubTaskDone: true,
-        defaultTransitionInProgress: undefined,
-        defaultTransitionDone: undefined,
-        transitions: {
-          OPEN: undefined,
-          IN_PROGRESS: undefined,
-          DONE: undefined
-        }
-      }
-    })
-    .constant('JIRA_UPDATE_POLL_INTERVAL', 60 * 1000 * 5)
-    .constant('IS_ELECTRON', (typeof window.ipcRenderer !== 'undefined'))
-    .constant('THEMES', [
-        'red',
-        'pink',
-        'purple',
-        'deep-purple',
-        'indigo',
-        'blue',
-        'light-blue',
-        'cyan',
-        'teal',
-        'green',
-        'light-green',
-        'lime',
-        'yellow',
-        'amber',
-        'orange',
-        'deep-orange',
-        'brown',
-        'grey',
-        'blue-grey'
-      ]
-    )
     .config(configMdTheme)
     .config(configMarked)
     .run(initGlobalModels)
