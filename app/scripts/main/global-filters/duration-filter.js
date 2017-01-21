d945838ace3fbff72f5db405e257309af9617423
Johannes Millan <johannes.millan@gmail.com>
2017-01-21 17:25:50 +0100

    speed up duration filter

--- app/scripts/main/global-filters/duration-filter.js
+++ app/scripts/main/global-filters/duration-filter.js
@@ -17,15 +17,21 @@
   function duration($window) {
     return function (input, longWords) {
       let output = '';
-      input = angular.copy($window.moment.duration(input));
+      let dayHours = 0;
+
+      // this would be correct but simply checking for an object is faster
+      //if (!$window.moment.isDuration(input)) {
+      if (!angular.isObject(input)) {
+        input = $window.moment.duration(input);
+      }
 
       if (!longWords) {
         if (input._data.days) {
-          //output = input._data.days + 'd ';
-          input._data.hours = input._data.hours + (input._data.days * 24);
+          //output = input._data.days + ' d ';
+          dayHours = input._data.hours + (input._data.days * 24);
         }
         if (input._data.hours) {
-          output += input._data.hours + 'h ';
+          output += input._data.hours + dayHours + 'h ';
         }
         if (input._data.minutes) {
           output += input._data.minutes + 'm ';
@@ -36,10 +42,10 @@
       } else {
         if (input._data.days) {
           //output = input._data.days + ' days ';
-          input._data.hours = input._data.hours + (input._data.days * 24);
+          dayHours = input._data.hours + (input._data.days * 24);
         }
         if (input._data.hours) {
-          output += input._data.hours + ' hours ';
+          output += input._data.hours + dayHours + ' hours ';
         }
         if (input._data.minutes) {
           output += input._data.minutes + ' minutes ';
