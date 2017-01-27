9d9f7ceda58cb80aabeab225c672535fa4c7c67f
Johannes Millan <johannes.millan@gmail.com>
2017-01-27 03:19:13 +0100

    queue dialogs instead of closing the previous one on default

--- app/scripts/dialogs/dialogs-s.js
+++ app/scripts/dialogs/dialogs-s.js
@@ -14,30 +14,62 @@
     .service('Dialogs', Dialogs);
 
   /* @ngInject */
-  function Dialogs($mdDialog, DIALOGS) {
-    //function doAsyncSeries(arr) {
-    //  return arr.reduce(function (promise, item) {
-    //    return promise.then(function () {
-    //      return Jira.updateStatus(item.val, item.type);
-    //    });
-    //  }, $q.when('NOT_YET'));
-    //}
+  function Dialogs($mdDialog, DIALOGS, $q) {
+    const dialogQueue = [];
 
-    return function (dialogName, locals) {
-
-
-
+    function createDialogObject(dialogName, locals) {
       // copy and extend defaults for dialog
-      let defaults = angular.copy(DIALOGS.DEFAULTS);
+      const defaults = angular.copy(DIALOGS.DEFAULTS);
       let dialog = angular.extend(defaults, DIALOGS[dialogName]);
 
       // add passed variables
       dialog = angular.extend(dialog, {
         locals: locals
       });
+      return dialog;
+    }
 
-      // return dialog instance for further handling
-      return $mdDialog.show(dialog);
+    function addToQueue(dialogObj, wrapperPromise) {
+      dialogQueue.push({
+        obj: dialogObj,
+        wrapperPromise
+      });
+    }
+
+    function removeLastResolvedFromQueue() {
+      dialogQueue.shift();
+    }
+
+    function openNextInQueue() {
+      let nextDialog = dialogQueue[0];
+
+      if (nextDialog) {
+        $mdDialog.show(nextDialog.obj)
+          .then((res) => {
+            nextDialog.wrapperPromise.resolve(res);
+            removeLastResolvedFromQueue();
+            openNextInQueue();
+          }, (err) => {
+            nextDialog.wrapperPromise.reject(err);
+            removeLastResolvedFromQueue();
+            openNextInQueue();
+          });
+      }
+    }
+
+    // actual method to call
+    return function (dialogName, locals) {
+      let wrapperPromise = $q.defer();
+      const dialogObj = createDialogObject(dialogName, locals);
+      addToQueue(dialogObj, wrapperPromise);
+
+      // when there are currently no other open dialogs
+      if (dialogQueue.length === 1) {
+        // start dialog
+        openNextInQueue();
+      }
+
+      return wrapperPromise.promise;
     };
   }
 
