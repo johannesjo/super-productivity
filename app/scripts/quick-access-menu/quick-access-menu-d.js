8d0d495d33b2ffa73799841c99aa2e0dfa4507ff
Johannes Millan <johannes.millan@gmail.com>
2017-01-28 03:26:10 +0100

    add basic quick access menu

---
+++ app/scripts/quick-access-menu/quick-access-menu-d.js
@@ -0,0 +1,33 @@
+/**
+ * @ngdoc directive
+ * @name superProductivity.directive:quickAccessMenu
+ * @description
+ * # quickAccessMenu
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('superProductivity')
+    .directive('quickAccessMenu', quickAccessMenu);
+
+  /* @ngInject */
+  function quickAccessMenu() {
+    return {
+      templateUrl: 'scripts/quick-access-menu/quick-access-menu-d.html',
+      bindToController: true,
+      controller: QuickAccessMenuCtrl,
+      controllerAs: 'vm',
+      restrict: 'E',
+      scope: true
+    };
+  }
+
+  /* @ngInject */
+  function QuickAccessMenuCtrl() {
+    this.isOpen = false;
+    this.selectedMode = 'md-fling';
+  }
+
+})();
