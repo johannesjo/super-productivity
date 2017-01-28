9f1b108dbf8b5a66bbf5c9644c23e4db19c0564e
Johannes Millan <johannes.millan@gmail.com>
2017-01-28 01:05:53 +0100

    move global model initialization into its own service

--- app/scripts/_app.js
+++ app/scripts/_app.js
@@ -77,44 +77,8 @@
     $mdThemingProvider.alwaysWatchTheme(true);
   }
 
-  function initGlobalModels(LS_DEFAULTS, DEFAULT_THEME, $rootScope, Tasks, $localStorage, Projects, $state) {
-    $localStorage.$default(LS_DEFAULTS);
-
-    // we don't use r.$state because it looks more like something special
-    // this way
-    $rootScope.$state = $state;
-
-    $rootScope.r = {};
-
-    $rootScope.r.projects = Projects.getList();
-
-    $rootScope.r.tasks = Tasks.getToday();
-    $rootScope.r.backlogTasks = Tasks.getBacklog();
-    $rootScope.r.currentTask = Tasks.getCurrent();
-    $rootScope.r.doneBacklogTasks = Tasks.getDoneBacklog();
-
-    $rootScope.r.currentSession = $localStorage.currentSession;
-    // reset after for every start
-    $rootScope.r.currentSession.timeWorkedWithoutBreak = undefined;
-
-    $rootScope.r.distractions = $localStorage.distractions;
-
-    $rootScope.r.jiraSettings = $localStorage.jiraSettings;
-
-    $rootScope.r.tomorrowsNote = $localStorage.tomorrowsNote;
-
-    $rootScope.r.git = $localStorage.git;
-
-    $rootScope.r.theme = $localStorage.theme || DEFAULT_THEME;
-
-    if ($rootScope.r.theme.indexOf('dark') > -1) {
-      $rootScope.r.bodyClass = 'dark-theme';
-    } else {
-      $rootScope.r.bodyClass = '';
-    }
-
-    // needs to be last for the updates from current data via the watcher!!!
-    $rootScope.r.currentProject = Projects.getAndUpdateCurrent();
+  function initGlobalModels(InitGlobalModels) {
+    InitGlobalModels();
   }
 
   function initGlobalShortcuts($document, Dialogs) {
