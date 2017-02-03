f41bcf254dd8dbd850d6978e93d3d6bd61abbf04
Johannes Millan <johannes.millan@gmail.com>
2017-02-03 01:58:40 +0100

    add more options for simple task summary

--- app/scripts/dialogs/simple-task-summary/simple-task-summary-c.js
+++ app/scripts/dialogs/simple-task-summary/simple-task-summary-c.js
@@ -14,7 +14,7 @@
     .controller('SimpleTaskSummaryCtrl', SimpleTaskSummaryCtrl);
 
   /* @ngInject */
-  function SimpleTaskSummaryCtrl($mdDialog, tasks, settings, TasksUtil, $scope) {
+  function SimpleTaskSummaryCtrl($mdDialog, tasks, settings, TasksUtil, $scope, ParseDuration) {
     let vm = this;
 
     vm.options = settings;
@@ -26,7 +26,31 @@
       vm.options.separateFieldsBy = '';
     }
 
-    function formatLine() {
+    function formatTask(task) {
+      let taskTxt = '';
+      if (vm.options.showDate && task.dateStr) {
+        taskTxt += task.dateStr;
+      }
+      if (vm.options.showTitle) {
+        if (taskTxt.length > 0) {
+          taskTxt += vm.options.separateFieldsBy;
+        }
+        taskTxt += task.title;
+      }
+      if (vm.options.showTimeSpent) {
+        if (taskTxt.length > 0) {
+          taskTxt += vm.options.separateFieldsBy;
+        }
+
+        if (vm.options.isTimeSpentAsMilliseconds) {
+          taskTxt += task.timeSpent.asMilliseconds();
+        } else {
+          taskTxt += ParseDuration.toString(task.timeSpent);
+        }
+      }
+
+      taskTxt += vm.options.separateBy;
+      return taskTxt;
     }
 
     function createTasksText(tasks) {
@@ -37,7 +61,7 @@
         for (let i = 0; i < tasks.length; i++) {
           let task = tasks[i];
           if ((!vm.options.isListDoneOnly || task.isDone) && (!vm.options.isWorkedOnTodayOnly || TasksUtil.isWorkedOnToday(task))) {
-            tasksTxt += task.title + vm.options.separateBy;
+            tasksTxt += formatTask(task);
             if (vm.options.isUseNewLine) {
               tasksTxt += newLineSeparator;
             }
@@ -47,7 +71,7 @@
             for (let j = 0; j < task.subTasks.length; j++) {
               let subTask = task.subTasks[j];
               if ((!vm.options.isListDoneOnly || subTask.isDone) && (!vm.options.isWorkedOnTodayOnly || TasksUtil.isWorkedOnToday(subTask))) {
-                tasksTxt += subTask.title + vm.options.separateBy;
+                tasksTxt += formatTask(task);
                 if (vm.options.isUseNewLine) {
                   tasksTxt += newLineSeparator;
                 }
