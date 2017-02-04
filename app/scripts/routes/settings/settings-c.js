269289143b799488a68f0caa2bc0a3299804b94f
Johannes Millan <johannes.millan@gmail.com>
2017-02-04 02:57:29 +0100

    refactor theme selection to its own directive

--- app/scripts/routes/settings/settings-c.js
+++ app/scripts/routes/settings/settings-c.js
@@ -14,7 +14,7 @@
     .controller('SettingsCtrl', SettingsCtrl);
 
   /* @ngInject */
-  function SettingsCtrl($localStorage, $window, $scope, Projects, DEFAULT_THEME, THEMES, IS_ELECTRON) {
+  function SettingsCtrl($localStorage, $window, $scope, Projects, IS_ELECTRON) {
     let vm = this;
     const _ = $window._;
     vm.IS_ELECTRON = IS_ELECTRON;
@@ -22,16 +22,7 @@
     function init() {
       vm.allProjects = Projects.getList();
       vm.selectedCurrentProject = $localStorage.currentProject;
-
-      $localStorage.theme = $localStorage.theme || DEFAULT_THEME;
-      vm.isDarkTheme = $localStorage.theme && $localStorage.theme.indexOf('dark') > -1;
-      vm.selectedTheme = $localStorage.theme && $localStorage.theme
-          .replace('-theme', '')
-          .replace('-dark', '');
     }
-
-    // theme stuff
-    vm.themes = THEMES;
 
     // WATCHER & EVENTS
     // ----------------
@@ -43,31 +34,6 @@
       init();
     }, true));
 
-    watchers.push($scope.$watch('vm.selectedTheme', function (value) {
-      if (value) {
-        if (vm.isDarkTheme) {
-          $localStorage.theme = value + '-dark';
-        } else {
-          $localStorage.theme = value + '-theme';
-        }
-      }
-    }));
-
-    watchers.push($scope.$watch('vm.isDarkTheme', function (value) {
-      if (value) {
-        $localStorage.theme = $localStorage.theme.replace('-theme', '-dark');
-        $localStorage.bodyClass = 'dark-theme';
-      } else {
-        $localStorage.theme = $localStorage.theme.replace('-dark', '-theme');
-        $localStorage.bodyClass = '';
-      }
-      // for some reason project needs to be updated directly
-      if ($localStorage.currentProject && $localStorage.currentProject.data) {
-        $localStorage.currentProject.data.theme = $localStorage.theme;
-      }
-    }));
-
-    // otherwise we update on view change
     $scope.$on('$destroy', () => {
       // remove watchers manually
       _.each(watchers, (watcher) => {
---
+++ app/scripts/settings/theme-settings/theme-settings-d.js
@@ -0,0 +1,87 @@
+/**
+ * @ngdoc directive
+ * @name superProductivity.directive:themeSettings
+ * @description
+ * # themeSettings
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('superProductivity')
+    .directive('themeSettings', themeSettings);
+
+  /* @ngInject */
+  function themeSettings() {
+    return {
+      templateUrl: 'scripts/settings/theme-settings/theme-settings-d.html',
+      bindToController: true,
+      controller: ThemeSettingsCtrl,
+      controllerAs: 'vm',
+      restrict: 'E',
+      scope: {
+        currentTheme: '=',
+        isCurrentProjectTheme: '@'
+      }
+    };
+  }
+
+  /* @ngInject */
+  function ThemeSettingsCtrl($scope, THEMES, DEFAULT_THEME, $localStorage) {
+    let vm = this;
+    vm.themes = THEMES;
+    console.log(vm.currentTheme);
+
+    vm.currentTheme = vm.currentTheme || DEFAULT_THEME;
+
+    vm.isDarkTheme = vm.currentTheme && vm.currentTheme.indexOf('dark') > -1;
+    vm.selectedTheme = vm.currentTheme && vm.currentTheme
+        .replace('-theme', '')
+        .replace('-dark', '');
+
+    // WATCHER & EVENTS
+    // ----------------
+    const watchers = [];
+
+    watchers.push($scope.$watch('vm.selectedTheme', function (value) {
+      if (value) {
+        if (vm.isDarkTheme) {
+          vm.currentTheme = value + '-dark';
+        } else {
+          vm.currentTheme = value + '-theme';
+        }
+      }
+    }));
+
+    watchers.push($scope.$watch('vm.isDarkTheme', function (value) {
+      if (vm.currentTheme) {
+        if (value) {
+          vm.currentTheme = vm.currentTheme.replace('-theme', '-dark');
+          if (vm.isCurrentProjectTheme) {
+            $localStorage.bodyClass = 'dark-theme';
+          }
+        } else {
+          vm.currentTheme = vm.currentTheme.replace('-dark', '-theme');
+          if (vm.isCurrentProjectTheme) {
+            $localStorage.bodyClass = '';
+          }
+        }
+        // for some reason project needs to be updated directly
+        if (vm.isCurrentProjectTheme) {
+          if (vm.currentTheme.currentProject && vm.currentTheme.currentProject.data) {
+            vm.currentTheme.currentProject.data.theme = vm.currentTheme;
+          }
+        }
+      }
+    }));
+
+    $scope.$on('$destroy', () => {
+      // remove watchers manually
+      _.each(watchers, (watcher) => {
+        watcher();
+      });
+    });
+  }
+
+})();
