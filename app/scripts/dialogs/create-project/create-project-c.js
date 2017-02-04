88c2c699f309da7f0db5261025b792d06af00caf
Johannes Millan <johannes.millan@gmail.com>
2017-02-04 03:05:39 +0100

    add theme selection create project dialog

--- app/scripts/dialogs/create-project/create-project-c.js
+++ app/scripts/dialogs/create-project/create-project-c.js
@@ -14,11 +14,12 @@
     .controller('CreateProjectCtrl', CreateProjectCtrl);
 
   /* @ngInject */
-  function CreateProjectCtrl($mdDialog, Projects, SimpleToast, IS_ELECTRON) {
+  function CreateProjectCtrl($mdDialog, Projects, SimpleToast, IS_ELECTRON, $localStorage) {
     let vm = this;
     vm.IS_ELECTRON = IS_ELECTRON;
     vm.task = {};
     vm.projectSettings = {};
+    vm.projectSettings.theme = $localStorage.theme;
 
     vm.createProject = (project) => {
       Projects.createNew(project.title, vm.projectSettings);
