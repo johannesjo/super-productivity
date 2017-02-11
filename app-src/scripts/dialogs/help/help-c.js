7ade65585669a341e2e853611337bcb3d0395545
Johannes Millan <johannes.millan@gmail.com>
2017-02-11 16:36:11 +0100

    improve help doc for daily planner

--- app-src/scripts/dialogs/help/help-c.js
+++ app-src/scripts/dialogs/help/help-c.js
@@ -14,11 +14,12 @@
     .controller('HelpCtrl', HelpCtrl);
 
   /* @ngInject */
-  function HelpCtrl($mdDialog, theme, $state, IS_ELECTRON, template) {
+  function HelpCtrl($mdDialog, theme, $state, IS_ELECTRON, template, $localStorage) {
     let vm = this;
     vm.task = {};
     vm.theme = theme;
     vm.IS_ELECTRON = IS_ELECTRON;
+    vm.keys = $localStorage.keys;
 
     if (template === 'PAGE') {
       vm.helpTpl = 'scripts/dialogs/help/help-' + $state.current.name + '.html';
@@ -29,5 +30,47 @@
     vm.cancel = () => {
       $mdDialog.hide();
     };
+
+    vm.exampleTask = {
+      title: 'Example task',
+      timeSpent: moment.duration(1, 'hour'),
+      timeEstimate: moment.duration(2, 'hour'),
+      progress: 50,
+      showNotes: true,
+      isUpdated: true,
+      originalComments: [
+        '[admin]: yes comments from Jira are shown too <3',
+        '[user]: really? that could be useful!'
+      ],
+      originalChangelog: [
+        {
+          author: 'Some Author',
+          created: moment(),
+          items: [
+            {
+              field: 'jiraField',
+              toString: 'Changed to value'
+            }
+          ]
+        }
+      ],
+      originalAttachment: [
+        'Attachment loaded from jira'
+      ],
+      status: 'IN_PROGRESS',
+      originalStatus: {
+        name: 'In Progress'
+      },
+      notes: `**Some example notes**
+      
+      * supports markdown
+      * list item
+      `
+    };
+    vm.exampleTasks = [
+      {
+        title: 'Example task'
+      }
+    ]
   }
 })();
