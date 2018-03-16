caca38a9a0783666c22e65797b82db964cd8f07f
Johannes Millan <johannes.millan@gmail.com>
2018-03-16 15:55:40 +0100

    feat: improve slide animation

---
+++ app-src/scripts/main/global-animations/slide-down.ani.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('superProductivity')
+    .animation('.ani-slide-up-down', slideAnimation);
+
+  /* @ngInject */
+  function slideAnimation() {
+    // placeholder for later
+
+    return {
+      enter: function($el, doneFn) {
+        const el = $el[0];
+        const ch = el.clientHeight;
+        const sh = el.scrollHeight;
+        const noHeightSet = !el.style.height;
+        const isCollapsed = true;
+
+        el.style.maxHeight = (isCollapsed || noHeightSet ? sh : 0) + 'px';
+
+        console.log(el);
+        //$(el).slideIn(1000, doneFn);
+        //return $animateCss(el, {
+        //  event: 'enter',
+        //  structural: true,
+        //  addClass: 'maroon-setting',
+        //  from: { height: 0 },
+        //  to: { height: 200 }
+        //});
+
+        doneFn();
+      }
+    }
+  }
+}());
+
+
