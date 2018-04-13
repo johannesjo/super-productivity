b71a0fd3e8a0aee5c0771ea8d4a9e352b46c5139
Johannes Millan <johannes.millan@gmail.com>
2018-04-13 18:30:22 +0200

    feat(enlargeImage): make it appear as original image is being enlarged

--- app-src/scripts/enlarge-image/enlarge-image-d.js
+++ app-src/scripts/enlarge-image/enlarge-image-d.js
@@ -8,10 +8,28 @@
 (() => {
   'use strict';
 
+  function getCoords(elem) { // crossbrowser version
+    const box = elem.getBoundingClientRect();
+
+    const body = document.body;
+    const docEl = document.documentElement;
+
+    const scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    const scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+    const clientTop = docEl.clientTop || body.clientTop || 0;
+    const clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    const top = box.top + scrollTop - clientTop;
+    const left = box.left + scrollLeft - clientLeft;
+
+    return { top: Math.round(top), left: Math.round(left) };
+  }
+
   class EnlargeImageCtrl {
     /* @ngInject */
     constructor($element) {
-      this.lightboxEl = angular.element(document.body);
+      this.lightboxParentEl = angular.element(document.body);
       this.$el = $element;
       $element.bind('click', () => {
         this.showImg();
@@ -19,18 +37,24 @@
     }
 
     hideImg() {
+      this.$el[0].setAttribute('style', `visibility: visible`);
       this.$enlargedImgWrapperEl.remove();
     }
 
     showImg() {
-      const enlargedImgWrapperEl = this.htmlToElement(`<div class="enlarged-image-wrapper"><img src="${this.enlargeImage}" class="enlarged-image"></div>`);
+      const enlargedImgWrapperEl = this.htmlToElement(`<div class="enlarged-image-wrapper"></div>`);
+      const newImageEl = this.htmlToElement(`<img src="${this.enlargeImage}" class="enlarged-image">`);
+      enlargedImgWrapperEl.append(newImageEl);
+      const origImgCoords = getCoords(this.$el[0]);
       this.$enlargedImgWrapperEl = angular.element(enlargedImgWrapperEl);
-
-      this.lightboxEl.append(this.$enlargedImgWrapperEl);
-
+      this.lightboxParentEl.append(this.$enlargedImgWrapperEl);
+      const newImageCoords = getCoords(newImageEl);
+      newImageEl.setAttribute('style', `transform: translate3d(${origImgCoords.left - newImageCoords.left - this.$el[0].width}px, ${origImgCoords.top - newImageCoords.top - this.$el[0].height}px, 0) scale(0.3)`);
       this.$enlargedImgWrapperEl.bind('click', () => {
         this.hideImg();
       });
+      this.$el[0].setAttribute('style', `visibility: hidden`);
+
     }
 
     htmlToElement(html) {
