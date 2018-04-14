34c11b446e76c0a9e88b5a4e559120013ea0b675
Johannes Millan <johannes.millan@gmail.com>
2018-04-14 14:55:31 +0200

    feat(enlargeImage): also add zoom out ani

--- app-src/scripts/enlarge-image/enlarge-image-d.js
+++ app-src/scripts/enlarge-image/enlarge-image-d.js
@@ -31,29 +31,46 @@
     constructor($element) {
       this.lightboxParentEl = angular.element(document.body);
       this.$el = $element;
+      this.imageEl = this.$el[0];
       $element.bind('click', () => {
         this.showImg();
       });
     }
 
     hideImg() {
-      this.$el[0].setAttribute('style', `visibility: visible`);
-      this.$enlargedImgWrapperEl.remove();
+
+      this.$enlargedImgWrapperEl.removeClass('ani-enter');
+      this.$enlargedImgWrapperEl.addClass('ani-remove');
+      this.setCoordsForImageAni();
+      this.imageEl.setAttribute('style', `visibility: visible`);
+
+      this.$enlargedImgWrapperEl.on('transitionend', () => {
+        this.$enlargedImgWrapperEl.remove();
+      });
+    }
+
+    setCoordsForImageAni() {
+      const origImgCoords = getCoords(this.imageEl);
+      const newImageCoords = getCoords(this.newImageEl);
+      this.newImageEl.setAttribute('style', `transform: translate3d(${origImgCoords.left - newImageCoords.left - this.imageEl.width}px, ${origImgCoords.top - newImageCoords.top - this.imageEl.height}px, 0) scale(0.3)`);
     }
 
     showImg() {
       const enlargedImgWrapperEl = this.htmlToElement(`<div class="enlarged-image-wrapper"></div>`);
-      const newImageEl = this.htmlToElement(`<img src="${this.enlargeImage}" class="enlarged-image">`);
-      enlargedImgWrapperEl.append(newImageEl);
-      const origImgCoords = getCoords(this.$el[0]);
+      this.newImageEl = this.htmlToElement(`<img src="${this.enlargeImage}" class="enlarged-image">`);
+      enlargedImgWrapperEl.append(this.newImageEl);
       this.$enlargedImgWrapperEl = angular.element(enlargedImgWrapperEl);
       this.lightboxParentEl.append(this.$enlargedImgWrapperEl);
-      const newImageCoords = getCoords(newImageEl);
-      newImageEl.setAttribute('style', `transform: translate3d(${origImgCoords.left - newImageCoords.left - this.$el[0].width}px, ${origImgCoords.top - newImageCoords.top - this.$el[0].height}px, 0) scale(0.3)`);
+      this.setCoordsForImageAni();
+
+      setTimeout(() => {
+        this.$enlargedImgWrapperEl.addClass('ani-enter');
+      }, 0);
+
       this.$enlargedImgWrapperEl.bind('click', () => {
         this.hideImg();
       });
-      this.$el[0].setAttribute('style', `visibility: hidden`);
+      this.imageEl.setAttribute('style', `visibility: hidden`);
 
     }
 
