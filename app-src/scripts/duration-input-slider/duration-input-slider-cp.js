965909fa0eaea2933f0672318d2006d8f30209de
Johannes Millan <johannes.millan@gmail.com>
2018-05-01 17:04:56 +0200

    feat(durationInputSlider): connect input to model and allow for multiple rounds

--- app-src/scripts/duration-input-slider/duration-input-slider-cp.js
+++ app-src/scripts/duration-input-slider/duration-input-slider-cp.js
@@ -10,12 +10,18 @@
 
   class DurationInputSliderCtrl {
     /* @ngInject */
-    constructor(ParseDuration, $element) {
+    constructor(ParseDuration, $element, $scope) {
       const el = $element[0];
       this.el = el;
+      this.$scope = $scope;
+      this.minutesBefore = 0;
     }
 
-    $onInit(){
+    onModelChange() {
+      console.log(this);
+    }
+
+    $onInit() {
       this.circle = this.el.querySelector('.handle-wrapper');
 
       this.mouseDownHandler = () => {
@@ -32,9 +38,9 @@
         const theta = Math.atan2(y, x) * (180 / Math.PI);
 
         const cssDegrees = convertThetaToCssDegrees(theta);
-        const rotate = 'rotate(' + cssDegrees + 'deg)';
-        this.circle.style.transform = rotate;
-
+        //const isMovingRight = ev
+        this.setValueFromRotation(cssDegrees);
+        this.setCircleRotation(cssDegrees);
       };
 
       function convertThetaToCssDegrees(theta) {
@@ -48,12 +54,62 @@
       };
 
       this.el.addEventListener('mousedown', this.mouseDownHandler);
+
+      this.setRotationFromValue();
     }
 
     $onDestroy() {
       this.el.removeEventListener('mousedown', this.mouseUpHandler);
       this.el.removeEventListener('mousemove', this.mouseMoveHandler);
       this.el.removeEventListener('mouseup', this.mouseUpHandler);
+    }
+
+    setCircleRotation(cssDegrees) {
+      const rotate = 'rotate(' + cssDegrees + 'deg)';
+      this.circle.style.transform = rotate;
+    }
+
+    setValueFromRotation(degrees) {
+      let minutesFromDegrees;
+      // NOTE: values are negative for the last quadrant
+      if (degrees >= 0) {
+        minutesFromDegrees = (degrees / 360 * 60);
+      } else {
+        minutesFromDegrees = ((degrees + 360) / 360 * 60)
+      }
+
+      minutesFromDegrees = parseInt(minutesFromDegrees, 10);
+
+      let hours = parseInt(moment.duration(this.ngModel).asHours(), 10);
+
+      const minuteDelta = minutesFromDegrees - this.minutesBefore;
+      const threeshold = 40;
+      if (minuteDelta > threeshold) {
+        hours--;
+      } else if (-1 * minuteDelta > threeshold) {
+        hours++;
+      }
+
+      this.minutesBefore = minutesFromDegrees;
+
+      if (hours < 0) {
+        hours = 0;
+      }
+
+      this.$scope.$evalAsync(() => {
+        this.ngModel = moment.duration({
+          hours: hours,
+          minutes: minutesFromDegrees
+        });
+      });
+    }
+
+    setRotationFromValue() {
+      const momentVal = moment.duration(this.ngModel);
+      const minutes = momentVal.minutes();
+      const degrees = minutes * 360 / 60;
+      this.minutesBefore = minutes;
+      this.setCircleRotation(degrees);
     }
   }
 
@@ -63,8 +119,8 @@
       templateUrl: 'scripts/duration-input-slider/duration-input-slider-cp.html',
       controller: DurationInputSliderCtrl,
       controllerAs: '$ctrl',
-      bindToController: {
-        ngModel: '<'
+      bindings: {
+        ngModel: '='
       },
       require: { $ngModelCtrl: '^ngModel' }
     });
