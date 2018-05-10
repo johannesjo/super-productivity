7912b74bf126ff28db3e8db91edee54284149736
Johannes Millan <johannes.millan@gmail.com>
2018-05-10 21:36:45 +0200

    feat: add add-task-bar as standalone component

--- app-src/scripts/daily-planner/daily-planner-d.js
+++ app-src/scripts/daily-planner/daily-planner-d.js
@@ -27,70 +27,14 @@
   /* @ngInject */
   function DailyPlannerCtrl(IS_ELECTRON, $rootScope, $window, $scope, Tasks, TasksUtil, Dialogs, $state, Jira, $filter, Git, $mdDialog, EV, $interval) {
     let vm = this;
-    vm.refreshRemoteTasks = () => {
-      vm.taskSuggestions = [];
-
-      if (Jira.isSufficientJiraSettings()) {
-        Jira.checkForNewAndAddToBacklog();
-
-        Jira.getSuggestions().then((res) => {
-          vm.taskSuggestions = vm.taskSuggestions.concat(Jira.transformIssues(res));
-        });
-      }
-
-      // add new git tasks
-      Git.checkForNewAndAddToBacklog();
-
-      if (Git.isRepoConfigured() && $rootScope.r.git.isShowIssuesFromGit) {
-        Git.getIssueList()
-          .then((res) => {
-            vm.taskSuggestions = vm.taskSuggestions.concat(res.data);
-          });
-      }
-    };
-
-    vm.getFilteredTaskSuggestions = (searchText) => {
-      return searchText ? $filter('filter')(vm.taskSuggestions, searchText, false, 'title') : vm.taskSuggestions;
-    };
 
     vm.init = () => {
       vm.limitBacklogTo = 3;
       vm.backlogTasks = Tasks.getBacklog();
 
       vm.isRemoteTasks = (IS_ELECTRON && Jira.isSufficientJiraSettings() || $rootScope.r.git.isAutoImportToBacklog);
-      vm.refreshRemoteTasks();
     };
     vm.init();
-
-    vm.addTask = () => {
-      if (vm.newTask) {
-        if (vm.newTask.originalType && vm.newTask.originalType === 'GITHUB' && $rootScope.r.git.isShowIssuesFromGit) {
-          Git.getCommentListForIssue(vm.newTask.originalId)
-            .then(res => {
-              vm.newTask.originalComments = res.data;
-              Tasks.addToday(vm.newTask);
-              vm.newTask = undefined;
-              vm.newTaskTitle = undefined;
-            });
-        } else {
-          Tasks.addToday(vm.newTask);
-          vm.newTask = undefined;
-          vm.newTaskTitle = undefined;
-        }
-
-      } else if (vm.newTaskTitle) {
-        Tasks.addToday({
-          title: vm.newTaskTitle
-        });
-        vm.newTaskTitle = undefined;
-      }
-
-      // if we have already defined enough tasks and the
-      // new task field is empty go to work view
-      else if ($rootScope.r.tasks.length > 0) {
-        vm.done();
-      }
-    };
 
     vm.done = () => {
       // only open if there is no current task already selected
@@ -101,6 +45,14 @@
           });
       } else {
         $state.go('work-view');
+      }
+    };
+
+    vm.onEmptySubmit = () => {
+      // if we have already defined enough tasks and the
+      // new task field is empty go to work view
+      if ($rootScope.r.tasks.length > 0) {
+        this.done();
       }
     };
 
