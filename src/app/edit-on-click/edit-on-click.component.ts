c3cf8485a37795c5aa0dea872893436a9e9ecefa
Johannes Millan <johannes.millan@gmail.com>
2018-05-14 20:50:36 +0200

    feat: port edit on click

--- src/app/edit-on-click/edit-on-click.component.ts
+++ src/app/edit-on-click/edit-on-click.component.ts
@@ -1,22 +1,54 @@
-import {Component, OnInit} from '@angular/core';
-import {Input} from '@angular/core';
-import {Output} from '@angular/core';
-import {EventEmitter} from '@angular/core';
-import {ElementRef} from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Directive } from '@angular/core';
+import { OnChanges } from '@angular/core';
 
-@Component({
-  selector: 'sup-edit-on-click',
-  templateUrl: './edit-on-click.component.html',
-  styleUrls: ['./edit-on-click.component.scss']
+
+// HELPER
+// -----------------------------------
+function insertAtCursor(el, newText) {
+  const sel = window.getSelection();
+
+  const start = sel.anchorOffset;
+  const end = sel.focusOffset;
+  const text = el.innerText;
+
+  const textBefore = text.substring(0, start);
+  const textAfter = text.substring(end, text.length);
+
+  const completeTextAfterInsert = (textBefore + newText + textAfter).trim();
+
+  el.innerText = completeTextAfterInsert;
+
+  // reset caret to proper offset
+  const range = document.createRange();
+  range.setStart(el.childNodes[0], start + newText.length);
+  range.collapse(true);
+  const sel2 = window.getSelection();
+
+  sel2.removeAllRanges();
+  sel2.addRange(range);
+}
+
+function removeTags(str) {
+  return str.replace(/<\/?[^`]+?\/?>/gmi, '\n') //replace all tags
+    .replace(/\n/gmi, '') // replace line breaks
+    .replace(/&nbsp;/gmi, '') // replace line breaks
+    .trim();
+}
+
+@Directive({
+  selector: '[supEditOnClick]',
 })
-export class EditOnClickComponent implements OnInit {
+export class EditOnClickComponent implements OnInit, OnChanges {
   @Input() value: string;
   @Input() eventId: string;
   @Output() editFinished: EventEmitter<any> = new EventEmitter();
-  modelCopy: string;
+  lastValue: string;
   isShowEdit: boolean;
-  inputEl: HTMLInputElement;
-  textEl: HTMLElement;
   el: HTMLElement;
 
 
@@ -25,45 +57,107 @@ export class EditOnClickComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.textEl = this.el.querySelectorAll('div')[0];
-  }
+    const el = this.el;
 
-  toggleShowEdit() {
-    this.textEl.style.display = 'none';
-    this.isShowEdit = true;
-    this.modelCopy = this.value;
-    setTimeout(() => {
-      this.inputEl = this.el.querySelectorAll('input')[0];
-      this.inputEl.focus();
-      this.inputEl.value = this.modelCopy;
+    if (!(el.getAttribute('contenteditable'))) {
+      el.setAttribute('contenteditable', 'true');
+    }
+
+    el.addEventListener('input', () => {
+      this.setValueFromElement();
     });
+
+    el.addEventListener('blur', (ev) => {
+      this.setValueFromElement();
+      this.onEditDone(ev);
+    });
+
+    // prevent keyboard shortcuts from firing when here
+    el.addEventListener('keydown', (ev) => {
+      ev.stopPropagation();
+
+      // blur on escape
+      if (ev.keyCode === 13 || ev.keyCode === 27) {
+        ev.preventDefault();
+        setTimeout(() => {
+          el.blur();
+        });
+      }
+    });
+
+    // blur on enter
+    el.addEventListener('keypress', function (ev) {
+      if (ev.keyCode === 13) {
+        // prevent keyboard shortcuts from firing when here
+        ev.preventDefault();
+        setTimeout(() => {
+          el.blur();
+        });
+      }
+    });
+
+    el.onpaste = (ev) => {
+      ev.stopPropagation();
+      ev.preventDefault();
+      const text = ev.clipboardData.getData('text/plain')
+        .trim();
+      insertAtCursor(el, text);
+      this.setValueFromElement();
+    };
   }
 
-  finishEdit() {
-    this.modelCopy = this.inputEl.value;
-
-    const isChanged = (this.value !== this.modelCopy);
-    if (isChanged) {
-      // update if changes were made
-      this.value = this.modelCopy;
+  ngOnChanges(changes) {
+    if (!changes.hasOwnProperty('value')) {
+      return false;
     }
 
-    // check for show edit to only trigger once
-    // if (this.isShowEdit && typeof this.editFinished === "function") {
-    if (this.isShowEdit) {
-      this.editFinished.emit({isChanged, newVal: this.value});
-    }
+    this.lastValue = changes.value.currentValue;
 
-    this.isShowEdit = false;
-    this.textEl.style.display = 'block';
+    this.refreshView();
   }
 
+  refreshView() {
+    this.el.innerText = this.value;
+  }
 
-  // clickToggleEvHandler(ev, eventId) {
-  //   if (eventId === vm.editOnClickEvId) {
-  //     vm.toggleShowEdit();
-  //   }
-  // }
-  //
-  // $scope.$on(EDIT_ON_CLICK_TOGGLE_EV, clickToggleEvHandler);
+  onEditDone(event) {
+    // deselect all text
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+    }
+    /* tslint:disable */
+    else if (document.selection) {
+      document.selection.empty();
+    }
+    /* tslint:enable */
+
+    const curVal = this.el.innerText;
+    const isChanged = (this.lastValue !== curVal);
+
+    if (this.editFinished) {
+      this.editFinished.emit({
+        isChanged,
+        newVal: curVal,
+        $taskEl: this.el.closest('.task'),
+        event,
+      });
+    }
+    this.lastValue = curVal;
+  }
+
+  setValueFromElement() {
+    let curVal = this.el.innerText;
+    curVal = removeTags(curVal);
+    this.value = curVal;
+  }
+
+  toggleFromOutside(ev, eventId) {
+    if (eventId === this.eventId) {
+      setTimeout(() => {
+        this.el.focus();
+        // select all when doing this
+        document.execCommand('selectAll', false, null);
+      });
+    }
+  }
 }
