e3b623ee4caf8ac576619e1038da2fed86c26c14
Johannes Millan <johannes.millan@gmail.com>
2018-11-04 23:42:57 +0100

    feat: replace days with hours

--- src/app/ui/duration/ms-to-string.pipe.ts
+++ src/app/ui/duration/ms-to-string.pipe.ts
@@ -3,9 +3,16 @@ import * as moment from 'moment';
 
 export const msToString = (value: any, showSeconds?: boolean, isHideEmptyPlaceholder?: boolean): string => {
   const md = moment.duration(value);
+  let hours = 0;
+  if (+md.days() > 0) {
+    hours = (md.days() * 24);
+  }
+  if (+md.hours() > 0) {
+    hours += md.hours();
+  }
   const parsed =
-    ((+md.days() > 0) ? (md.days() + 'd ') : '')
-    + ((+md.hours() > 0) ? (md.hours() + 'h ') : '')
+    // ((+md.days() > 0) ? (md.days() + 'd ') : '')
+    ((hours > 0) ? (hours + 'h ') : '')
     + ((+md.minutes() > 0) ? (md.minutes() + 'm ') : '')
     + (showSeconds && (+md.seconds() > 0) ? (md.seconds() + 's ') : '');
   if (!isHideEmptyPlaceholder && parsed.trim() === '') {
