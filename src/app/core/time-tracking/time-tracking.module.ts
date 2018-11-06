91329fc5a499fc2c67d0f4c80c784a13772b377d
Johannes Millan <johannes.millan@gmail.com>
2018-11-06 19:21:58 +0100

    feat: create tick in a more reactive style

--- src/app/core/time-tracking/time-tracking.module.ts
+++ src/app/core/time-tracking/time-tracking.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimeTrackingService } from './time-tracking.service';
+import { DialogIdleComponent } from './dialog-idle/dialog-idle.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
+  declarations: [DialogIdleComponent],
   providers: [
     TimeTrackingService
   ]
 })
 export class TimeTrackingModule {
-  constructor(private readonly _timeTrackingService: TimeTrackingService) {
-    this._timeTrackingService.init();
-  }
 }
--- src/app/core/time-tracking/time-tracking.service.ts
+++ src/app/core/time-tracking/time-tracking.service.ts
@@ -1,35 +1,30 @@
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 import { TRACKING_INTERVAL, WORKLOG_DATE_STR_FORMAT } from '../../app.constants';
-import { BehaviorSubject } from 'rxjs';
+import { interval, Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
 import { Tick } from './time-tracking';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimeTrackingService {
-  public tick$: BehaviorSubject<Tick> = new BehaviorSubject({
-    duration: 0,
-    date: moment().format(WORKLOG_DATE_STR_FORMAT)
-  });
+  private _currentTrackingStart: number;
+  public tick$: Observable<Tick> = interval(TRACKING_INTERVAL).pipe(
+    map(() => {
+      const delta = Date.now() - this._currentTrackingStart;
+      this._currentTrackingStart = Date.now();
+      return {
+        duration: delta,
+        date: moment().format(WORKLOG_DATE_STR_FORMAT)
+      };
+    }),
+    // important because we want the same interval for everyone
+    share()
+  );
 
-  init() {
-    this.initPoll();
-  }
 
-  initPoll() {
-    let currentTrackingStart = moment();
-
-    setInterval(() => {
-      const now = moment();
-      const realPeriodDuration = moment.duration(now.diff(currentTrackingStart))
-        .asMilliseconds();
-      this.tick$.next({
-        duration: realPeriodDuration,
-        date: now.format(WORKLOG_DATE_STR_FORMAT)
-      });
-      // set to now
-      currentTrackingStart = moment();
-    }, TRACKING_INTERVAL);
+  constructor() {
+    this._currentTrackingStart = Date.now();
   }
 }
