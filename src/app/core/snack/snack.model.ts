53ee30cf562d14f89a73efaf73238cc7336d6a70
Johannes Millan <johannes.millan@gmail.com>
2018-11-10 22:23:30 +0100

    feat(snack): add custom icon support

--- src/app/core/snack/snack.model.ts
+++ src/app/core/snack/snack.model.ts
@@ -5,6 +5,7 @@ export type SnackType = 'ERROR' | 'SUCCESS'| 'GOOGLE_LOGIN';
 export interface SnackParams {
   message: string;
   type?: SnackType;
+  icon?: string;
   actionStr?: string;
   actionId?: string;
   delay?: number;
