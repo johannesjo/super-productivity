4a49db6d322dd9d3223f57d02689aca75f247858
Johannes Millan <johannes.millan@gmail.com>
2018-11-30 22:49:19 +0100

    feat(notes): save note to session storage

--- src/app/note/dialog-add-note/dialog-add-note.component.ts
+++ src/app/note/dialog-add-note/dialog-add-note.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { NoteService } from '../note.service';
 import { Reminder } from '../../reminder/reminder.model';
 
+const NOTE_TMP_STORAGE_KEY = 'SP_NOTE_TMP_EDIT';
+
 @Component({
   selector: 'dialog-add-note',
   templateUrl: './dialog-add-note.component.html',
@@ -18,7 +20,7 @@ export class DialogAddNoteComponent {
     private _noteService: NoteService,
     @Inject(MAT_DIALOG_DATA) public data: { reminder: Reminder },
   ) {
-    this._matDialogRef.disableClose = true;
+    this.noteContent = sessionStorage.getItem(NOTE_TMP_STORAGE_KEY);
   }
 
   submit() {
@@ -31,11 +33,19 @@ export class DialogAddNoteComponent {
       remindAt,
       true,
     );
+    this._clearSessionStorage();
     this.close();
+  }
+
+  saveTmp() {
+    sessionStorage.setItem(NOTE_TMP_STORAGE_KEY, this.noteContent);
   }
 
   close() {
     this._matDialogRef.close();
   }
 
+  private _clearSessionStorage() {
+    sessionStorage.setItem(NOTE_TMP_STORAGE_KEY, '');
+  }
 }
