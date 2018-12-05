af03eaecccf4c6ab87444456137c7c50adb4eacb
Johannes Millan <johannes.millan@gmail.com>
2018-12-05 17:16:32 +0100

    feat: add note to install extension for jira cfg

--- src/app/issue/issue.const.ts
+++ src/app/issue/issue.const.ts
@@ -5,7 +5,12 @@ export const ISSUE_PROVIDER_FORM_CFGS: ConfigFormConfig = [
   {
     title: 'Jira',
     key: 'JIRA',
-    items:[
+    items: [
+      {
+        className: 'tpl isHideWhenJiraSupport',
+        template: `<p>Please <a href="https://chrome.google.com/webstore/detail/super-productivity/ljkbjodfmekklcoibdnhahlaalhihmlb">
+download the chrome extension</a> in order to allow communication with the Jira Api. Note that this doesn\'t work for mobile.</p>`,
+      },
       {
         className: 'tpl',
         template: ' <h3 class="sub-section-heading">Credentials</h3>',
