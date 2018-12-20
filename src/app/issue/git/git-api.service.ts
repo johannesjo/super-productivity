a0da47a5ef0f764be840cf18c64b5ace7501f608
Johannes Millan <johannes.millan@gmail.com>
2018-12-20 20:45:24 +0100

    feat(git): add pre check for api

--- src/app/issue/git/git-api.service.ts
+++ src/app/issue/git/git-api.service.ts
@@ -4,10 +4,10 @@ import { GitCfg } from './git';
 import { SnackService } from '../../core/snack/snack.service';
 import { HttpClient } from '@angular/common/http';
 import { GIT_API_BASE_URL } from './git.const';
-import { Observable } from 'rxjs';
-import { GitIssueSearchResult, GitOriginalIssue } from './git-api-responses';
+import { Observable, throwError } from 'rxjs';
+import { GitIssueSearchResult } from './git-api-responses';
 import { map } from 'rxjs/operators';
-import { IssueProviderKey, SearchResultItem } from '../issue';
+import { SearchResultItem } from '../issue';
 import { mapGitIssue, mapGitIssueToSearchResult } from './git-issue/git-issue-map.util';
 
 const BASE = GIT_API_BASE_URL;
@@ -30,6 +30,7 @@ export class GitApiService {
 
 
   searchIssue(searchText: string): Observable<SearchResultItem[]> {
+    this._checkSettings();
     return this._http.get(`${BASE}search/issues?q=${encodeURI(searchText)}`)
       .pipe(
         map((res: GitIssueSearchResult) => {
@@ -43,14 +44,24 @@ export class GitApiService {
   }
 
   getIssueById(issueNumber: number) {
+    this._checkSettings();
     return this._http.get(`${BASE}repos/${this._cfg.repo}/issues/${issueNumber}`);
   }
 
   getCommentListForIssue(issueNumber: number) {
+    this._checkSettings();
     return this._http.get(BASE + `${BASE}repos/${this._cfg.repo}/issues/${issueNumber}/comments`);
   }
 
+  private _checkSettings() {
+    if (!this._isValidSettings()) {
+      this._snackService.open({type: 'ERROR', message: 'Git is not properly configured'});
+      throw throwError('Not enough settings');
+    }
+  }
+
   private _isValidSettings(): boolean {
-    return true;
+    const cfg = this._cfg;
+    return cfg && cfg.repo && cfg.repo.length > 0;
   }
 }
