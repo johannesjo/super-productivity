804fe64e4cd60bf929e676524f6b8069aed675a4
Johannes Millan <johannes.millan@gmail.com>
2019-01-04 16:32:34 +0100

    feat(jira): add issue selector for transition handling

--- src/app/issue/jira/jira-cfg/jira-cfg.component.ts
+++ src/app/issue/jira/jira-cfg/jira-cfg.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ConfigFormSection, ConfigSectionKey, SectionConfig } from '../../../core/config/config.model';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ConfigFormSection, ConfigSectionKey } from '../../../core/config/config.model';
 import { ProjectCfgFormKey } from '../../../project/project.model';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { JiraCfg } from '../jira';
 import { expandAnimation } from '../../../ui/animations/expand.ani';
+import { Subject } from 'rxjs';
+import { SearchResultItem } from '../../issue';
+import { catchError, debounceTime, switchMap, takeUntil } from 'rxjs/operators';
+import { JiraApiService } from '../jira-api.service';
 
 @Component({
   selector: 'jira-cfg',
@@ -16,17 +20,48 @@ import { expandAnimation } from '../../../ui/animations/expand.ani';
 export class JiraCfgComponent implements OnInit {
   @Output() save: EventEmitter<{ sectionKey: ConfigSectionKey | ProjectCfgFormKey, config: any }> = new EventEmitter();
 
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  issueSuggestionsCtrl: FormControl = new FormControl();
+  filteredIssueSuggestions: SearchResultItem[];
+  isLoading = false;
+
   section: ConfigFormSection;
   cfg: JiraCfg;
   fields: FormlyFieldConfig[];
   form = new FormGroup({});
   options: FormlyFormOptions = {};
 
-  constructor() {
+  constructor(private _jiraApiService: JiraApiService) {
   }
 
   ngOnInit(): void {
     this.fields = this.section.items;
+
+    this.issueSuggestionsCtrl.setValue('');
+
+    this.issueSuggestionsCtrl.valueChanges.pipe(
+      debounceTime(400),
+      switchMap((searchTerm) => {
+        if (searchTerm && searchTerm.length > 1) {
+          this.isLoading = true;
+          return this._jiraApiService.search(searchTerm).pipe(
+            catchError(() => {
+              return [];
+            })
+          );
+        } else {
+          // Note: the outer array signifies the observable stream the other is the value
+          return [[]];
+        }
+      }),
+      takeUntil(this.destroy$)
+    )
+      .subscribe((val) => {
+        console.log('sub', val);
+
+        this.isLoading = false;
+        this.filteredIssueSuggestions = val;
+      });
   }
 
   submit() {
@@ -38,6 +73,15 @@ export class JiraCfgComponent implements OnInit {
         config: this.cfg,
       });
     }
+  }
+
+  updateTransitionOptions() {
+    const issue = this.issueSuggestionsCtrl.value as SearchResultItem;
+    if (!issue || typeof issue === 'string') {
+      this.issueSuggestionsCtrl.setValue('');
+      return;
+    }
+
   }
 
 }
