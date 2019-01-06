f8908a7b021c46f726bcc36068257ac7fd97ce57
Johannes Millan <johannes.millan@gmail.com>
2019-01-06 19:43:30 +0100

    feat(pomodoro): add more outline code

--- src/app/pomodoro/pomodoro.service.ts
+++ src/app/pomodoro/pomodoro.service.ts
@@ -4,21 +4,30 @@ import { ConfigService } from '../core/config/config.service';
 import { map } from 'rxjs/operators';
 import { PomodoroConfig } from '../core/config/config.model';
 import { DEFAULT_CFG } from '../core/config/default-config.const';
+import { TimeTrackingService } from '../time-tracking/time-tracking.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PomodoroService {
   cfg$: Observable<PomodoroConfig> = this._configService.cfg$.pipe(map(cfg => cfg && cfg.pomodoro));
-  isPause$: Observable<boolean>;
-  isPlay$: Observable<boolean>;
+  duration$ = this.cfg$.pipe(map(cfg => cfg.duration));
+
+  // currentSessionStartTime$: Observable<number> = this.currentCycle$.pipe();
   currentSessionTime$: Observable<number> = new BehaviorSubject(DEFAULT_CFG.pomodoro.duration);
+  sessionProgress$: Observable<number>;
+
+  isManualPause$: Observable<boolean>;
+  isPomodoroBreak$: Observable<boolean>;
   currentCycle$: Observable<number>;
   isOnLongBreak: Observable<boolean>;
   isOnShortBreak: Observable<boolean>;
-  sessionProgress$: Observable<number>;
 
-  constructor(private _configService: ConfigService) {
+  constructor(
+    private _configService: ConfigService,
+    private _timeTrackingService: TimeTrackingService,
+  ) {
+    this.cfg$.subscribe(val => console.log(val));
   }
 
   skipBreak() {
