0128e6373a99b5c43d81d7e98256751699a1aa09
Johannes Millan <johannes.millan@gmail.com>
2019-02-01 20:16:31 +0100

    feat: improve planning mode

--- src/app/features/planning-mode/planning-mode.service.ts
+++ src/app/features/planning-mode/planning-mode.service.ts
@@ -1,33 +1,48 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from "rxjs";
 import { Observable } from "rxjs";
-import { combineLatest } from "rxjs";
+import { merge } from "rxjs";
 import { TaskService } from "../tasks/task.service";
 import { map } from "rxjs/operators";
-import { tap } from "rxjs/operators";
+import { withLatestFrom } from "rxjs/operators";
+import { delay } from "rxjs/operators";
+import { ProjectService } from "../project/project.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanningModeService {
-  private _iPlanningModeEndedUser$ = new BehaviorSubject(false);
+  private _iPlanningModeEndedUser$ = new BehaviorSubject<boolean>(false);
+  private _manualTriggerCheck$ = new BehaviorSubject(null);
+  private _triggerCheck$ = merge(
+    this._manualTriggerCheck$,
+    // TODO fix hacky way of waiting for data to be loaded
+    this._projectService.onProjectChange$.pipe(delay(100))
+  );
 
-  isPlanningMode$: Observable<boolean> = combineLatest(
-    this._taskService.isHasTasksToWorkOn$,
-    this._iPlanningModeEndedUser$,
-  ).pipe(
-    tap(v => console.log(v)),
-    map(([isHasTasksToWorkOn, isPlanningEndedByUser]) => !isHasTasksToWorkOn || !isPlanningEndedByUser),
+  isPlanningMode$: Observable<boolean> = this._triggerCheck$.pipe(
+    withLatestFrom(
+      this._taskService.isHasTasksToWorkOn$,
+      this._iPlanningModeEndedUser$,
+    ),
+    map(([t, isHasTasksToWorkOn, isPlanningEndedByUser]) => !isHasTasksToWorkOn && !isPlanningEndedByUser),
   );
 
   constructor(
     private _taskService: TaskService,
+    private _projectService: ProjectService,
   ) {
     this.isPlanningMode$.subscribe((v) => console.log('isPlanningMode$', v));
+    this._projectService.onProjectChange$.subscribe((v) => console.log('_projectService.onProjectChange$', v));
+    this.reCheckPlanningMode();
   }
-
 
   leavePlanningMode() {
     this._iPlanningModeEndedUser$.next(true);
+    this.reCheckPlanningMode();
+  }
+
+  reCheckPlanningMode() {
+    this._manualTriggerCheck$.next(true);
   }
 }
