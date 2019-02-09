a679f6589d39f7dabfadf80069e8b7a8e65b0460
Johannes Millan <johannes.millan@gmail.com>
2019-02-09 18:22:37 +0100

    feat(notes): make reminders work when another project is active

--- src/app/features/note/dialog-view-note-reminder/dialog-view-note-reminder.component.ts
+++ src/app/features/note/dialog-view-note-reminder/dialog-view-note-reminder.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { Reminder } from '../../reminder/reminder.model';
 import { Note } from '../note.model';
 import { NoteService } from '../note.service';
 import { Observable } from 'rxjs';
 import { ReminderService } from '../../reminder/reminder.service';
+import { ProjectService } from '../../project/project.service';
+import { Project } from '../../project/project.model';
 
 @Component({
   selector: 'dialog-view-note-reminder',
@@ -12,35 +14,43 @@ import { ReminderService } from '../../reminder/reminder.service';
   styleUrls: ['./dialog-view-note-reminder.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DialogViewNoteReminderComponent implements OnInit {
+export class DialogViewNoteReminderComponent {
   note$: Observable<Note> = this._noteService.getById(this.data.reminder.relatedId);
-
-  private _reminder: Reminder;
+  reminder: Reminder = this.data.reminder;
+  isForCurrentProject = (this.reminder.projectId === this._projectService.currentId);
+  targetProject$: Observable<Project> = this._projectService.getById(this.reminder.projectId);
 
   constructor(
     private _matDialogRef: MatDialogRef<DialogViewNoteReminderComponent>,
     private _noteService: NoteService,
+    private _projectService: ProjectService,
     private _reminderService: ReminderService,
     @Inject(MAT_DIALOG_DATA) public data: { reminder: Reminder },
   ) {
     this._matDialogRef.disableClose = true;
-    this._reminder = this.data.reminder;
-  }
-
-  ngOnInit() {
-
   }
 
   dismiss() {
-    this._noteService.update(this._reminder.relatedId, {
-      reminderId: null,
-    });
-    this._reminderService.removeReminder(this._reminder.id);
-    this.close();
+    const reminder = this.reminder;
+    if (this.isForCurrentProject) {
+      this._noteService.update(reminder.relatedId, {
+        reminderId: null,
+      });
+      this._reminderService.removeReminder(reminder.id);
+      this.close();
+
+    } else {
+      this._noteService.updateFromDifferentProject(reminder.projectId, reminder.relatedId, {
+        reminderId: null,
+      }).then(() => {
+        this._reminderService.removeReminder(reminder.id);
+        this.close();
+      });
+    }
   }
 
   snooze() {
-    this._reminderService.updateReminder(this._reminder.id, {
+    this._reminderService.updateReminder(this.reminder.id, {
       remindAt: Date.now() + (10 * 60 * 1000)
     });
     this.close();
