f485ae92b78a9171350d5f352c3fadd6569a0ba2
Johannes Millan <johannes.millan@gmail.com>
2019-02-16 22:48:19 +0100

    feat: make mobile notifications work

--- src/app/core/notify/notify.service.ts
+++ src/app/core/notify/notify.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 import { NotifyModel } from './notify.model';
+import { environment } from '../../../environments/environment';
+import { IS_ELECTRON } from '../../app.constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotifyService {
   async notify(options: NotifyModel): Promise<Notification> {
-    if (this._isBasicNotificationSupport()) {
+    if (this._isServiceWorkerAvailable()) {
+      const reg = await navigator.serviceWorker.getRegistration('ngsw-worker.js');
+      reg.showNotification(options.title, {
+        icon: 'assets/icons/icon-128x128.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        ...options,
+      });
+
+    } else if (this._isBasicNotificationSupport()) {
       const permission = await Notification.requestPermission();
       // not supported for basic notifications so we delete them
       delete options.actions;
@@ -34,7 +48,11 @@ export class NotifyService {
     }
   }
 
-  private _isBasicNotificationSupport() {
+  private _isBasicNotificationSupport(): boolean {
     return 'Notification' in window;
+  }
+
+  private _isServiceWorkerAvailable(): boolean {
+    return 'serviceWorker' in navigator && environment.production && !IS_ELECTRON;
   }
 }
