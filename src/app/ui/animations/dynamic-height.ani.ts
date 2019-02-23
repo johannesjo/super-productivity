5a11809b57595692b99afd326edfcf3b73dac2da
Johannes Millan <johannes.millan@gmail.com>
2019-02-24 00:50:44 +0100

    feat: add dynamic height ani

---
+++ src/app/ui/animations/dynamic-height.ani.ts
@@ -0,0 +1,13 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { ANI_STANDARD_TIMING } from './animation.const';
+
+export const dynamicHeightAnimation = [
+  trigger('dynamicHeight', [
+      transition('void <=> *', []),
+      transition('* <=> *', [
+        style({height: '{{startHeight}}px', opacity: 0}),
+        animate(ANI_STANDARD_TIMING),
+      ], {params: {startHeight: 0}})
+  ])
+];
+
