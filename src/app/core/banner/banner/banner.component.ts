00e488f77828efed472eed519381ea14f9ea54ab
Johannes Millan <johannes.millan@gmail.com>
2019-02-24 01:04:10 +0100

    feat(banner): handle banner transition if there are multiple

--- src/app/core/banner/banner/banner.component.ts
+++ src/app/core/banner/banner/banner.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BannerService } from '../banner.service';
 import { expandAnimation } from '../../../ui/animations/expand.ani';
-import { BannerAction } from '../banner.model';
+import { Banner, BannerAction } from '../banner.model';
+import { concatMap, mapTo } from 'rxjs/operators';
+import { merge, Observable, of, timer } from 'rxjs';
 
 @Component({
   selector: 'banner',
@@ -11,6 +13,20 @@ import { BannerAction } from '../banner.model';
   animations: [expandAnimation]
 })
 export class BannerComponent {
+  // TODO maybe improve if initial delay is annoying
+  activeBanner$: Observable<Banner> = this.bannerService.activeBanner$.pipe(
+    concatMap((activeBanner) => {
+      if (activeBanner) {
+        return merge(
+          of(null),
+          timer(500).pipe(mapTo(activeBanner))
+        );
+      } else {
+        return of(null);
+      }
+    })
+  );
+
   constructor(
     public bannerService: BannerService,
   ) {
