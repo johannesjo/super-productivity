6234ad54281206b851a2d401acfe4fe2f5f83412
Johannes Millan <johannes.millan@gmail.com>
2019-03-08 14:02:58 +0100

    refactor(worklog): remove dialog task summary

--- src/app/features/worklog/worklog.module.ts
+++ src/app/features/worklog/worklog.module.ts
@@ -5,7 +5,6 @@ import { UiModule } from '../../ui/ui.module';
 import { DialogWorklogExportComponent } from './dialog-worklog-export/dialog-worklog-export.component';
 import { WorklogExportComponent } from './worklog-export/worklog-export.component';
 import { FormsModule } from '@angular/forms';
-import { DialogTaskSummaryComponent } from './dialog-task-summary/dialog-task-summary.component';
 
 @NgModule({
   imports: [
@@ -17,11 +16,9 @@ import { DialogTaskSummaryComponent } from './dialog-task-summary/dialog-task-su
     WorklogComponent,
     DialogWorklogExportComponent,
     WorklogExportComponent,
-    DialogTaskSummaryComponent,
   ],
   entryComponents: [
     DialogWorklogExportComponent,
-    DialogTaskSummaryComponent,
   ],
   exports: [
     WorklogComponent,
