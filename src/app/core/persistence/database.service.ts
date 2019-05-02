19610be2a51d539bd72850e14fc569d13123e171
Johannes Millan <johannes.millan@gmail.com>
2019-05-03 00:50:34 +0200

    feat: clear database when syncing

--- src/app/core/persistence/database.service.ts
+++ src/app/core/persistence/database.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as localForage from 'localforage';
-import {SnackService} from '../snack/snack.service';
+import { SnackService } from '../snack/snack.service';
 
 @Injectable({
   providedIn: 'root',
@@ -32,6 +32,14 @@ export class DatabaseService {
   async remove(key: string): Promise<any> {
     try {
       return localForage.ready().then(() => localForage.removeItem(key));
+    } catch (e) {
+      this._snackService.open({type: 'ERROR', msg: 'Error while deleting data'});
+    }
+  }
+
+  async clearDatabase(): Promise<any> {
+    try {
+      return localForage.ready().then(() => localForage.clear());
     } catch (e) {
       this._snackService.open({type: 'ERROR', msg: 'Error while deleting data'});
     }
