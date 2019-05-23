6cc3cc160e5b0f1833dbb479e8387f60e7698268
Johannes Millan <johannes.millan@gmail.com>
2019-05-23 20:07:57 +0200

    feat(metrics): add basic metrics

--- src/app/features/metric/metric.service.ts
+++ src/app/features/metric/metric.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { initialMetricState, } from './store/metric.reducer';
-import { AddMetric, DeleteMetric, LoadMetricState, UpdateMetric, UpsertMetric } from './store/metric.actions';
-import { Observable } from 'rxjs';
-import { LineChartData, Metric, MetricState, PieChartData } from './metric.model';
-import { PersistenceService } from '../../core/persistence/persistence.service';
-import { getWorklogStr } from '../../util/get-work-log-str';
+import {Injectable} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {initialMetricState,} from './store/metric.reducer';
+import {AddMetric, DeleteMetric, LoadMetricState, UpdateMetric, UpsertMetric} from './store/metric.actions';
+import {combineLatest, from, Observable} from 'rxjs';
+import {LineChartData, Metric, MetricState, PieChartData} from './metric.model';
+import {PersistenceService} from '../../core/persistence/persistence.service';
+import {getWorklogStr} from '../../util/get-work-log-str';
 import {
   selectAllMetrics,
   selectImprovementCountsPieChartData,
@@ -16,7 +16,10 @@ import {
   selectProductivityHappinessLineChartData,
   selectProductivityHappinessLineChartDataComplete
 } from './store/metric.selectors';
-import { take } from 'rxjs/operators';
+import {map, switchMap, take} from 'rxjs/operators';
+import {TaskService} from '../tasks/task.service';
+import {WorklogService} from '../worklog/worklog.service';
+import {ProjectService} from '../project/project.service';
 
 @Injectable({
   providedIn: 'root',
@@ -29,10 +32,80 @@ export class MetricService {
   obstructionCountsPieChartData$: Observable<PieChartData> = this._store$.pipe(select(selectObstructionCountsPieChartData));
   productivityHappinessLineChartData$: Observable<LineChartData> = this._store$.pipe(select(selectProductivityHappinessLineChartDataComplete));
 
+  simpleMetrics$ = this._projectService.currentId$.pipe(
+    switchMap(() => {
+      return combineLatest(
+        this._worklogService.worklog$,
+        this._worklogService.totalTimeSpent$,
+        from(this._taskService.getAllTasks()),
+      ).pipe(
+        // TODO refactor to helper fn
+        map(([w, tt, allTasks]) => {
+          // SIMPLE METRICS
+
+          const s = {
+            start: 99999999999999999999999,
+            end: 99999999999999999999999,
+            timeSpent: 0,
+            timeEstimate: 0,
+
+            nrOfCompletedTasks: 0,
+            nrOfAllTasks: allTasks.length,
+            nrOfSubTasks: 0,
+            nrOfMainTasks: 0,
+            nrOfParentTasks: 0,
+            daysWorked: Object.keys(w).reduce((acc, y) => acc + w[y].daysWorked, 0),
+            tt,
+          };
+
+          allTasks.forEach((task) => {
+            if ((task.created < s.start)) {
+              s.start = task.created;
+            }
+
+            if (task.parentId) {
+              s.nrOfSubTasks++;
+            } else {
+              s.nrOfMainTasks++;
+              s.timeSpent += task.timeSpent;
+              s.timeEstimate += task.timeEstimate;
+            }
+
+            if (task.subTaskIds && task.subTaskIds.length) {
+              s.nrOfParentTasks++;
+            }
+
+            if (task.isDone) {
+              s.nrOfCompletedTasks++;
+            }
+          });
+
+          return {
+            ...s,
+            avgTimeSpentOnDay: s.timeSpent / s.daysWorked,
+            avgTimeSpentOnTask: s.timeSpent / s.nrOfMainTasks,
+            avgTimeSpentOnTaskIncludingSubTasks: s.timeSpent / (s.nrOfAllTasks - s.nrOfParentTasks),
+
+          };
+        }),
+      );
+    }),
+  );
+
   constructor(
     private _store$: Store<MetricState>,
+    private _taskService: TaskService,
+    private _projectService: ProjectService,
+    private _worklogService: WorklogService,
     private _persistenceService: PersistenceService,
   ) {
+
+    this.simpleMetrics$.subscribe((v) => console.log('simpleMetrics$', v));
+
+    // setTimeout(() => {
+    //   this.getSimpleMetrics().then(console.log);
+    // }, 2200);
+
     // // ADD RANDOM STUFF
     // const rnd = (max = 10, min = 0) => Math.floor(Math.random() * (max - min) + min);
     // const rndRange = (max = 10, min = 0): [number, number] => {
@@ -105,4 +178,6 @@ export class MetricService {
   getProductivityHappinessChartData$(howMany = 20): Observable<LineChartData> {
     return this._store$.pipe(select(selectProductivityHappinessLineChartData, {howMany}));
   }
+
+
 }
