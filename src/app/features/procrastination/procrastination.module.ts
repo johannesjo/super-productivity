842cdbe8f4915b3699d32643dddbeff703ff7523
Johannes Millan <johannes.millan@gmail.com>
2019-06-03 23:39:39 +0200

    feat(procrastination): add inputs

--- src/app/features/procrastination/procrastination.module.ts
+++ src/app/features/procrastination/procrastination.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {ProcrastinationComponent} from './procrastination.component';
 import {UiModule} from '../../ui/ui.module';
 import {TasksModule} from '../tasks/tasks.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     UiModule,
     TasksModule,
+    RouterModule,
   ],
   declarations: [
     ProcrastinationComponent,
