626628ed6ee403501dfa6a8801245b773c5ab6cb
Johannes Millan <johannes.millan@gmail.com>
2019-06-29 01:49:31 +0200

    feat(dailySummary): add migration for lastCompletedDay

--- src/app/features/project/migrate-projects-state.util.ts
+++ src/app/features/project/migrate-projects-state.util.ts
@@ -3,11 +3,14 @@ import {Dictionary} from '@ngrx/entity';
 import {Project} from './project.model';
 import {DEFAULT_PROJECT} from './project.const';
 import {DEFAULT_ISSUE_PROVIDER_CFGS} from '../issue/issue.const';
+import {getWorklogStr} from '../../util/get-work-log-str';
+import {getYesterdaysDate} from '../../util/get-yesterdays-date';
 
 export const migrateProjectState = (projectState: ProjectState): ProjectState => {
   const projectEntities: Dictionary<Project> = {...projectState.entities};
   Object.keys(projectEntities).forEach((key) => {
     projectEntities[key] = _extendProjectDefaults(projectEntities[key]);
+    projectEntities[key] = _addFirstEntryForDayCompleted(projectEntities[key]);
   });
   return {...projectState, entities: projectEntities};
 };
@@ -23,4 +26,13 @@ const _extendProjectDefaults = (project: Project): Project => {
       ...project.issueIntegrationCfgs,
     }
   };
+};
+
+const _addFirstEntryForDayCompleted = (project: Project): Project => {
+  return (project.hasOwnProperty('lastCompletedDay'))
+    ? project
+    : {
+      ...project,
+      lastCompletedDay: getWorklogStr(getYesterdaysDate())
+    };
 };
