a34ca9713c0506711a18a3f55f02bafad3e65455
Tobias Knöppler <tobias.knoeppler@ottogroup.com>
2019-07-04 12:02:15 +0200

    fix: Use sum over task.timeSpentOnDay instead of task.timeSpent, because the latter is incorrect (intentionally?)

--- src/app/features/project/project.model.js
+++ src/app/features/project/project.model.js
@@ -1,3 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var WorklogGrouping;
+(function (WorklogGrouping) {
+    WorklogGrouping["DATE"] = "DATE";
+    WorklogGrouping["PARENT"] = "PARENT";
+    WorklogGrouping["TASK"] = "TASK";
+    WorklogGrouping["WORKLOG"] = "WORKLOG";
+})(WorklogGrouping = exports.WorklogGrouping || (exports.WorklogGrouping = {}));
 //# sourceMappingURL=project.model.js.map
