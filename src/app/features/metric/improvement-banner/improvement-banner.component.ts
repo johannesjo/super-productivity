ebfc4aca0eb22607edb0c2bf8a85185ececc5ce4
Johannes Millan <johannes.millan@gmail.com>
2019-09-30 20:41:52 +0200

    feat: add little animation to banner

--- src/app/features/metric/improvement-banner/improvement-banner.component.ts
+++ src/app/features/metric/improvement-banner/improvement-banner.component.ts
@@ -1,18 +1,30 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
 import {Improvement} from '../improvement/improvement.model';
 import {ImprovementService} from '../improvement/improvement.service';
 import {getWorklogStr} from '../../../util/get-work-log-str';
+import {improvementBannerAnimation} from './improvement-banner.ani';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'improvement-banner',
   templateUrl: './improvement-banner.component.html',
   styleUrls: ['./improvement-banner.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [improvementBannerAnimation]
 })
-export class ImprovementBannerComponent {
+export class ImprovementBannerComponent implements OnDestroy {
+  improvements: Improvement[];
+
+  private _subs = new Subscription();
+
   constructor(
     public improvementService: ImprovementService,
   ) {
+    this._subs.add(this.improvementService.lastTrackedImprovementsTomorrow$.subscribe(val => this.improvements = val));
+  }
+
+  ngOnDestroy(): void {
+    this._subs.unsubscribe();
   }
 
   dismiss(improvement: Improvement) {
