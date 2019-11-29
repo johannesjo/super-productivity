fe89390d257fd1ea352092bde4401a378e9a07a8
Johannes Millan <johannes.millan@gmail.com>
2019-11-29 19:04:24 +0100

    fix: lockscreen not working for plasma desktops #259

--- electron/lockscreen.ts
+++ electron/lockscreen.ts
@@ -4,7 +4,7 @@ export default (cb?, customCommands?) => {
   const lockCommands = customCommands || {
     darwin: '/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend',
     win32: 'rundll32.exe user32.dll, LockWorkStation',
-    linux: '(hash gnome-screensaver-command 2>/dev/null && gnome-screensaver-command -l) || (hash dm-tool 2>/dev/null && dm-tool lock)'
+    linux: '(hash gnome-screensaver-command 2>/dev/null && gnome-screensaver-command -l) || (hash dm-tool 2>/dev/null && dm-tool lock) || (qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock)'
   };
 
   if (Object.keys(lockCommands).indexOf(process.platform) === -1) {
