2cb7df3d1f5f5ef09df9c17f1dc3d7fdaa1b633d
Johannes Millan <johannes.millan@gmail.com>
2019-11-29 23:00:40 +0100

    chore: add notarize debugging again

--- build/scripts/notarize.js
+++ build/scripts/notarize.js
@@ -16,15 +16,15 @@ exports.default = async function notarizing(context) {
   }
 
   try {
-    // let envBefore = process.env.DEBUG;
-    // process.env.DEBUG = 'electron-notarize';
+    let envBefore = process.env.DEBUG;
+    process.env.DEBUG = 'electron-notarize';
     await notarize({
       appBundleId,
       appPath,
       appleId: process.env.APPLEID,
       appleIdPassword: process.env.APPLEIDPASS,
     });
-    // process.env.DEBUG = envBefore;
+    process.env.DEBUG = envBefore;
   } catch (e) {
     console.error(e);
     throw new Error(e);
