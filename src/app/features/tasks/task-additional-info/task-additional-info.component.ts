ec9335f95b09922b5c20bb1422611f8268ba330d
Johannes Millan <johannes.millan@gmail.com>
2019-11-30 20:23:55 +0100

    feat(taskSidebar): make sub tasks collapsable

--- src/app/features/tasks/task-additional-info/task-additional-info.component.ts
+++ src/app/features/tasks/task-additional-info/task-additional-info.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, ComponentFactoryResolver, EventEmitter, Input, Output} from '@angular/core';
-import {TaskWithSubTasks} from '../task.model';
+import {TaskWithSubTasks, ShowSubTasksMode} from '../task.model';
 import {IssueService} from '../../issue/issue.service';
 import {AttachmentService} from '../../attachment/attachment.service';
 import {BehaviorSubject, Observable} from 'rxjs';
@@ -7,17 +7,23 @@ import {Attachment} from '../../attachment/attachment.model';
 import {switchMap} from 'rxjs/operators';
 import {T} from '../../../t.const';
 import {TaskService} from '../task.service';
+import {expandAnimation} from '../../../ui/animations/expand.ani';
+import {fadeAnimation} from '../../../ui/animations/fade.ani';
+import {swirlAnimation} from '../../../ui/animations/swirl-in-out.ani';
 
 @Component({
   selector: 'task-additional-info',
   templateUrl: './task-additional-info.component.html',
   styleUrls: ['./task-additional-info.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [expandAnimation, fadeAnimation, swirlAnimation]
 })
 export class TaskAdditionalInfoComponent {
   T = T;
   issueAttachments: Attachment[];
   taskData: TaskWithSubTasks;
+  ShowSubTasksMode = ShowSubTasksMode;
+
   @Input() selectedIndex = 0;
   @Output() taskNotesChanged: EventEmitter<string> = new EventEmitter();
   @Output() tabIndexChange: EventEmitter<number> = new EventEmitter();
@@ -51,5 +57,10 @@ export class TaskAdditionalInfoComponent {
 
   close() {
     this._taskService.setSelectedId(null);
+  }
+
+
+  toggleSubTaskMode() {
+    this._taskService.toggleSubTaskMode(this.task.id, true, true);
   }
 }
