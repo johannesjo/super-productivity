633fe5f48fdb202d98de7fc48e4155df199555c0
Johannes Millan <johannes.millan@gmail.com>
2019-12-29 19:20:41 +0100

    fix: resize observer throwing error when not available #269

--- src/app/util/resize-observer-obs.ts
+++ src/app/util/resize-observer-obs.ts
@@ -4,25 +4,35 @@ import {Observable} from 'rxjs';
 export const observeResize = (target: HTMLElement): Observable<any[]> => {
   return new Observable((observer) => {
     // tslint:disable-next-line
-    const resizeObserver = new window['ResizeObserver']((entries) => {
-      observer.next(entries);
-    });
-    resizeObserver.observe(target);
-    return () => {
-      resizeObserver.unobserve(target);
-    };
+    if (window['ResizeObserver']) {
+      // tslint:disable-next-line
+      const resizeObserver = new window['ResizeObserver']((entries) => {
+        observer.next(entries);
+      });
+      resizeObserver.observe(target);
+      return () => {
+        resizeObserver.unobserve(target);
+      };
+    } else {
+      console.warn('ResizeObserver not supported in this browser');
+    }
   });
 };
 
 export const observeWidth = (target: HTMLElement): Observable<number> => {
   return new Observable((observer) => {
     // tslint:disable-next-line
-    const resizeObserver = new window['ResizeObserver']((entries) => {
-      observer.next(entries[0].contentRect.width);
-    });
-    resizeObserver.observe(target);
-    return () => {
-      resizeObserver.unobserve(target);
-    };
+    if (window['ResizeObserver']) {
+      // tslint:disable-next-line
+      const resizeObserver = new window['ResizeObserver']((entries) => {
+        observer.next(entries[0].contentRect.width);
+      });
+      resizeObserver.observe(target);
+      return () => {
+        resizeObserver.unobserve(target);
+      };
+    } else {
+      console.warn('ResizeObserver not supported in this browser');
+    }
   });
 };
