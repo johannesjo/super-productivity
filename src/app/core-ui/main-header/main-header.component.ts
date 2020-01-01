e344d8b8e4c43eb89e472734fca57194f2abbff6
mostafa.amen07 <mostafa.amen07@gmail.com>
2020-01-01 14:13:51 +0200

    Fixed main header rtl style

--- src/app/core-ui/main-header/main-header.component.ts
+++ src/app/core-ui/main-header/main-header.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer2, ViewChild, OnDestroy} from '@angular/core';
 import {ProjectService} from '../../features/project/project.service';
 import {LayoutService} from '../layout/layout.service';
 import {BookmarkService} from '../../features/bookmark/bookmark.service';
@@ -8,7 +8,8 @@ import {T} from '../../t.const';
 import {fadeAnimation} from '../../ui/animations/fade.ani';
 import {NavigationStart, Router} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
-import {combineLatest, Observable} from 'rxjs';
+import {combineLatest, Observable, Subscription} from 'rxjs';
+import { LanguageService } from 'src/app/core/language/language.service';
 
 @Component({
   selector: 'main-header',
@@ -17,7 +18,7 @@ import {combineLatest, Observable} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [fadeAnimation]
 })
-export class MainHeaderComponent implements OnInit {
+export class MainHeaderComponent implements OnInit, OnDestroy {
   T = T;
   progressCircleRadius = 10;
   circumference = this.progressCircleRadius * Math.PI * 2;
@@ -35,6 +36,9 @@ export class MainHeaderComponent implements OnInit {
     })
   );
 
+  private _subs = new Subscription();
+  isRTL: boolean;
+
   constructor(
     public readonly projectService: ProjectService,
     public readonly bookmarkService: BookmarkService,
@@ -43,6 +47,7 @@ export class MainHeaderComponent implements OnInit {
     public readonly layoutService: LayoutService,
     private readonly _router: Router,
     private readonly _renderer: Renderer2,
+    private _languageService: LanguageService,
   ) {
   }
 
@@ -55,5 +60,13 @@ export class MainHeaderComponent implements OnInit {
       const dashOffset = this.circumference * -1 * progress;
       this._renderer.setStyle(this.circleSvg.nativeElement, 'stroke-dashoffset', dashOffset);
     });
+
+    this._subs.add( this._languageService.isLangRTL.subscribe( (val) => {
+      this.isRTL = val;
+    }));
+  }
+
+  ngOnDestroy() {
+    this._subs.unsubscribe();
   }
 }
