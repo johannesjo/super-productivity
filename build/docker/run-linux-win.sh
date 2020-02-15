efd27e52d0a99864a45d92df2fb1f3071856d8d9
Johannes Millan <johannes.millan@gmail.com>
2020-02-16 00:33:07 +0100

    chore: fix build

--- build/docker/run-linux-win.sh
+++ build/docker/run-linux-win.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 PWD2=$PWD
+# read as arg
+PUB=$1
 
 cd "$(dirname "$0")"
+
+echo PUB $PUB
 
 docker run $ENVS --rm \
         $(env | \
@@ -14,4 +18,4 @@ docker run $ENVS --rm \
         -v ~/.cache/electron:/root/.cache/electron \
         -v ~/.cache/electron-builder:/root/.cache/electron-builder \
         $(docker image build -q .) \
-        /bin/bash -c "echo '____DOCKER_INNER_START____' && node -v && npm -v && yarn -v && ls -l && yarn --link-duplicates --pure-lockfile && yarn run dist:linuxAndWin -p ${PUB} && ls -l ./dist"
+        /bin/bash -c "echo '____DOCKER_INNER_START____' && echo $PUB && node -v && npm -v && yarn -v && ls -l && yarn --link-duplicates --pure-lockfile && yarn run dist:linuxAndWin -p ${PUB} && ls -l ./dist"
