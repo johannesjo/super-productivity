c4258f9aed26cff65f535efbd8d398348dcbc60b
Johannes Millan <johannes.millan@gmail.com>
2020-02-25 16:49:38 +0100

    feat(jiraNew): deactivate service worker for electron again

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
   // TODO make asset caching work for electron
-  if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator && environment.production && !IS_ELECTRON) {
     navigator.serviceWorker.register('ngsw-worker.js');
-    // navigator.serviceWorker.register('issue.worker.js');
   }
 }).catch(err => console.log(err));
 
