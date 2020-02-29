ede2bb851ff517f41ea29b9759de0b7f3c440926
Johannes Millan <johannes.millan@gmail.com>
2020-02-29 14:39:06 +0100

    feat(jraNew): make cache work with ls

--- src/app/features/issue/cache/issue-cache.service.ts
+++ src/app/features/issue/cache/issue-cache.service.ts
@@ -2,27 +2,22 @@ import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {getCacheId} from './get-cache-id';
 import {tap} from 'rxjs/operators';
+import {loadFromLs, saveToLs} from '../../../core/persistence/local-storage';
 
 export interface CacheItem {
   r: any;
   e: number;
 }
 
-export interface CompleteCache {
-  [key: string]: CacheItem;
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class IssueCacheService {
-  dummyCache: CompleteCache = {};
-
   cache(url: string, requestInit: RequestInit, orgMethod: any, orgArguments: any[], minAlive = 25000): Observable<any> {
     const cacheId = getCacheId(requestInit, url);
 
     if (this._isUseCache(cacheId) && requestInit.method === 'GET') {
-      return of(this._loadFromCache(cacheId));
+      return of(this._loadResponseFromCache(cacheId));
     } else {
       return orgMethod(...orgArguments).pipe(
         tap((res) => {
@@ -33,25 +28,25 @@ export class IssueCacheService {
   }
 
   private _saveToCache(cacheId: string, response: any, minAlive: number) {
-    console.log('SAVING CACHE', cacheId);
     const item = {
       e: minAlive ? (Date.now() + minAlive) : null,
       r: response,
     };
-    this.dummyCache[cacheId] = item;
+    saveToLs(cacheId, item);
   }
 
-  private _loadFromCache(cacheId: string) {
-    console.log('LOAD CACHE');
-    return this.dummyCache[cacheId].r;
+  private _loadResponseFromCache(cacheId: string) {
+    const cacheItem = loadFromLs(cacheId);
+    return cacheItem && cacheItem.r;
+  }
+
+  private _loadResponseItemFromCache(cacheId: string): CacheItem {
+    return loadFromLs(cacheId);
   }
 
   private _isUseCache(cacheId: string) {
-    // return false;
-    // console.log(this.dummyCache[cacheId], Date.now(), !navigator.onLine);
-
-    // return environment.production && this.dummyCache[cacheId] && (this.dummyCache[cacheId].e > Date.now() || !navigator.onLine);
-    return this.dummyCache[cacheId] && (this.dummyCache[cacheId].e > Date.now() || !navigator.onLine);
+    const item = this._loadResponseItemFromCache(cacheId);
+    return item && (item.e > Date.now() || !navigator.onLine);
   }
 
 }
