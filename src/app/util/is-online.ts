7277a3582a4463fd90d7054460cbd3a2b0db469a
Johannes Millan <johannes.millan@gmail.com>
2020-03-02 15:01:30 +0100

    feat(jraNew): share isOnline$

--- src/app/util/is-online.ts
+++ src/app/util/is-online.ts
@@ -1,10 +1,12 @@
 import {fromEvent, merge, of} from 'rxjs';
-import {mapTo} from 'rxjs/operators';
+import {mapTo, shareReplay} from 'rxjs/operators';
 
 export const isOnline = () => navigator.onLine !== false;
 
 export const isOnline$ = merge(
   fromEvent(window, 'offline').pipe(mapTo(false)),
   fromEvent(window, 'online').pipe(mapTo(true)),
-  of(navigator.onLine)
+  of(navigator.onLine),
+).pipe(
+  shareReplay(1),
 );
