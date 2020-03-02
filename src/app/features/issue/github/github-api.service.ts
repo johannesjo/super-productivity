08632320f2884c0a734cf1075775038a04d39118
Johannes Millan <johannes.millan@gmail.com>
2020-03-02 15:59:39 +0100

    feat(githubNew): use real search loading the complete issues for the repo

--- src/app/features/issue/github/github-api.service.ts
+++ src/app/features/issue/github/github-api.service.ts
@@ -4,9 +4,9 @@ import {GithubCfg} from './github';
 import {SnackService} from '../../../core/snack/snack.service';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {GITHUB_API_BASE_URL} from './github.const';
-import {combineLatest, Observable, ObservableInput, throwError} from 'rxjs';
-import {GithubOriginalComment, GithubOriginalIssue} from './github-api-responses';
-import {catchError, map, share, switchMap, take} from 'rxjs/operators';
+import {Observable, ObservableInput, of, throwError} from 'rxjs';
+import {GithubIssueSearchResult} from './github-api-responses';
+import {catchError, map, switchMap} from 'rxjs/operators';
 import {mapGithubIssue, mapGithubIssueToSearchResult} from './github-issue/github-issue-map.util';
 import {GithubComment, GithubIssue} from './github-issue/github-issue.model';
 import {SearchResultItem} from '../issue.model';
@@ -44,116 +44,46 @@ export class GithubApiService {
     }
   }
 
-  getById$(id: number): Observable<GithubIssue> {
+  getById$(issueId: number, isGetComments = true): Observable<GithubIssue> {
     this._checkSettings();
-
-    return this.getCompleteIssueDataForRepo$()
-      .pipe(switchMap(issues => issues.filter(issue => issue.id === id)));
-  }
-
-  getCompleteIssueDataForRepo$(repo = this._cfg.repo, isSkipCheck = false, isForceRefresh = false): Observable<GithubIssue[]> {
-    if (!isSkipCheck) {
-      this._checkSettings();
-    }
-    return combineLatest([
-      this._getAllIssuesForRepo$(repo, isSkipCheck),
-      this._getAllCommentsForRepo$(repo, isSkipCheck),
-    ]).pipe(
-      take(1),
-      map(([issues, comments]) => this._mergeIssuesAndComments(issues, comments)),
-    );
-    // }
-  }
-
-
-  searchIssueForRepo$(searchText: string, repo = this._cfg.repo): Observable<SearchResultItem[]> {
-    const filterFn = issue => {
-      try {
-        return issue.title.toLowerCase().match(searchText.toLowerCase())
-          || issue.body.toLowerCase().match(searchText.toLowerCase());
-      } catch (e) {
-        console.warn('RegEx Error', e);
-        return false;
-      }
-    };
-    this._checkSettings();
-
-    return this.getCompleteIssueDataForRepo$(repo)
+    return this._http.get(`${BASE}repos/${this._cfg.repo}/issues/${issueId}`)
       .pipe(
-        catchError(this._handleRequestError$.bind(this)),
-        // a single request should suffice
-        share(),
-        map((issues: GithubIssue[]) =>
-          issues.filter(filterFn)
-            .map(mapGithubIssueToSearchResult)
+        switchMap(issue => isGetComments
+          ? this.getCommentListForIssue$(issueId).pipe(
+            map(comments => ({...issue, comments}))
+          )
+          : of(issue),
         ),
+        catchError(this._handleRequestError$.bind(this)),
       );
   }
 
-  refreshIssuesCacheIfOld(): void {
-    this.getCompleteIssueDataForRepo$().subscribe();
-  }
-
-  getIssueWithCommentsByIssueNumber$(issueNumber: number): Observable<GithubIssue> {
+  getCommentListForIssue$(issueId: number): Observable<GithubComment[]> {
     this._checkSettings();
-    return combineLatest([
-      this._http.get(`${BASE}repos/${this._cfg.repo}/issues/${issueNumber}`,
-        {headers: this._header ? this._header : {}}),
-      this._http.get(`${BASE}repos/${this._cfg.repo}/issues/${issueNumber}/comments`,
-        {headers: this._header ? this._header : {}}),
-    ]).pipe(
-      catchError(this._handleRequestError$.bind(this)),
-      map(([issue, comments]: [GithubOriginalIssue, GithubOriginalComment[]]) => {
-        return {
-          ...mapGithubIssue(issue),
-          comments,
-        };
-      }),
-    );
-  }
-
-  private _getAllIssuesForRepo$(repo = this._cfg.repo, isSkipCheck = false): Observable<GithubIssue[]> {
-    if (!isSkipCheck) {
-      this._checkSettings();
-    }
-    return this._http.get(
-      `${BASE}repos/${repo}/issues?per_page=100`,
-      {headers: this._header ? this._header : {}}
-    ).pipe(
-      catchError(this._handleRequestError$.bind(this)),
-      map((issues: GithubOriginalIssue[]) => issues ? issues.map(mapGithubIssue) : []),
-    );
-  }
-
-  private _getAllCommentsForRepo$(repo = this._cfg.repo, isSkipCheck = false): Observable<GithubComment[]> {
-    if (!isSkipCheck) {
-      this._checkSettings();
-    }
-    return combineLatest([
-      // the last 500 should hopefully be enough
-      this._getCommentsPageForRepo$(1, repo, isSkipCheck),
-      this._getCommentsPageForRepo$(2, repo, isSkipCheck),
-      this._getCommentsPageForRepo$(3, repo, isSkipCheck),
-      this._getCommentsPageForRepo$(4, repo, isSkipCheck),
-      this._getCommentsPageForRepo$(5, repo, isSkipCheck),
-    ])
+    return this._http.get(`${BASE}repos/${this._cfg.repo}/issues/${issueId}/comments`)
       .pipe(
         catchError(this._handleRequestError$.bind(this)),
-        map(([p1, p2, p3, p4, p5]) => [].concat(p1, p2, p3, p4, p5))
       );
   }
 
-  private _getCommentsPageForRepo$(page = 1, repo = this._cfg.repo, isSkipCheck = false): Observable<GithubComment[]> {
-    if (!isSkipCheck) {
-      this._checkSettings();
-    }
-    return this._http.get(
-      `${BASE}repos/${repo}/issues/comments?sort=created&direction=desc&per_page=100&page=${page}`,
-      {headers: this._header ? this._header : {}}
-    ).pipe(
-      catchError(this._handleRequestError$.bind(this)),
-      map(res => res as GithubComment[])
-    );
+
+  searchIssueForRepo$(searchText: string, isSearchAllGithub = false): Observable<SearchResultItem[]> {
+    this._checkSettings();
+    const repoQuery = isSearchAllGithub
+      ? '' :
+      `+repo:${this._cfg.repo}`;
+
+    return this._http.get(`${BASE}search/issues?q=${encodeURI(searchText + repoQuery)}`)
+      .pipe(
+        catchError(this._handleRequestError$.bind(this)),
+        map((res: GithubIssueSearchResult) => {
+          if (res && res.items) {
+            return res.items.map(mapGithubIssue).map(mapGithubIssueToSearchResult);
+          } else {
+            return [];
+          }
+        }),
+      );
   }
 
   private _checkSettings() {
@@ -194,46 +124,8 @@ export class GithubApiService {
     return throwError({[HANDLED_ERROR_PROP_STR]: 'Github: Api request failed.'});
   }
 
-  private _mergeIssuesAndComments(issues: GithubIssue[], comments: GithubComment[]): GithubIssue[] {
-    return issues.map(issue => {
-      return {
-        ...issue,
-        comments: comments.filter(comment => {
-          return (comment.issue_url === issue.apiUrl);
-        }),
-      };
-    });
-  }
-
   private _isValidSettings(): boolean {
     const cfg = this._cfg;
     return cfg && cfg.repo && cfg.repo.length > 0;
   }
 }
-
-//
-// searchIssue(searchText: string): Observable<SearchResultItem[]> {
-//   this._checkSettings();
-// return this._http.get(`${BASE}search/issues?q=${encodeURI(searchText)}`)
-//   .pipe(
-//     catchError(this._handleRequestError.bind(this)),
-//     map((res: GithubIssueSearchResult) => {
-//       if (res && res.items) {
-//         return res.items.map(mapGithubIssue).map(mapGithubIssueToSearchResult);
-//       } else {
-//         return [];
-//       }
-//     }),
-//   );
-// }
-//
-//
-
-//
-// getCommentsForIssue(issueId: number): Observable<any> {
-//   this._checkSettings();
-// return this._http.get(BASE + `${BASE}repos/${this._cfg.repo}/issues/${issueId}/comments`)
-//   .pipe(
-//     catchError(this._handleRequestError.bind(this)),
-//   );
-// }
