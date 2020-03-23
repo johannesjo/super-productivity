191367f5fb3cdcd65eb2f3f31adcec97773ddaa8
Johannes Millan <johannes.millan@gmail.com>
2020-03-23 21:54:40 +0100

    feat(sharedTaskListGitlab): make update polling work for gitlab

--- src/app/features/issue/providers/gitlab/gitlab-issue/gitlab-issue.effects.ts
+++ src/app/features/issue/providers/gitlab/gitlab-issue/gitlab-issue.effects.ts
@@ -7,14 +7,16 @@ import {GlobalConfigService} from '../../../../config/global-config.service';
 import {SnackService} from '../../../../../core/snack/snack.service';
 import {TaskService} from '../../../../tasks/task.service';
 import {ProjectService} from '../../../../project/project.service';
-import {filter, first, switchMap, takeUntil, tap} from 'rxjs/operators';
-import {Observable, timer} from 'rxjs';
+import {filter, first, map, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {forkJoin, Observable, timer} from 'rxjs';
 import {GITLAB_INITIAL_POLL_DELAY, GITLAB_POLL_INTERVAL} from '../gitlab.const';
 import {IssueService} from '../../../issue.service';
 import {GITLAB_TYPE} from '../../../issue.const';
 import {IssueEffectHelperService} from '../../../issue-effect-helper.service';
 import {GitlabCfg} from '../gitlab';
 import {T} from 'src/app/t.const';
+import {TaskWithSubTasks} from '../../../../tasks/task.model';
+import {WorkContextService} from '../../../../work-context/work-context.service';
 
 const isGitlabEnabled = (gitlabCfg: GitlabCfg) => gitlabCfg && gitlabCfg.project && gitlabCfg.project.length > 2;
 
@@ -24,71 +26,56 @@ export class GitlabIssueEffects {
   pollNewIssuesToBacklog$: any = this._issueEffectHelperService.pollToBacklogTriggerToProjectId$.pipe(
     switchMap((pId) => this._projectService.getGitlabCfgForProject$(pId).pipe(
       first(),
-      filter(githubCfg => isGitlabEnabled(githubCfg) && githubCfg.isAutoAddToBacklog),
-      switchMap(githubCfg => this._pollTimer$.pipe(
+      filter(gitlabCfg => isGitlabEnabled(gitlabCfg) && gitlabCfg.isAutoAddToBacklog),
+      switchMap(gitlabCfg => this._pollTimer$.pipe(
         // NOTE: required otherwise timer stays alive for filtered actions
         takeUntil(this._issueEffectHelperService.pollToBacklogActions$),
         tap(() => console.log('GITLAB!_POLL_BACKLOG_CHANGES')),
-        tap(() => this._importNewIssuesToBacklog(pId, githubCfg)),
+        tap(() => this._importNewIssuesToBacklog(pId, gitlabCfg)),
       )),
     )),
   );
 
 
-  // @Effect({dispatch: false})
-  // pollNewIssuesToBacklog$: any = this._actions$
-  //   .pipe(
-  //     ofType(
-  //       ProjectActionTypes.LoadProjectRelatedDataSuccess,
-  //       ProjectActionTypes.UpdateProjectIssueProviderCfg,
-  //     ),
-  //     withLatestFrom(
-  //       this._projectService.isGitlabEnabled$,
-  //       this._projectService.currentGitlabCfg$,
-  //     ),
-  //     switchMap(([a, isEnabled, gitlabCfg]) => {
-  //       return (isEnabled && gitlabCfg.isAutoAddToBacklog)
-  //         ? timer(GITLAB_INITIAL_POLL_DELAY, GITLAB_POLL_INTERVAL).pipe(
-  //           // tap(() => console.log('GITLAB_POLL_BACKLOG_CHANGES')),
-  //           tap(this._importNewIssuesToBacklog.bind(this))
-  //         )
-  //         : EMPTY;
-  //     }),
-  //   );
-  // private _pollChangesForIssues$: Observable<any> = timer(GITLAB_INITIAL_POLL_DELAY, GITLAB_POLL_INTERVAL).pipe(
-  //   withLatestFrom(
-  //     this._store$.pipe(select(selectGitlabTasks)),
-  //   ),
-  //   tap(([, gitlabTasks]: [number, Task[]]) => {
-  //     if (gitlabTasks && gitlabTasks.length > 0) {
-  //       this._snackService.open({
-  //         msg: T.F.GITLAB.S.POLLING,
-  //         svgIco: 'gitlab',
-  //         isSpinner: true,
-  //       });
-  //       gitlabTasks.forEach((task) => this._issueService.refreshIssue(task, true, false));
-  //     }
-  //   }),
-  // );
-  // @Effect({dispatch: false})
-  // pollIssueChangesAndBacklogUpdates: any = this._actions$
-  //   .pipe(
-  //     ofType(
-  //       // while load state should be enough this just might fix the error of polling for inactive projects?
-  //       ProjectActionTypes.LoadProjectRelatedDataSuccess,
-  //       ProjectActionTypes.UpdateProjectIssueProviderCfg,
-  //     ),
-  //     withLatestFrom(
-  //       this._projectService.isGitlabEnabled$,
-  //       this._projectService.currentGitlabCfg$,
-  //     ),
-  //     switchMap(([a, isEnabled, gitlabCfg]) => {
-  //       return (isEnabled && gitlabCfg.isAutoPoll)
-  //         ? this._pollChangesForIssues$
-  //         : EMPTY;
-  //     })
-  //   );
-  //
+  @Effect({dispatch: false})
+  pollIssueChangesForCurrentContext$: any = this._issueEffectHelperService.pollIssueTaskUpdatesActions$
+    .pipe(
+      switchMap(() => this._pollTimer$),
+      switchMap(() => this._updateIssuesForCurrentContext$),
+    );
+
+  private _updateIssuesForCurrentContext$ = this._workContextService.allTasksForCurrentContext$.pipe(
+    first(),
+    switchMap((tasks) => {
+      const gitIssueTasks = tasks.filter(task => task.issueType === GITLAB_TYPE);
+      return forkJoin(gitIssueTasks.map(task => this._projectService.getGitlabCfgForProject$(task.projectId).pipe(
+        first(),
+        map(cfg => ({
+          cfg,
+          task,
+        }))
+        ))
+      );
+    }),
+    map((cos) => cos
+      .filter(({cfg, task}: { cfg: GitlabCfg, task: TaskWithSubTasks }) =>
+        isGitlabEnabled(cfg) && cfg.isAutoPoll
+      )
+      .map(({task}: { cfg: GitlabCfg, task: TaskWithSubTasks }) => task)
+    ),
+    tap((gitlabTasks: TaskWithSubTasks[]) => gitlabTasks.forEach((gitlabTask) => {
+      if (gitlabTasks && gitlabTasks.length > 0) {
+        this._snackService.open({
+          msg: T.F.GITLAB.S.POLLING,
+          svgIco: 'gitlab',
+          isSpinner: true,
+        });
+        gitlabTasks.forEach((task) =>
+          this._issueService.refreshIssue(task, true, false));
+      }
+    })),
+  );
+
 
   private _pollTimer$: Observable<any> = timer(GITLAB_INITIAL_POLL_DELAY, GITLAB_POLL_INTERVAL);
 
@@ -101,6 +88,7 @@ export class GitlabIssueEffects {
               private readonly _gitlabApiService: GitlabApiService,
               private readonly _issueService: IssueService,
               private readonly _taskService: TaskService,
+              private readonly _workContextService: WorkContextService,
               private readonly _persistenceService: PersistenceService,
               private readonly _issueEffectHelperService: IssueEffectHelperService,
   ) {
