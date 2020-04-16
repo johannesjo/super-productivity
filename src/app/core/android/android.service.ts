ec58ea349e11319c1018dbc1dc0432bc94ff61c7
Johannes Millan <johannes.millan@gmail.com>
2020-04-17 01:43:21 +0200

    feat(android): improve data loading

--- src/app/core/android/android.service.ts
+++ src/app/core/android/android.service.ts
@@ -4,22 +4,25 @@ import {Observable} from 'rxjs';
 import {TagService} from '../../features/tag/tag.service';
 import {TaskService} from '../../features/tasks/task.service';
 import {TODAY_TAG} from '../../features/tag/tag.const';
-import {filter, switchMap} from 'rxjs/operators';
+import {switchMap} from 'rxjs/operators';
 import {androidInterface} from './android-interface';
+import {DataInitService} from '../data-init/data-init.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AndroidService {
-  private _todayTagTasksFlat$: Observable<Task[]> = this._tagService.getTagById$(TODAY_TAG.id).pipe(
-    filter(tag => !!tag),
+  private _todayTagTasksFlat$: Observable<Task[]> = this._dataInitService.isAllDataLoadedInitially$.pipe(
+    switchMap(() => this._tagService.getTagById$(TODAY_TAG.id)),
     switchMap(tag => this._taskService.getByIdsLive$(tag.taskIds))
   );
 
   constructor(
+    private _dataInitService: DataInitService,
     private _tagService: TagService,
     private _taskService: TaskService,
   ) {
+    // this._todayTagTasksFlat$.subscribe((v) => console.log('_todayTagTasksFlat$', v));
   }
 
   init() {
