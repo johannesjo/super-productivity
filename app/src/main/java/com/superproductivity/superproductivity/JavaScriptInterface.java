31c21dbb51b79e7b0fb72a2bcd80614c986f3c68
Johannes Millan <johannes.millan@gmail.com>
2020-04-17 02:16:54 +0200

    feat: make updating text work

--- app/src/main/java/com/superproductivity/superproductivity/JavaScriptInterface.java
+++ app/src/main/java/com/superproductivity/superproductivity/JavaScriptInterface.java
@@ -1,7 +1,10 @@
 package com.superproductivity.superproductivity;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
@@ -26,8 +29,27 @@ public class JavaScriptInterface {
     @JavascriptInterface
     public void updateTaskData(String str) {
         Intent intent = new Intent(TaskListWidget.LIST_CHANGED);
-        intent.putExtra("taskJson", str);
+//        intent.putExtra("taskJson", str);
         Log.v("jsInterface", str);
-        mContext.getApplicationContext().sendBroadcast(intent);
+//        mContext.getApplicationContext().sendBroadcast(intent);
+        updateWidgets(str);
+    }
+
+
+    private void updateWidgets(String s) {
+        Intent intent = new Intent(mContext.getApplicationContext(), TaskListWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+        // since it seems the onUpdate() is only fired on that:
+        AppWidgetManager widgetManager = AppWidgetManager.getInstance(mContext);
+        int[] ids = widgetManager.getAppWidgetIds(new ComponentName(mContext, TaskListWidget.class));
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        widgetManager.notifyAppWidgetViewDataChanged(ids, android.R.id.list);
+
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        intent.putExtra("taskJson", s);
+
+        mContext.sendBroadcast(intent);
     }
 }
--- app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
+++ app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
@@ -2,6 +2,7 @@ package com.superproductivity.superproductivity;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -17,35 +18,42 @@ import androidx.annotation.NonNull;
 public class TaskListWidget extends AppWidgetProvider {
     public static final String LIST_CHANGED = "com.superproductivity.superproductivity.LIST_CHANGED";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.task_list_widget);
-        setRemoteAdapter(context, views);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 
     private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
-        views.setRemoteAdapter(R.id.widget_list, new Intent(context, TaskListWidgetService.class));
+//        views.setRemoteAdapter(R.id.widget_list, new Intent(context, TaskListWidgetService.class));
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        Log.v("XXXXXXXXXXXXXXXXXX", "yeeees");
-        Log.v("XXXXXXXXXXXXXXXXXX", intent.getAction());
-        if (intent.getAction().equals(LIST_CHANGED)) {
-            // handle intent here
-            String s = intent.getStringExtra("taskJson");
-            Log.v("XXXXXXXYY", s);
+
+//        if (intent.getAction().equals(LIST_CHANGED)) {
+//            Log.v("XXXXXXXYY", "DDDD");
+//            // handle intent here
+//            String s = intent.getStringExtra("taskJson");
+//
+//            RemoteViews views = new RemoteViews(context.getPackageName(), R.id.test_text);
+//            views.setTextViewText(R.id.test_text, s);
+//            AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, TaskListWidget.class), views);
+//            Log.v("XXXXXXXYY", s);
+//        }
+
+        String str = intent.getStringExtra("taskJson");
+        if (str != null) {
+            Log.v("XXXXXXXYY", str);
+            Log.v("XXXXXXXYY", str);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.task_list_widget);
+            views.setTextViewText(R.id.test_text, str);
+//        views.setTextViewText(R.id.test_text, "aaaaaa");
+
+            AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, TaskListWidget.class), views);
         }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.v("onUpdate", "ON UPPPPPPPPPPPPPPPP DAAAATE");
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -71,5 +79,17 @@ public class TaskListWidget extends AppWidgetProvider {
         Toast.makeText(context, "onDisabled called", Toast.LENGTH_LONG).show();
         // Enter relevant functionality for when the last widget is disabled
     }
+
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.task_list_widget);
+//        setRemoteAdapter(context, views);
+
+        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
 }
 
