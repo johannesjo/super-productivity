5ab839a9a0b6e87d14784984fa8e299227274baf
Johannes Millan <johannes.millan@gmail.com>
2020-04-17 03:07:04 +0200

    feat: add parse json fn

--- app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
+++ app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
@@ -8,6 +8,12 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -32,6 +38,32 @@ public class TaskListWidget extends AppWidgetProvider {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.task_list_widget);
             views.setTextViewText(R.id.test_text, taskJsonStr);
             AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, TaskListWidget.class), views);
+        }
+    }
+
+    void parseJSONToList(String jsonStr) {
+        try {
+            JSONArray tasks = new JSONArray(jsonStr);
+
+            // looping through All Contacts
+            for (int i = 0; i < tasks.length(); i++) {
+                JSONObject c = tasks.getJSONObject(i);
+
+                String id = c.getString("id");
+                String title = c.getString("title");
+
+                // tmp hash map for single contact
+                HashMap<String, String> task = new HashMap<>();
+
+                // adding each child node to HashMap key => value
+                task.put("id", id);
+                task.put("title", title);
+
+                // adding task to task list
+//                taskList.add(task);
+            }
+        } catch (final JSONException e) {
+            Log.e("Sup Widget", "Json parsing error: " + e.getMessage());
         }
     }
 }
