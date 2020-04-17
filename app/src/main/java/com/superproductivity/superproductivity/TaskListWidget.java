a0b0d0a55788213c1ea632b5c7607a712167efcd
Johannes Millan <johannes.millan@gmail.com>
2020-04-17 04:08:07 +0200

    feat: make parsing task data work

--- app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
+++ app/src/main/java/com/superproductivity/superproductivity/TaskListWidget.java
@@ -6,13 +6,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.RemoteViews;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * Implementation of App Widget functionality.
@@ -20,6 +21,8 @@ import java.util.HashMap;
 public class TaskListWidget extends AppWidgetProvider {
     public static final String LIST_CHANGED = "com.superproductivity.superproductivity.LIST_CHANGED";
     String taskJsonStr;
+    ArrayList<String> taskList;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -36,7 +39,28 @@ public class TaskListWidget extends AppWidgetProvider {
         if (taskJsonStr != null) {
 //            Log.v("TaskListWidget", taskJsonStr);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.task_list_widget);
+//            views.setTextViewText(R.id.test_text, taskJsonStr);
+//            views.setRemoteAdapter(R.id., new Intent(context, TaskListWidgetService.class));
+
+            parseJSONToList(taskJsonStr);
             views.setTextViewText(R.id.test_text, taskJsonStr);
+
+//            views.setTextViewText(R.id.task_list, taskJsonStr);
+
+//
+//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                    android.R.layout.simple_list_item_1, android.R.id.text1, taskList);
+//
+//            // Initialise a listview adapter with the project titles and use it
+//            // in the listview to show the list of project.
+//            mListView = (ListView) findViewById(R.id.list);
+//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                    android.R.layout.simple_list_item_1, android.R.id.text1,
+//                    taskList.toArray(new String[taskList.size()]));
+//
+//            mListView.setAdapter(adapter);
+//            lv = (ListView) findViewById(R.id.list);
+
             AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, TaskListWidget.class), views);
         }
     }
@@ -46,21 +70,11 @@ public class TaskListWidget extends AppWidgetProvider {
             JSONArray tasks = new JSONArray(jsonStr);
 
             // looping through All Contacts
+            taskList = new ArrayList<>();
             for (int i = 0; i < tasks.length(); i++) {
                 JSONObject c = tasks.getJSONObject(i);
-
-                String id = c.getString("id");
                 String title = c.getString("title");
-
-                // tmp hash map for single contact
-                HashMap<String, String> task = new HashMap<>();
-
-                // adding each child node to HashMap key => value
-                task.put("id", id);
-                task.put("title", title);
-
-                // adding task to task list
-//                taskList.add(task);
+                taskList.add(title);
             }
         } catch (final JSONException e) {
             Log.e("Sup Widget", "Json parsing error: " + e.getMessage());
