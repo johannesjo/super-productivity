ae988f8711fb07dbe2eb05e9adb5eb674bda19cd
Johannes Millan <johannes.millan@gmail.com>
2020-04-19 17:07:10 +0200

    fix(android): error outside web view

--- src/app/core/android/android-interface.ts
+++ src/app/core/android/android-interface.ts
@@ -1,3 +1,5 @@
+import {IS_ANDROID_WEB_VIEW} from '../../util/is-android-web-view';
+
 export interface AndroidInterface {
   showToast(s: string): void;
 
@@ -10,16 +12,18 @@ export interface AndroidInterface {
 
 export const androidInterface: AndroidInterface = (window as any).SUPAndroid;
 
-androidInterface.getGoogleToken = () => {
-  androidInterface.triggerGetGoogleToken();
+if (IS_ANDROID_WEB_VIEW) {
+  androidInterface.getGoogleToken = () => {
+    androidInterface.triggerGetGoogleToken();
 
-  // TODO add map similar to jira api
-  return new Promise<string>((resolve, reject) => {
-    (window as any).googleGetTokenSuccessCallback = (token: string) => {
-      resolve(token);
-    };
-    (window as any).googleGetTokenErrorCallback = (token: string) => {
-      reject(token);
-    };
-  });
-};
+    // TODO add map similar to jira api
+    return new Promise<string>((resolve, reject) => {
+      (window as any).googleGetTokenSuccessCallback = (token: string) => {
+        resolve(token);
+      };
+      (window as any).googleGetTokenErrorCallback = (token: string) => {
+        reject(token);
+      };
+    });
+  };
+}
