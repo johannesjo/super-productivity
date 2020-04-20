98abd1086711c7c2e7fdc178146c056e27a0b0af
Johannes Millan <johannes.millan@gmail.com>
2020-04-20 23:52:04 +0200

    feat: sort done tasks last for android widget tasks

--- src/app/core/android/android.service.ts
+++ src/app/core/android/android.service.ts
@@ -4,7 +4,7 @@ import {Observable} from 'rxjs';
 import {TagService} from '../../features/tag/tag.service';
 import {TaskService} from '../../features/tasks/task.service';
 import {TODAY_TAG} from '../../features/tag/tag.const';
-import {switchMap} from 'rxjs/operators';
+import {map, switchMap} from 'rxjs/operators';
 import {androidInterface} from './android-interface';
 import {DataInitService} from '../data-init/data-init.service';
 
@@ -14,7 +14,11 @@ import {DataInitService} from '../data-init/data-init.service';
 export class AndroidService {
   private _todayTagTasksFlat$: Observable<Task[]> = this._dataInitService.isAllDataLoadedInitially$.pipe(
     switchMap(() => this._tagService.getTagById$(TODAY_TAG.id)),
-    switchMap(tag => this._taskService.getByIdsLive$(tag.taskIds))
+    switchMap(tag => this._taskService.getByIdsLive$(tag.taskIds)),
+    map(tasks => tasks && tasks.sort((a, b) => (a.isDone === b.isDone)
+      ? 0
+      : (a.isDone ? 1 : -1)
+    )),
   );
 
   constructor(
@@ -23,6 +27,7 @@ export class AndroidService {
     private _taskService: TaskService,
   ) {
     // this._todayTagTasksFlat$.subscribe((v) => console.log('_todayTagTasksFlat$', v));
+    // this._todayTagTasksFlat$.subscribe((tasks) => console.log(tasks.map((value, index) => value.isDone)));
   }
 
   init() {
