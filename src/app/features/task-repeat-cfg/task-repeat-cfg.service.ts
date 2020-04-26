0c9e79165bbadedf798d3fd879d7399ba099c629
Johannes Millan <johannes.millan@gmail.com>
2020-04-26 16:28:01 +0200

    fix: lint

--- src/app/features/task-repeat-cfg/task-repeat-cfg.service.ts
+++ src/app/features/task-repeat-cfg/task-repeat-cfg.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {select, Store} from '@ngrx/store';
-import {selectAllTaskRepeatCfgs, selectTaskRepeatCfgById,} from './store/task-repeat-cfg.reducer';
+import {selectAllTaskRepeatCfgs, selectTaskRepeatCfgById} from './store/task-repeat-cfg.reducer';
 import {
   AddTaskRepeatCfgToTask,
   DeleteTaskRepeatCfg,
