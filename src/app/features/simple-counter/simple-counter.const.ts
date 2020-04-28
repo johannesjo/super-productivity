0b802ef543e5908d22a101188e8b18fdd60875d6
Johannes Millan <johannes.millan@gmail.com>
2020-04-28 15:48:57 +0200

    feat(simpleCounter): outline model

---
+++ src/app/features/simple-counter/simple-counter.const.ts
@@ -0,0 +1,29 @@
+import {SimpleCounter} from './simple-counter.model';
+
+export const EMPTY_SIMPLE_COUNTER: SimpleCounter = {
+  id: undefined,
+
+  // basic cfg
+  title: undefined,
+  isEnabled: false,
+  iconPlay: undefined,
+  iconPause: undefined,
+
+  // adv cfg
+  isStartWhenTrackingTime: false,
+  isPauseWhenTimeTrackingIsPaused: false,
+
+  // dynamic
+  time: 0,
+  totalTimeOnDay: {},
+  isRunning: false,
+};
+
+export const DEFAULT_SIMPLE_COUNTERS: SimpleCounter[] = [
+  {
+    ...EMPTY_SIMPLE_COUNTER,
+    title: 'Standing Desk Timer',
+    iconPause: 'airline_seat_recline_normal',
+    iconPlay: 'emoji_people',
+  }
+];
---
+++ src/app/features/simple-counter/simple-counter.model.ts
@@ -0,0 +1,18 @@
+export interface SimpleCounter {
+  id: string;
+
+  // basic cfg
+  title: string;
+  isEnabled: boolean;
+  iconPlay: string;
+  iconPause: string;
+
+  // adv cfg
+  isStartWhenTrackingTime: boolean;
+  isPauseWhenTimeTrackingIsPaused: boolean;
+
+  // dynamic
+  time: number;
+  totalTimeOnDay: { [key: string]: number };
+  isRunning: boolean;
+}
