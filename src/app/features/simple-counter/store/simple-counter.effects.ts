c05ef3257ae106c64d31cb1b119ec9e4c5b72337
Johannes Millan <johannes.millan@gmail.com>
2020-04-29 00:59:09 +0200

    feat(simpleCounter): make persistence work

--- src/app/features/simple-counter/store/simple-counter.effects.ts
+++ src/app/features/simple-counter/store/simple-counter.effects.ts
@@ -1,26 +1,37 @@
 import {Injectable} from '@angular/core';
-import {Actions} from '@ngrx/effects';
-import {Store} from '@ngrx/store';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {select, Store} from '@ngrx/store';
 import {PersistenceService} from '../../../core/persistence/persistence.service';
+import {
+  addSimpleCounter,
+  deleteSimpleCounter,
+  deleteSimpleCounters,
+  updateAllSimpleCounters,
+  updateSimpleCounter,
+  upsertSimpleCounter
+} from './simple-counter.actions';
+import {tap, withLatestFrom} from 'rxjs/operators';
+import {selectSimpleCounterFeatureState} from './simple-counter.reducer';
+import {SimpleCounterState} from '../simple-counter.model';
 
 
 @Injectable()
 export class SimpleCounterEffects {
 
-  // updateSimpleCountersStorage$ = createEffect(() => this._actions$.pipe(
-  //   ofType(
-  //     simpleCounterActions.addSimpleCounter,
-  //     simpleCounterActions.updateSimpleCounter,
-  //     simpleCounterActions.upsertSimpleCounter,
-  //     simpleCounterActions.deleteSimpleCounter,
-  //     simpleCounterActions.deleteSimpleCounters,
-  //   ),
-  //   withLatestFrom(
-  //     this._store$.pipe(select(selectSimpleCounterFeatureState)),
-  //   ),
-  //   tap(this._saveToLs.bind(this)),
-  //   tap(this._updateLastActive.bind(this)),
-  // ), {dispatch: false});
+  updateSimpleCountersStorage$ = createEffect(() => this._actions$.pipe(
+    ofType(
+      addSimpleCounter,
+      updateSimpleCounter,
+      upsertSimpleCounter,
+      deleteSimpleCounter,
+      deleteSimpleCounters,
+      updateAllSimpleCounters,
+    ),
+    withLatestFrom(
+      this._store$.pipe(select(selectSimpleCounterFeatureState)),
+    ),
+    tap(([, featureState]) => this._saveToLs(featureState)),
+  ), {dispatch: false});
 
 
   constructor(
@@ -30,12 +41,8 @@ export class SimpleCounterEffects {
   ) {
   }
 
-  private _saveToLs([action, currentProjectId, simpleCounterState]) {
-    if (currentProjectId) {
-      this._persistenceService.saveLastActive();
-      this._persistenceService.simpleCounter.saveState(simpleCounterState);
-    } else {
-      throw new Error('No current project id');
-    }
+  private _saveToLs(simpleCounterState: SimpleCounterState) {
+    this._persistenceService.saveLastActive();
+    this._persistenceService.simpleCounter.saveState(simpleCounterState);
   }
 }
