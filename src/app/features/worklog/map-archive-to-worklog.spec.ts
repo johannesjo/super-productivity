d79107c0c7a0c9c8b8ef19155e0422b4d331381c
Johannes Millan <johannes.millan@gmail.com>
2020-05-08 22:36:29 +0200

    test: add first unit test for map archive to worklog on several days

--- src/app/features/worklog/map-archive-to-worklog.spec.ts
+++ src/app/features/worklog/map-archive-to-worklog.spec.ts
@@ -1,8 +1,7 @@
 import {arrayToDictionary} from '../../util/array-to-dictionary';
-import {DEFAULT_TASK, TaskCopy, TaskState} from '../tasks/task.model';
+import {DEFAULT_TASK, Task, TaskCopy, TaskState} from '../tasks/task.model';
 import {mapArchiveToWorklog} from './map-archive-to-worklog';
-import {initialTaskState} from '../tasks/store/task.reducer';
-import {Dictionary} from '@ngrx/entity';
+import {Dictionary, EntityState} from '@ngrx/entity';
 import {Worklog} from './worklog.model';
 
 const START_END_ALL = {
@@ -14,17 +13,16 @@ const START_END_ALL = {
   }
 };
 
-const fakeTaskStateFromArray = (tasks: TaskCopy[]): TaskState => {
+const fakeTaskStateFromArray = (tasks: TaskCopy[]): EntityState<Task> => {
   const dict = arrayToDictionary(tasks) as Dictionary<TaskCopy>;
   return {
-    ...initialTaskState,
     entities: dict,
     ids: Object.keys(dict),
-  } as TaskState;
+  } as EntityState<Task>;
 };
 
 describe('mapArchiveToWorklog', () => {
-  it('should work for simple data', () => {
+  it('should work for single task with multiple days', () => {
     const ts = fakeTaskStateFromArray([
       {
         ...DEFAULT_TASK,
@@ -58,5 +56,64 @@ describe('mapArchiveToWorklog', () => {
     expect(w[2018].ent[2].ent[16].dayStr).toBe('Fri');
     expect(w[2018].ent[2].ent[16].dateStr).toBe('2018-02-16');
     expect(w[2018].ent[2].ent[16].logEntries.length).toBe(1);
+  });
+
+
+  it('should work for parent task with sub tasks on separate days', () => {
+    const ts = fakeTaskStateFromArray([
+      {
+        ...DEFAULT_TASK,
+        title: 'A',
+        id: 'A',
+        subTaskIds: ['SUB_B', 'SUB_C'],
+        timeSpent: 13332,
+        timeSpentOnDay: {
+          '2015-01-15': 9999,
+          '2018-02-16': 3333,
+        }
+      },
+      {
+        ...DEFAULT_TASK,
+        title: 'SUB_B',
+        id: 'SUB_B',
+        parentId: 'A',
+        timeSpentOnDay: {
+          '2015-01-15': 9999,
+        }
+      },
+      {
+        ...DEFAULT_TASK,
+        title: 'SUB_C',
+        id: 'SUB_C',
+        parentId: 'A',
+        timeSpentOnDay: {
+          '2018-02-16': 3333,
+        }
+      },
+    ]);
+
+
+    const r = mapArchiveToWorklog(ts, [], START_END_ALL);
+    const w: Worklog = r.worklog;
+
+    expect(r.totalTimeSpent).toBe(13332);
+
+    expect(w[2015].timeSpent).toBe(9999);
+    expect(w[2015].ent[1].timeSpent).toBe(9999);
+    expect(w[2015].ent[1].daysWorked).toBe(1);
+    expect(w[2015].ent[1].ent[15].timeSpent).toBe(9999);
+    expect(w[2015].ent[1].ent[15].logEntries.length).toBe(2);
+    expect(w[2015].ent[1].ent[15].logEntries[0].task.id).toBe('A');
+    expect(w[2015].ent[1].ent[15].logEntries[1].task.id).toBe('SUB_B');
+    expect(w[2015].ent[1].ent[15].logEntries[1].parentId).toBe('A');
+
+    expect(w[2018].timeSpent).toBe(3333);
+    expect(w[2018].ent[2].timeSpent).toBe(3333);
+    expect(w[2018].ent[2].daysWorked).toBe(1);
+    expect(w[2018].ent[2].ent[16].timeSpent).toBe(3333);
+    expect(w[2018].ent[2].ent[16].logEntries.length).toBe(2);
+    expect(w[2018].ent[2].ent[16].logEntries[0].task.id).toBe('A');
+    expect(w[2018].ent[2].ent[16].logEntries[1].task.id).toBe('SUB_C');
+    expect(w[2018].ent[2].ent[16].logEntries[1].parentId).toBe('A');
   });
 });
