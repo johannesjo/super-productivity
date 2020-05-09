685da5f1d27ca32864a89049aff270c4b4330dd1
Johannes Millan <johannes.millan@gmail.com>
2020-05-09 10:10:45 +0200

    test: add another

--- src/app/features/worklog/util/get-complete-state-for-work-context.util.spec.ts
+++ src/app/features/worklog/util/get-complete-state-for-work-context.util.spec.ts
@@ -28,7 +28,7 @@ const fakeTaskStateFromArray = (tasks: TaskCopy[]): EntityState<Task> => {
 };
 
 describe('getCompleteStateForWorkContext', () => {
-  fit('should include sub tasks for tags', () => {
+  it('should include sub tasks for tags', () => {
     const ts = fakeTaskStateFromArray([
       {
         ...DEFAULT_TASK,
@@ -50,15 +50,23 @@ describe('getCompleteStateForWorkContext', () => {
         parentId: 'PT',
       },
     ]);
+    const archiveS = fakeTaskStateFromArray([]);
 
-    const r = getCompleteStateForWorkContext(TAG_CTX, ts, fakeTaskStateFromArray([]));
-    // expect(r.unarchivedIds).toEqual(['PT', 'SUB_B', 'SUB_C']);
+    const r = getCompleteStateForWorkContext(TAG_CTX, ts, archiveS);
     expect(r.completeStateForWorkContext.ids).toEqual(['PT', 'SUB_B', 'SUB_C']);
   });
 
 
-  xit('should include sub tasks for tags for the archive', () => {
+  it('should include sub tasks for tags for the archive', () => {
     const ts = fakeTaskStateFromArray([
+      {
+        ...DEFAULT_TASK,
+        title: 'PT_TODAY',
+        id: 'PT_TODAY',
+        tagIds: [TAG_ID],
+      },
+    ]);
+    const archiveS = fakeTaskStateFromArray([
       {
         ...DEFAULT_TASK,
         title: 'PT',
@@ -79,8 +87,8 @@ describe('getCompleteStateForWorkContext', () => {
         parentId: 'PT',
       },
     ]);
-    const r = getCompleteStateForWorkContext(TAG_CTX, fakeTaskStateFromArray([]), ts);
-    expect(r.completeStateForWorkContext.ids).toEqual(['A', 'SUB_B', 'SUB_C']);
+    const r = getCompleteStateForWorkContext(TAG_CTX, ts, archiveS);
+    expect(r.completeStateForWorkContext.ids).toEqual(['PT_TODAY', 'PT', 'SUB_B', 'SUB_C']);
   });
 });
 
