75da9d1fe90726ae3789dc306b404e21511929e8
Johannes Millan <johannes.millan@gmail.com>
2020-06-01 00:16:49 +0200

    feat(dbx): add translations and handle errors

--- src/app/features/dropbox/store/dropbox.effects.ts
+++ src/app/features/dropbox/store/dropbox.effects.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {Actions, Effect, ofType} from '@ngrx/effects';
 import {GlobalConfigActionTypes, UpdateGlobalConfigSection} from '../../config/store/global-config.actions';
 import {
+  catchError,
   distinctUntilChanged,
   exhaustMap,
   filter,
   map,
   pairwise,
-  shareReplay, startWith,
+  shareReplay,
+  startWith,
   switchMap,
   tap,
   withLatestFrom
@@ -20,10 +22,11 @@ import {SyncService} from '../../../imex/sync/sync.service';
 import {DROPBOX_MIN_SYNC_INTERVAL} from '../dropbox.const';
 import {SyncProvider} from '../../../imex/sync/sync-provider';
 import {SYNC_INITIAL_SYNC_TRIGGER} from '../../../imex/sync/sync.const';
-import {combineLatest, EMPTY} from 'rxjs';
+import {combineLatest, EMPTY, from} from 'rxjs';
 import {isOnline$} from '../../../util/is-online';
 import {SnackService} from '../../../core/snack/snack.service';
 import {dbxLog} from '../dropbox-log.util';
+import {T} from '../../../t.const';
 
 
 @Injectable()
@@ -43,17 +46,22 @@ export class DropboxEffects {
     // don't run multiple after each other when dialog is open
     exhaustMap(([trigger, isOnline]) => {
       if (!isOnline) {
-        this._snackService.open({msg: 'Unable to sync, because offline', type: 'ERROR'});
+        this._snackService.open({msg: T.F.DROPBOX.S.OFFLINE, type: 'ERROR'});
         if (trigger === SYNC_INITIAL_SYNC_TRIGGER) {
           this._syncService.setInitialSyncDone(true, SyncProvider.Dropbox);
         }
         return;
       }
-      return this._dropboxSyncService.sync().then(() => {
-        if (trigger === SYNC_INITIAL_SYNC_TRIGGER) {
-          this._syncService.setInitialSyncDone(true, SyncProvider.Dropbox);
-        }
-      });
+      return this._dropboxSyncService.sync()
+        .then(() => {
+          if (trigger === SYNC_INITIAL_SYNC_TRIGGER) {
+            this._syncService.setInitialSyncDone(true, SyncProvider.Dropbox);
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+          this._snackService.open({msg: T.F.DROPBOX.S.SYNC_ERROR, type: 'ERROR'});
+        });
     }),
   );
 
@@ -75,8 +83,21 @@ export class DropboxEffects {
     withLatestFrom(this._isChangedAccessCode$),
     filter(([, isChanged]: [UpdateGlobalConfigSection, boolean]) => isChanged),
     switchMap(([{payload}, isChanged]: [UpdateGlobalConfigSection, boolean]) =>
-      this._dropboxApiService.getAccessTokenFromAuthCode((payload.sectionCfg as any).authCode)),
-    tap(() => this._snackService.open({type: 'SUCCESS', msg: 'Access Token successfully generated'})),
+      from(this._dropboxApiService.getAccessTokenFromAuthCode((payload.sectionCfg as any).authCode)).pipe(
+        // NOTE: catch needs to be limited to request only, otherwise we break the chain
+        catchError((e) => {
+          console.error(e);
+          this._snackService.open({type: 'ERROR', msg: T.F.DROPBOX.S.ACCESS_TOKEN_ERROR});
+          // filter
+          return EMPTY;
+        }),
+      )
+    ),
+    tap(() => setTimeout(() => this._snackService.open({
+        type: 'SUCCESS',
+        msg: T.F.DROPBOX.S.ACCESS_TOKEN_GENERATED
+      }), 200)
+    ),
     map((_accessToken: string) => new UpdateGlobalConfigSection({
       sectionKey: 'dropboxSync',
       sectionCfg: {accessToken: _accessToken}
