0290789c0bced8aa061df0aa4b89d927038822a0
Johannes Millan <johannes.millan@gmail.com>
2020-05-31 18:39:48 +0200

    feat(dbx): fix spinner for axios

--- src/app/core-ui/global-progress-bar/global-progress-bar-interceptor.service.ts
+++ src/app/core-ui/global-progress-bar/global-progress-bar-interceptor.service.ts
@@ -3,6 +3,7 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/com
 import {Observable} from 'rxjs';
 import {finalize} from 'rxjs/operators';
 import {GlobalProgressBarService} from './global-progress-bar.service';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,29 @@ export class GlobalProgressBarInterceptorService implements HttpInterceptor {
   constructor(
     private globalProgressBarService: GlobalProgressBarService,
   ) {
+
+    axios.interceptors.request.use((config) => {
+      console.log(config);
+
+      this.globalProgressBarService.countUp();
+      return config;
+    }, (error) => {
+      this.globalProgressBarService.countDown();
+      return Promise.reject(error);
+    });
+
+    axios.interceptors.response.use((response) => {
+      this.globalProgressBarService.countDown();
+      return response;
+    }, (error) => {
+      this.globalProgressBarService.countDown();
+      return Promise.reject(error);
+    });
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(req);
+
     this.globalProgressBarService.countUp();
     return next.handle(req).pipe(
       finalize(() => {
@@ -23,3 +44,5 @@ export class GlobalProgressBarInterceptorService implements HttpInterceptor {
     );
   }
 }
+
+
