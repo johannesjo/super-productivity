786e0164b1ef8922ee20c0dcdfa11872febdde94
Johannes Millan <johannes.millan@gmail.com>
2020-06-10 18:22:36 +0200

    feat: save per default for fullscreen markdown dialog

--- src/app/ui/dialog-fullscreen-markdown/dialog-fullscreen-markdown.component.ts
+++ src/app/ui/dialog-fullscreen-markdown/dialog-fullscreen-markdown.component.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, OnDestroy} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {T} from '../../t.const';
+import {Subscription} from 'rxjs';
+import {ESCAPE} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'dialog-fullscreen-markdown',
@@ -8,16 +10,30 @@ import {T} from '../../t.const';
   styleUrls: ['./dialog-fullscreen-markdown.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DialogFullscreenMarkdownComponent {
+export class DialogFullscreenMarkdownComponent implements OnDestroy {
   T = T;
+
+  private _subs = new Subscription();
 
   constructor(
     private _matDialogRef: MatDialogRef<DialogFullscreenMarkdownComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
+    // we want to save as default
+    _matDialogRef.disableClose = true;
+    this._subs.add(_matDialogRef.keydownEvents().subscribe(e => {
+      if (e.keyCode === ESCAPE) {
+        e.preventDefault();
+        this.close();
+      }
+    }));
   }
 
-  close(isSave: boolean) {
-    this._matDialogRef.close(isSave && this.data.content);
+  ngOnDestroy() {
+    this._subs.unsubscribe();
+  }
+
+  close(isSkipSave = false) {
+    this._matDialogRef.close(isSkipSave || this.data.content);
   }
 }
