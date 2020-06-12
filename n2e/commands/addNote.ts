50516d4f0c3b06e098fd30791bb5cb0da42c857a
Johannes Millan <johannes.millan@gmail.com>
2020-06-12 16:29:05 +0200

    test: check for new note available after reload

---
+++ n2e/commands/addNote.ts
@@ -0,0 +1,22 @@
+import {NightwatchBrowser} from 'nightwatch';
+
+const ADD_NOTE_BTN = '#add-note-btn';
+const TEXTAREA = 'dialog-add-note textarea';
+const ADD_NOTE_SUBMIT_BTN = 'dialog-add-note button[type=submit]';
+const NOTES_WRAPPER = 'notes';
+
+module.exports = {
+  async command(this: NightwatchBrowser, noteName: string) {
+    return this
+      .moveToElement(NOTES_WRAPPER, 10, 50)
+      .waitForElementPresent(ADD_NOTE_BTN)
+      .click(ADD_NOTE_BTN)
+
+      .waitForElementPresent(TEXTAREA)
+      .setValue(TEXTAREA, noteName)
+
+      .click(ADD_NOTE_SUBMIT_BTN)
+      .moveToElement(NOTES_WRAPPER, 10, 50)
+      ;
+  }
+};
--- n2e/n-browser-interface.ts
+++ n2e/n-browser-interface.ts
@@ -2,6 +2,7 @@ import {NightwatchBrowser} from 'nightwatch';
 
 export interface NBrowser extends NightwatchBrowser {
   addTask: (taskTitle: string) => NBrowser;
+  addNote: (noteTitle: string) => NBrowser;
   goToDefaultProject: () => NBrowser;
   openPanelForTask: (taskSel: string) => NBrowser;
   addTaskWithReminder: (taskTitle: string, taskSel?: string) => NBrowser;
