4e28d131eecd7129e9298c872b3d9e0bf561119b
Johannes Millan <johannes.millan@gmail.com>
2020-07-06 13:44:35 +0200

    fix: don't ask for notification permission without user interaction

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,6 @@ import { PersistenceModule } from './persistence/persistence.module';
 import { ChromeExtensionInterfaceModule } from './chrome-extension-interface/chrome-extension-interface.module';
 import { SnackModule } from './snack/snack.module';
 import { RouterModule } from '@angular/router';
-import { NotifyModule } from './notify/notify.module';
 import { LocalBackupModule } from '../imex/local-backup/local-backup.module';
 import { BannerModule } from './banner/banner.module';
 import { CompressionModule } from './compression/compression.module';
@@ -17,7 +16,6 @@ import { CompressionModule } from './compression/compression.module';
     ChromeExtensionInterfaceModule,
     SnackModule,
     BannerModule,
-    NotifyModule,
     LocalBackupModule,
     CompressionModule,
   ],
@@ -26,7 +24,6 @@ import { CompressionModule } from './compression/compression.module';
     ChromeExtensionInterfaceModule,
     SnackModule,
     BannerModule,
-    NotifyModule,
   ],
 })
 export class CoreModule {
