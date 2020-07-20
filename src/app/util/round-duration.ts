4a80f260be6169b49d1274666047efe515043c68
Johannes Millan <johannes.millan@gmail.com>
2020-07-20 22:00:59 +0200

    fix: add fallback to prevent NaN for daily summary

--- src/app/util/round-duration.ts
+++ src/app/util/round-duration.ts
@@ -18,26 +18,26 @@ export const roundMinutes = (minutes: number, factor: number, isRoundUp: boolean
 
 export const roundDurationVanilla = (val: number, roundTo: RoundTimeOption, isRoundUp: boolean = false): number => {
   const asMinutes = parseMsToMinutes(val);
-  const MSF = 60000;
+  const A_MINUTE = 60000;
 
   switch (roundTo) {
     case '5M':
-      return roundMinutes(asMinutes, 5, isRoundUp) * MSF;
+      return roundMinutes(asMinutes, 5, isRoundUp) * A_MINUTE;
 
     case 'QUARTER':
-      return roundMinutes(asMinutes, 15, isRoundUp) * MSF;
+      return roundMinutes(asMinutes, 15, isRoundUp) * A_MINUTE;
 
     case 'HALF':
-      return roundMinutes(asMinutes, 30, isRoundUp) * MSF;
+      return roundMinutes(asMinutes, 30, isRoundUp) * A_MINUTE;
 
     case 'HOUR':
-      return roundMinutes(asMinutes, 60, isRoundUp) * MSF;
+      return roundMinutes(asMinutes, 60, isRoundUp) * A_MINUTE;
 
     default:
       return val;
   }
 };
 
-export const parseMsToMinutes = (ms: number): number => {
+export const parseMsToMinutes = (ms: number = 0): number => {
   return Math.round(ms / 60000);
 };
