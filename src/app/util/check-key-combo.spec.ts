0f4d2b7550b225bfefca40245394775e7bb8fad9
Johannes Millan <johannes.millan@gmail.com>
2020-07-24 22:24:45 +0200

    fix: cannot read property 'includes' of undefined #453 #456 #457

---
+++ src/app/util/check-key-combo.spec.ts
@@ -0,0 +1,33 @@
+import { checkKeyCombo } from './check-key-combo';
+
+describe('checkKeyCombo', () => {
+  it('should return a true if keys are pressed', () => {
+    const ev: Partial<KeyboardEvent> = {
+      key: 'A',
+      ctrlKey: true,
+      shiftKey: true
+    };
+    expect(checkKeyCombo(ev as any, 'Ctrl+Shift+A')).toBe(true);
+  });
+
+  it('should return a false if keys are not pressed', () => {
+    const ev: Partial<KeyboardEvent> = {
+      key: 'A',
+      ctrlKey: true,
+      shiftKey: true
+    };
+    const comboToCheck = 'Ctrl+Shift+A';
+    expect(checkKeyCombo({...ev, ctrlKey: false} as any, comboToCheck)).toBe(false);
+    expect(checkKeyCombo({...ev, shiftKey: false} as any, comboToCheck)).toBe(false);
+    expect(checkKeyCombo({...ev, key: 'B'} as any, comboToCheck)).toBe(false);
+  });
+
+  it('should not throw for undefined', () => {
+    const ev: Partial<KeyboardEvent> = {
+      key: 'A',
+      ctrlKey: true,
+      shiftKey: true
+    };
+    expect((checkKeyCombo as any)(ev as any, undefined)).toBe(false);
+  });
+});
--- src/app/util/check-key-combo.ts
+++ src/app/util/check-key-combo.ts
@@ -2,12 +2,14 @@ const isSpecialKeyExactlyRight = (isKeyRequired: boolean, isKeyPressed: boolean)
   return (isKeyRequired && isKeyPressed) || (!isKeyRequired && !isKeyPressed);
 };
 
-export const checkKeyCombo = (ev: KeyboardEvent, comboToTest: string | null) => {
-  if (comboToTest !== null) {
+export const checkKeyCombo = (ev: KeyboardEvent, comboToTest: string | null): boolean => {
+  // NOTE: comboToTest can sometimes be undefined
+  if (!!comboToTest) {
     const isPlusKey = comboToTest.includes('++');
     const comboKeys: string[] = comboToTest.split('+');
     const standardKey: string = comboKeys[comboKeys.length - 1];
     const sk = comboKeys.splice(0);
+
     sk.splice(-1, 1);
 
     return isSpecialKeyExactlyRight(sk.includes('Ctrl'), ev.ctrlKey)
@@ -15,7 +17,6 @@ export const checkKeyCombo = (ev: KeyboardEvent, comboToTest: string | null) => 
       && isSpecialKeyExactlyRight(sk.includes('Meta'), ev.metaKey)
       && (!(sk.includes('Shift')) || ev.shiftKey)
       && (ev.key === standardKey || isPlusKey && ev.key === '+');
-  } else {
-    return null;
   }
+  return false;
 };
