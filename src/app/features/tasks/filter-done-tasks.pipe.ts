c68b6a077e6e9388371ce94042cfcddcc0a1b9c8
Johannes Millan <johannes.millan@gmail.com>
2020-07-31 17:25:55 +0200

    fix: sub task collapsing not working as it should

--- src/app/features/tasks/filter-done-tasks.pipe.ts
+++ src/app/features/tasks/filter-done-tasks.pipe.ts
@@ -4,8 +4,10 @@ import { TaskWithSubTasks } from './task.model';
 export const filterDoneTasks = (tasks: TaskWithSubTasks[], currentTaskId: string | null, isFilterDone: boolean, isFilterAll: boolean): any => {
   return isFilterDone
     ? tasks.filter(task => !task.isDone)
-    : (isFilterAll && currentTaskId)
-      ? tasks.filter(task => task.id === currentTaskId)
+    : (isFilterAll)
+      ? !!currentTaskId
+        ? tasks.filter(task => task.id !== currentTaskId)
+        : []
       : tasks;
 };
 
