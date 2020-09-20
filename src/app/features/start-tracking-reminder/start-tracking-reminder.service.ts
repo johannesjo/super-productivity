68e802eeb32781b1b884ee51154b652c28830f5b
Johannes Millan <johannes.millan@gmail.com>
2020-09-20 15:11:25 +0200

    feat(startTrackingReminder): prepare banner #507

--- src/app/features/start-tracking-reminder/start-tracking-reminder.service.ts
+++ src/app/features/start-tracking-reminder/start-tracking-reminder.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { IdleService } from '../time-tracking/idle.service';
 import { TaskService } from '../tasks/task.service';
 import { GlobalConfigService } from '../config/global-config.service';
-import { combineLatest, EMPTY, Observable, of } from 'rxjs';
+import { combineLatest, EMPTY, Observable, of, Subject } from 'rxjs';
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { realTimer$ } from './real-timer';
+import { BannerService } from '../../core/banner/banner.service';
+import { BannerId } from '../../core/banner/banner.model';
+import { msToString } from '../../ui/duration/ms-to-string.pipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StartTrackingReminderService {
   // TODO implement
-  cfg$: any = this._globalConfigService.cfg$;
+  cfgx$: any = this._globalConfigService.cfg$;
+  cfg$: any = of({
+    minTime: 4000,
+    isEnabled: true,
+  });
 
   // counter$: any = interval(1000).pipe(
   //   timeInterval(),
@@ -35,21 +42,51 @@ export class StartTrackingReminderService {
     )
   );
 
+  // private _manualReset$: Subject<void> = new Subject();
+
   constructor(
     private _idleService: IdleService,
     private _taskService: TaskService,
     private _globalConfigService: GlobalConfigService,
-    // private _bannerService: BannerService,
+    private _bannerService: BannerService,
   ) {
     // this.counter$.subscribe((v) => console.log('test', v))
     this.remindCounter$.subscribe((v) => console.log('remind$', v));
   }
 
   init() {
-    console.log('INIT');
-    this._openBanner();
+    this.remindCounter$.subscribe((count) => {
+      const isShow = true;
+      if (isShow) {
+        this._openBanner(count, {});
+      } else {
+        this._bannerService.dismiss(BannerId.StartTrackingReminder);
+      }
+    });
+
   }
 
-  private _openBanner() {
+  private _openBanner(duration: number, cfg: any) {
+    const durationStr = msToString(duration);
+    this._bannerService.open({
+      id: BannerId.StartTrackingReminder,
+      ico: 'free_breakfast',
+      msg: 'You have been not tracking time for' + durationStr,
+      translateParams: {
+        time: '15m'
+      },
+      action: {
+        label: 'Add to task',
+        fn: () => {
+          this._bannerService.dismiss(BannerId.StartTrackingReminder);
+        }
+      },
+      action2: {
+        label: 'Dismiss',
+        fn: () => {
+          this._bannerService.dismiss(BannerId.StartTrackingReminder);
+        }
+      },
+    });
   }
 }
