a7da58c62f7b955d155fd5e906c4a90e1273f1b2
Johannes Millan <johannes.millan@gmail.com>
2020-10-10 22:57:39 +0200

    refactor(webDav): cleanup #159

--- src/app/imex/sync/web-dav/web-dav.module.ts
+++ src/app/imex/sync/web-dav/web-dav.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EffectsModule } from '@ngrx/effects';
-import { WebDavEffects } from './store/web-dav-effects.service';
-import { FormsModule } from '@angular/forms';
-import { UiModule } from '../../../ui/ui.module';
 
 @NgModule({
   declarations: [],
   exports: [],
-  imports: [
-    CommonModule,
-    FormsModule,
-    UiModule,
-    EffectsModule.forFeature([WebDavEffects])
-  ]
+  imports: []
 })
 export class WebDavModule {
 }
