d70a84d898586965054ac56c59dc77d16820ce94
Johannes Millan <johannes.millan@gmail.com>
2020-10-11 17:10:54 +0200

    refactor: move keyboard config to own file

--- src/app/features/config/global-config.model.ts
+++ src/app/features/config/global-config.model.ts
@@ -1,44 +1,7 @@
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { ProjectCfgFormKey } from '../project/project.model';
 import { LanguageCode, MODEL_VERSION_KEY } from '../../app.constants';
-
-export type KeyboardConfig = Readonly<{
-  globalShowHide: string | null,
-  globalAddNote: string | null,
-  globalAddTask: string | null,
-  toggleBacklog: string | null,
-  goToWorkView: string | null,
-  // goToFocusMode: string|null,
-  // goToDailyAgenda: string|null,
-  goToSettings: string | null,
-  addNewTask: string | null,
-  globalToggleTaskStart: string | null,
-  showHelp: string | null,
-  addNewNote: string | null,
-  openProjectNotes: string | null,
-  toggleBookmarks: string | null;
-  openDistractionPanel: string | null,
-  zoomIn: string | null,
-  zoomOut: string | null,
-  zoomDefault: string | null,
-  taskEditTitle: string | null,
-  taskToggleAdditionalInfoOpen: string | null,
-  taskOpenEstimationDialog: string | null,
-  taskToggleDone: string | null,
-  taskAddSubTask: string | null,
-  taskMoveToProject: string | null,
-  taskDelete: string | null,
-  taskSchedule: string | null,
-  selectPreviousTask: string | null,
-  selectNextTask: string | null,
-  moveTaskUp: string | null,
-  moveTaskDown: string | null,
-  moveToBacklog: string | null,
-  moveToTodaysTasks: string | null,
-  expandSubTasks: string | null,
-  collapseSubTasks: string | null,
-  togglePlay: string | null,
-}>;
+import { KeyboardConfig } from './keyboard-config.model';
 
 export type MiscConfig = Readonly<{
   isDarkMode: boolean;
