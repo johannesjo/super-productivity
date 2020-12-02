6973a9556b40b16dce07ab4232fe9bdf164af93c
Johannes Millan <johannes.millan@gmail.com>
2020-12-02 19:21:05 +0100

    fix(autoRepair): persist fixed data to database

--- src/app/core/data-init/data-init.service.ts
+++ src/app/core/data-init/data-init.service.ts
@@ -60,6 +60,7 @@ export class DataInitService {
           appDataComplete: fixedData,
           isOmitTokens,
         }));
+        await this._persistenceService.importComplete(fixedData);
       }
     }
   }
