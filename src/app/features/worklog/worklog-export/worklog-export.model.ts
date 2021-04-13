e9085b55cc64a3c46ab5bd79e5515bee0c0f3a3b
Stefan Kofler <kofler.s90@gmail.com>
2021-04-13 20:28:47 +0100

    refactor(worklog-export): split RowItem in TaskFields and TimeFields

--- src/app/features/worklog/worklog-export/worklog-export.model.ts
+++ src/app/features/worklog/worklog-export/worklog-export.model.ts
@@ -3,20 +3,6 @@ import { Tag } from '../../tag/tag.model';
 import { WorklogTask } from '../../tasks/task.model';
 import { WorkStartEnd } from '../../work-context/work-context.model';
 
-export interface RowItem {
-  dates: string[];
-  workStart: number | undefined;
-  workEnd: number | undefined;
-  timeSpent: number;
-  timeEstimate: number;
-  tasks: WorklogTask[];
-  titles?: string[];
-  titlesWithSub?: string[];
-  notes: string[];
-  projects: string[];
-  tags: string[];
-}
-
 export interface WorklogExportData {
   tasks: WorklogTask[];
   projects: Project[];
@@ -27,4 +13,27 @@ export interface WorklogExportData {
 export interface WorkTimes {
   start: WorkStartEnd;
   end: WorkStartEnd;
+}
+
+export type TimeFields = {
+  dates: string[];
+  timeSpent: number;
+  timeEstimate: number;
+  workStart: number;
+  workEnd: number;
+};
+
+export type TaskFields = {
+  tasks: WorklogTask[];
+  titles: string[];
+  titlesWithSub: string[];
+  notes: string[];
+  projects: string[];
+  tags: string[];
+};
+
+export type RowItem = TimeFields & TaskFields;
+
+export interface ItemsByKey<T> {
+  [key: string]: T;
 }
