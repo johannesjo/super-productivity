2c46b549e25aee468aea317f08104e10ae8547b8
Johannes Millan <johannes.millan@gmail.com>
2021-04-25 22:43:51 +0200

    feat(timeline): make most basic events work

--- src/app/features/timeline/timeline.component.ts
+++ src/app/features/timeline/timeline.component.ts
@@ -52,11 +52,19 @@ export class TimelineComponent {
   private mapToViewEntries(tasks: Task[]): any {
     // TODO make current always the first task
     const viewEntries: any[] = [];
+    const scheduledTasks: any[] = [];
+
     let lastTime: any;
     let prev: any;
     console.log(tasks);
 
     tasks.forEach((task, index, arr) => {
+      // TODO replace with plannedAt
+      if (task.reminderId && task.plannedAt) {
+        scheduledTasks.push(task);
+        return;
+      }
+
       prev = arr[index - 1];
       let time;
 
@@ -70,7 +78,7 @@ export class TimelineComponent {
         time = Date.now();
       }
 
-      console.log(time, lastTime);
+      // console.log(time, lastTime);
 
       viewEntries.push({
         id: task.id,
@@ -83,17 +91,49 @@ export class TimelineComponent {
       lastTime = time;
     });
 
-    const lastEntry = viewEntries && viewEntries[viewEntries.length - 1];
-    console.log({lastEntry});
+    // const lastEntry = viewEntries && viewEntries[viewEntries.length - 1];
+    // console.log({lastEntry});
+    //
+    // if (lastEntry && lastEntry.type === TimelineViewEntryType.TaskFull) {
+    //   const task = lastEntry.data;
+    //   viewEntries.push({
+    //     id: 'END',
+    //     type: TimelineViewEntryType.WorkdayEnd,
+    //     time: lastTime + Math.max(0, task.timeEstimate - task.timeSpent)
+    //   });
+    // }
 
-    if (lastEntry && lastEntry.type === TimelineViewEntryType.TaskFull) {
-      const task = lastEntry.data;
-      viewEntries.push({
-        id: 'END',
-        type: TimelineViewEntryType.WorkdayEnd,
-        time: lastTime + Math.max(0, task.timeEstimate - task.timeSpent)
+    if (scheduledTasks.length) {
+      scheduledTasks.forEach((scheduledTask, i) => {
+        const firstEntryBeforeIndex = viewEntries.findIndex(viewEntry => viewEntry.time !== 0 && viewEntry.time >= scheduledTask.plannedAt);
+
+        const splitTask = viewEntries[firstEntryBeforeIndex - 1].data;
+        const scheduledTaskDuration = Math.max(0, scheduledTask.timeEstimate - scheduledTask.timeSpent);
+
+        viewEntries.splice(firstEntryBeforeIndex, 0, {
+          id: scheduledTask.id,
+          time: scheduledTask.plannedAt,
+          type: TimelineViewEntryType.TaskFull,
+          data: scheduledTask,
+        });
+        viewEntries.splice(firstEntryBeforeIndex + 1, 0, {
+          id: splitTask.id,
+          time: scheduledTask.plannedAt + scheduledTaskDuration,
+          type: TimelineViewEntryType.Text,
+          data: '... ' + splitTask.title,
+        });
+
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let j = firstEntryBeforeIndex + 2; j < viewEntries.length; j++) {
+          const viewEntry = viewEntries[j];
+          if (viewEntry.time) {
+            viewEntry.time = viewEntry.time + scheduledTaskDuration;
+          }
+        }
       });
     }
+
+    console.log(viewEntries);
 
     return viewEntries;
     //   {
--- src/app/features/timeline/timeline.model.ts
+++ src/app/features/timeline/timeline.model.ts
@@ -14,4 +14,5 @@ export enum TimelineViewEntryType {
   Break,
   Now,
   WorkdayEnd,
+  Text,
 }
