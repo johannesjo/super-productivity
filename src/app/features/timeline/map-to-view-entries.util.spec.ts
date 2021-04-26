a350dc534f9ea9e3c6b59b70c50da7d73602bb61
Johannes Millan <johannes.millan@gmail.com>
2021-04-26 21:51:16 +0200

    test(timeline): add second simple test

--- src/app/features/timeline/map-to-view-entries.util.spec.ts
+++ src/app/features/timeline/map-to-view-entries.util.spec.ts
@@ -23,12 +23,64 @@ describe('mapToViewEntries()', () => {
       id: fakeTasks[0].id,
       type: TimelineViewEntryType.TaskFull,
       time: startTime,
-      data: fakeTasks[0]
+      data: fakeTasks[0],
+      isSameTimeAsPrevious: false,
     }, {
       id: fakeTasks[1].id,
       type: TimelineViewEntryType.TaskFull,
       time: 5033,
-      data: fakeTasks[1]
+      data: fakeTasks[1],
+      isSameTimeAsPrevious: false,
+    }]);
+  });
+
+  it('should work for simple task list 2', () => {
+    const startTime = 44;
+    const fakeTasks = [
+      {...FAKE_TASK, timeEstimate: 5000},
+      {...FAKE_TASK, timeEstimate: 8000},
+      {...FAKE_TASK, timeEstimate: 5000, timeSpent: 5000},
+      {...FAKE_TASK},
+      {...FAKE_TASK, timeEstimate: 3000, timeSpent: 1000},
+      {...FAKE_TASK},
+    ];
+    const r = mapToViewEntries(fakeTasks, null, startTime);
+    expect(r).toEqual([{
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: startTime,
+      data: fakeTasks[0],
+      isSameTimeAsPrevious: false,
+    }, {
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: 5044,
+      data: fakeTasks[1],
+      isSameTimeAsPrevious: false,
+    }, {
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: 13044,
+      data: fakeTasks[2],
+      isSameTimeAsPrevious: false,
+    }, {
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: 13044,
+      data: fakeTasks[3],
+      isSameTimeAsPrevious: true,
+    }, {
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: 13044,
+      data: fakeTasks[4],
+      isSameTimeAsPrevious: true,
+    }, {
+      id: FID,
+      type: TimelineViewEntryType.TaskFull,
+      time: 15044,
+      data: fakeTasks[5],
+      isSameTimeAsPrevious: false,
     }]);
   });
 });
