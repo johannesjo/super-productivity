dad31c1f3bfb3d3e265afcc7a9481188dcba5bdb
Johannes Millan <johannes.millan@gmail.com>
2021-04-27 21:18:45 +0200

    refactor(timeline): re-order code

--- src/app/features/timeline/map-to-view-entries.util.ts
+++ src/app/features/timeline/map-to-view-entries.util.ts
@@ -2,23 +2,51 @@ import { Task } from '../tasks/task.model';
 import { TimelineViewEntry, TimelineViewEntryType } from './timeline.model';
 import { msToString } from '../../ui/duration/ms-to-string.pipe';
 
-export const mapToViewEntries = (tasks: Task[], currentId: string | null, startTime: number = Date.now()): TimelineViewEntry[] => {
-  const viewEntries: TimelineViewEntry[] = [];
-  const scheduledTasks: Task[] = [];
+// const FAKE_TIMELINE_EVENTS: CustomEvent[] = [{}];
 
+export const mapToViewEntries = (tasks: Task[], currentId: string | null, startTime: number = Date.now()): TimelineViewEntry[] => {
+
+  const initialTasks: Task[] = currentId
+    ? resortTasksWithCurrentFirst(currentId, tasks)
+    : tasks;
+
+  const [viewEntries, scheduledTasks] = createViewEntriesForNormalTasks(startTime, initialTasks);
+
+  return addViewEntriesForScheduled(scheduledTasks, viewEntries);
+
+  // const lastEntry = viewEntries && viewEntries[viewEntries.length - 1];
+  // console.log({lastEntry});
+  //
+  // if (lastEntry && lastEntry.type === TimelineViewEntryType.TaskFull) {
+  //   const task = lastEntry.data;
+  //   viewEntries.push({
+  //     id: 'END',
+  //     type: TimelineViewEntryType.WorkdayEnd,
+  //     time: lastTime + Math.max(0, task.timeEstimate - task.timeSpent)
+  //   });
+  // }
+
+};
+
+const resortTasksWithCurrentFirst = (currentId: string, tasks: Task[]): Task[] => {
+  let newTasks = tasks;
+  const currentTask = tasks.find(t => t.id === currentId);
+  if (currentTask) {
+    newTasks = [currentTask, ...tasks.filter(t => t.id !== currentId)] as Task[];
+  }
+  return newTasks;
+};
+
+const createViewEntriesForNormalTasks = (startTime: number, tasks: Task[]): [TimelineViewEntry[], Task[]] => {
   let lastTime: any;
   let prev: any;
-  let sortedTasks: Task[] = tasks;
 
-  // make current always first task
-  if (currentId) {
-    const currentTask = tasks.find(t => t.id === currentId);
-    if (currentTask) {
-      sortedTasks = [currentTask, ...tasks.filter(t => t.id !== currentId)] as Task[];
-    }
-  }
+  const sortedTasks: Task[] = tasks;
+  const scheduledTasks: Task[] = [];
+  const viewEntries: TimelineViewEntry[] = [];
 
   sortedTasks.forEach((task, index, arr) => {
+    // NOTE: not pretty but performant
     if (task.reminderId && task.plannedAt) {
       scheduledTasks.push(task);
       return;
@@ -39,7 +67,7 @@ export const mapToViewEntries = (tasks: Task[], currentId: string | null, startT
 
     viewEntries.push({
       id: task.id,
-      type: TimelineViewEntryType.TaskFull,
+      type: TimelineViewEntryType.Task,
       time,
       data: task,
       // TODO add isSameTimeAsPrevious at the very end
@@ -49,74 +77,59 @@ export const mapToViewEntries = (tasks: Task[], currentId: string | null, startT
     lastTime = time;
   });
 
-  // const lastEntry = viewEntries && viewEntries[viewEntries.length - 1];
-  // console.log({lastEntry});
-  //
-  // if (lastEntry && lastEntry.type === TimelineViewEntryType.TaskFull) {
-  //   const task = lastEntry.data;
-  //   viewEntries.push({
-  //     id: 'END',
-  //     type: TimelineViewEntryType.WorkdayEnd,
-  //     time: lastTime + Math.max(0, task.timeEstimate - task.timeSpent)
-  //   });
-  // }
+  return [viewEntries, scheduledTasks];
+};
 
-  if (scheduledTasks.length) {
-    scheduledTasks.forEach((scheduledTask, i) => {
-      const firstEntryBeforeIndex = viewEntries.findIndex(
-        viewEntry =>
-          viewEntry.time
-          && viewEntry.time !== 0
-          && viewEntry.time >= (scheduledTask.plannedAt as number)
-      );
-
-      // TODO check once we have more different
-      const splitTask: Task | undefined = viewEntries[firstEntryBeforeIndex - 1]?.data as Task;
-      const scheduledTaskDuration = getTimeForTask(scheduledTask);
-
-      viewEntries.splice(firstEntryBeforeIndex || 0, 0, {
-        id: scheduledTask.id,
-        time: scheduledTask.plannedAt,
-        type: TimelineViewEntryType.TaskFull,
-        data: scheduledTask,
-        isSameTimeAsPrevious: false,
-      });
-
-      const isAddSplitTask = (splitTask && (splitTask.timeEstimate - splitTask.timeSpent > 0));
-      if (isAddSplitTask) {
-        const splitTime = getTimeForTask(splitTask) - scheduledTaskDuration;
-        const splitStr = msToString(splitTime);
-        viewEntries.splice(firstEntryBeforeIndex + 1, 0, {
-          id: (splitTask as Task).id,
-          time: (scheduledTask.plannedAt as number) + scheduledTaskDuration,
-          type: TimelineViewEntryType.Text,
-          data: '... ' + (splitTask as Task).title + ' (' + splitStr + ')',
-          isSameTimeAsPrevious: true,
-        });
-      }
-
-      const startIndexOfFollowing = firstEntryBeforeIndex + (isAddSplitTask ? 2 : 1);
-      // eslint-disable-next-line @typescript-eslint/prefer-for-of
-      for (let j = startIndexOfFollowing; j < viewEntries.length; j++) {
-        const viewEntry = viewEntries[j];
-        if (viewEntry.time) {
-          viewEntry.time = viewEntry.time + scheduledTaskDuration;
-        }
-      }
-    });
+const addViewEntriesForScheduled = (scheduledTasks: Task[], viewEntries: TimelineViewEntry[]): TimelineViewEntry[] => {
+  if (!scheduledTasks.length) {
+    return viewEntries;
   }
+  const newViewEntries: TimelineViewEntry[] = viewEntries.slice(0);
 
-  console.log(viewEntries);
+  scheduledTasks.forEach((scheduledTask, i) => {
+    const firstEntryBeforeIndex = newViewEntries.findIndex(
+      viewEntry =>
+        viewEntry.time
+        && viewEntry.time !== 0
+        && viewEntry.time >= (scheduledTask.plannedAt as number)
+    );
 
-  return viewEntries;
-  //   {
-  //     type: TimelineViewEntryType.TaskFull,
-  //     time: Date.now(),
-  //     data: {
-  //       ...DEFAULT_TASK,
-  //       title: 'SomeTask',
-  //     }
-  //   },
+    // TODO check once we have more different
+    const splitTask: Task | undefined = newViewEntries[firstEntryBeforeIndex - 1]?.data as Task;
+    const scheduledTaskDuration = getTimeForTask(scheduledTask);
+
+    newViewEntries.splice(firstEntryBeforeIndex || 0, 0, {
+      id: scheduledTask.id,
+      time: scheduledTask.plannedAt,
+      type: TimelineViewEntryType.ScheduledTask,
+      data: scheduledTask,
+      isSameTimeAsPrevious: false,
+    });
+
+    const isAddSplitTask = (splitTask && (splitTask.timeEstimate - splitTask.timeSpent > 0));
+    if (isAddSplitTask) {
+      const splitTime = getTimeForTask(splitTask) - scheduledTaskDuration;
+      const splitStr = msToString(splitTime);
+      newViewEntries.splice(firstEntryBeforeIndex + 1, 0, {
+        id: (splitTask as Task).id,
+        time: (scheduledTask.plannedAt as number) + scheduledTaskDuration,
+        type: TimelineViewEntryType.Text,
+        data: '... ' + (splitTask as Task).title + ' (' + splitStr + ')',
+        isSameTimeAsPrevious: true,
+      });
+    }
+
+    const startIndexOfFollowing = firstEntryBeforeIndex + (isAddSplitTask ? 2 : 1);
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    for (let j = startIndexOfFollowing; j < newViewEntries.length; j++) {
+      const viewEntry = newViewEntries[j];
+      if (viewEntry.time) {
+        viewEntry.time = viewEntry.time + scheduledTaskDuration;
+      }
+    }
+  });
+
+  return newViewEntries;
 };
 
 const getTimeForTask = (task: Task): number => {
