db68c3df0806dc2e51f2cf5ffd2641b962919285
Johannes Millan <johannes.millan@gmail.com>
2021-05-06 23:15:25 +0200

    refactor(timeline): uglify thanks to prettier

--- src/app/pages/timeline-page/timeline-page.module.ts
+++ src/app/pages/timeline-page/timeline-page.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelinePageComponent } from './timeline-page.component';
 import { WorkViewModule } from '../../features/work-view/work-view.module';
-import { TimelineModule } from '../../features/timeline/timeline.module';
 
 @NgModule({
   declarations: [TimelinePageComponent],
