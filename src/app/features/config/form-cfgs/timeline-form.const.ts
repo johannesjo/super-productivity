3ca54f1d8ddce8f5922bc17e9749ad2168c9e963
Johannes Millan <johannes.millan@gmail.com>
2021-05-10 22:09:28 +0200

    feat(timeline): add validation for timeline form

--- src/app/features/config/form-cfgs/timeline-form.const.ts
+++ src/app/features/config/form-cfgs/timeline-form.const.ts
@@ -1,6 +1,22 @@
 import { ConfigFormSection, TimelineConfig } from '../global-config.model';
 import { T } from '../../../t.const';
 
+const isValidSplitTime = (v: string | undefined): boolean => {
+  if (v && v.split) {
+    const split = v.split(':');
+    return (
+      split.length === 2 &&
+      !isNaN(+split[0]) &&
+      !isNaN(+split[1]) &&
+      +split[0] >= 0 &&
+      +split[0] <= 24 &&
+      +split[1] >= 0 &&
+      +split[1] <= 59
+    );
+  }
+  return false;
+};
+
 export const TIMELINE_FORM_CFG: ConfigFormSection<TimelineConfig> = {
   title: T.GCF.TIMELINE.TITLE,
   help: T.GCF.TIMELINE.HELP,
@@ -18,8 +34,14 @@ export const TIMELINE_FORM_CFG: ConfigFormSection<TimelineConfig> = {
       key: 'workStart',
       type: 'input',
       templateOptions: {
+        required: true,
         label: T.GCF.TIMELINE.L_WORK_START,
         description: T.GCF.TIMELINE.WORK_START_END_DESCRIPTION,
+      },
+      validators: {
+        validTimeString: (c: { value: string | undefined }) => {
+          return isValidSplitTime(c.value);
+        },
       },
     },
     {
@@ -27,8 +49,14 @@ export const TIMELINE_FORM_CFG: ConfigFormSection<TimelineConfig> = {
       key: 'workEnd',
       type: 'input',
       templateOptions: {
+        required: true,
         label: T.GCF.TIMELINE.L_WORK_END,
         description: T.GCF.TIMELINE.WORK_START_END_DESCRIPTION,
+      },
+      validators: {
+        validTimeString: (c: { value: string | undefined }) => {
+          return isValidSplitTime(c.value);
+        },
       },
     },
   ],
