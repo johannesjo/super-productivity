0749082e6ce7353d11775c7f901dc88b44a9548a
Johannes Millan <johannes.millan@gmail.com>
2021-05-25 20:57:25 +0200

    refactor: remove unused worklog page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,7 +6,6 @@ import { ProjectTaskPageModule } from './project-task-page/project-task-page.mod
 import { DailySummaryModule } from './daily-summary/daily-summary.module';
 import { MetricPageModule } from './metric-page/metric-page.module';
 import { SchedulePageModule } from './schedule-page/schedule-page.module';
-import { WorklogPageModule } from './worklog-page/worklog-page.module';
 import { ProjectSettingsPageModule } from './project-settings-page/project-settings-page.module';
 import { TagTaskPageModule } from './tag-task-page/tag-task-page.module';
 import { TagSettingsPageModule } from './tag-settings-page/tag-settings-page.module';
@@ -21,7 +20,6 @@ import { TimelinePageModule } from './timeline-page/timeline-page.module';
     ProjectSettingsPageModule,
     DailySummaryModule,
     MetricPageModule,
-    WorklogPageModule,
     SchedulePageModule,
     TagTaskPageModule,
     TagSettingsPageModule,
