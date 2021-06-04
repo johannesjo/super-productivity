bd13e26d1ebbab6859e1860bd149032f55bb6872
Johannes Millan <johannes.millan@gmail.com>
2021-06-04 14:34:24 +0200

    fix: tag deletion not working anymore #1264

--- src/app/core-ui/work-context-menu/work-context-menu.component.ts
+++ src/app/core-ui/work-context-menu/work-context-menu.component.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { WorkContextType } from '../../features/work-context/work-context.model';
 import { T } from 'src/app/t.const';
 import { TODAY_TAG } from '../../features/tag/tag.const';
-import { from, Observable, of, Subscription } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { DialogCreateProjectComponent } from '../../features/project/dialogs/create-project/dialog-create-project.component';
 import { DialogConfirmComponent } from '../../ui/dialog-confirm/dialog-confirm.component';
 import { MatDialog } from '@angular/material/dialog';
 import { TagService } from '../../features/tag/tag.service';
-import { concatMap, filter, first, switchMap, take, tap } from 'rxjs/operators';
+import { filter, first, switchMap, take, tap } from 'rxjs/operators';
 import { Tag } from '../../features/tag/tag.model';
 import { WorkContextService } from '../../features/work-context/work-context.service';
 import { Router } from '@angular/router';
@@ -19,14 +19,13 @@ import { Project } from '../../features/project/project.model';
   styleUrls: ['./work-context-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WorkContextMenuComponent implements OnDestroy {
+export class WorkContextMenuComponent {
   @Input() project!: Project;
   @Input() contextId?: string;
   T: typeof T = T;
   TODAY_TAG_ID: string = TODAY_TAG.id as string;
   isForProject: boolean = true;
   base: string = 'project';
-  private _subs: Subscription = new Subscription();
 
   constructor(
     private _matDialog: MatDialog,
@@ -40,31 +39,26 @@ export class WorkContextMenuComponent implements OnDestroy {
     this.base = this.isForProject ? 'project' : 'tag';
   }
 
-  ngOnDestroy(): void {
-    this._subs.unsubscribe();
-  }
-
+  // TODO move flow to dialog as async wont't work here
   deleteTag() {
-    this._subs.add(
-      this._confirmTagDelete()
-        .pipe(
-          filter((isDelete) => isDelete && !!this.contextId),
-          switchMap(() =>
-            this._workContextService.activeWorkContextTypeAndId$.pipe(take(1)),
-          ),
-          tap(({ activeId }) => console.log(activeId, this.contextId)),
-          switchMap(({ activeId }) =>
-            activeId === this.contextId
-              ? from(this._router.navigateByUrl('/'))
-              : of(true),
-          ),
-        )
-        .subscribe(() => {
-          if (this.contextId) {
-            this._tagService.removeTag(this.contextId);
-          }
-        }),
-    );
+    // NOTE: in this particular case we don't want to unsubscribe, since this all should still
+    // happen after the menu is hidden
+    this._confirmTagDelete()
+      .pipe(
+        filter((isDelete) => isDelete && !!this.contextId),
+        switchMap(() =>
+          this._workContextService.activeWorkContextTypeAndId$.pipe(take(1)),
+        ),
+        tap(({ activeId }) => console.log(activeId, this.contextId)),
+        switchMap(({ activeId }) =>
+          activeId === this.contextId ? from(this._router.navigateByUrl('/')) : of(true),
+        ),
+      )
+      .subscribe(() => {
+        if (this.contextId) {
+          this._tagService.removeTag(this.contextId);
+        }
+      });
   }
 
   edit(project: Project) {
@@ -81,8 +75,9 @@ export class WorkContextMenuComponent implements OnDestroy {
 
     return this._tagService.getTagById$(this.contextId).pipe(
       first(),
-      concatMap((tag: Tag) =>
-        this._matDialog
+      tap(() => console.log('TID')),
+      switchMap((tag: Tag) => {
+        const obs = this._matDialog
           .open(DialogConfirmComponent, {
             restoreFocus: true,
             data: {
@@ -90,8 +85,13 @@ export class WorkContextMenuComponent implements OnDestroy {
               translateParams: { tagName: tag.title },
             },
           })
-          .afterClosed(),
-      ),
+          .afterClosed()
+          .pipe(tap(() => console.log('AAA')));
+        obs.subscribe((v) => console.log(`obs`, v));
+
+        return obs;
+      }),
+      tap(() => console.log('XXX')),
     );
   }
 }
