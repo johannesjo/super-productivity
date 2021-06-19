b9bc16596667ecd7878584b33ac579a1470fc9fb
Johannes Millan <johannes.millan@gmail.com>
2021-06-19 14:29:58 +0200

    feat(timeline): improve styling for repeat task projection

--- src/app/features/timeline/timeline.module.ts
+++ src/app/features/timeline/timeline.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { TimelineComponent } from './timeline.component';
 import { TasksModule } from '../tasks/tasks.module';
 import { UiModule } from '../../ui/ui.module';
-import { TimelineCustomEventComponent } from './timeline-custom-event/timeline-custom-event.component';
+import { TimelineRepeatTaskProjectionComponent } from './timeline-repeat-task-projection/timeline-repeat-task-projection.component';
 import { DialogTimelineInitialSetupComponent } from './dialog-timeline-initial-setup/dialog-timeline-initial-setup.component';
+import { TimelineCustomEventComponent } from './timeline-custom-event/timeline-custom-event.component';
 
 @NgModule({
   declarations: [
     TimelineComponent,
     TimelineCustomEventComponent,
+    TimelineRepeatTaskProjectionComponent,
     DialogTimelineInitialSetupComponent,
   ],
   exports: [TimelineComponent],
