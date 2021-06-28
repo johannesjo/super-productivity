06b9142384af91b2b7863e76b0cb595e432b1a55
Johannes Millan <johannes.millan@gmail.com>
2021-06-28 23:30:41 +0200

    feat: extend is touch only with android web view

--- src/app/util/is-touch.ts
+++ src/app/util/is-touch.ts
@@ -8,6 +8,9 @@
 // }
 // export const IS_TOUCH = isTouch();
 
+import { IS_ANDROID_WEB_VIEW } from './is-android-web-view';
+
 export const isTouchOnly = (): boolean => window.matchMedia('(pointer: coarse)').matches;
 
-export const IS_TOUCH_ONLY = isTouchOnly();
+// TODO check if IS_ANDROID_WEB_VIEW is really required
+export const IS_TOUCH_ONLY = isTouchOnly() || IS_ANDROID_WEB_VIEW;
