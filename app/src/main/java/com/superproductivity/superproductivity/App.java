579a17ca86fa0e558ca76c909abe23a4e921813b
Johannes Millan <johannes.millan@gmail.com>
2021-07-11 15:16:06 +0200

    feat: remove web view stuff from activity

--- app/src/main/java/com/superproductivity/superproductivity/App.java
+++ app/src/main/java/com/superproductivity/superproductivity/App.java
@@ -2,6 +2,9 @@ package com.superproductivity.superproductivity;
 
 import android.app.Application;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import androidx.lifecycle.Lifecycle;
@@ -10,10 +13,38 @@ import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ProcessLifecycleOwner;
 
 public class App extends Application implements LifecycleObserver {
+    WebView wv;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
+//        ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
+
+        WebHelper.instanceView(getApplicationContext());
+
+        boolean IS_DEBUG = 0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
+
+        // if your build is in debug mode, enable inspecting of web views
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (IS_DEBUG) {
+                WebView.setWebContentsDebuggingEnabled(true);
+            }
+        }
+
+        wv = WebHelper.getWebView();
+        if (wv != null) {
+            // needs to come last for some settings to take effect
+            if (IS_DEBUG) {
+                // String url = "https://test-app.super-productivity.com";
+                String url = "http://10.0.2.2:4200/";
+                // String url = "https://app.super-productivity.com";
+                wv.loadUrl(url);
+                Toast.makeText(this, "DEBUG: " + url, Toast.LENGTH_SHORT).show();
+            } else {
+                wv.loadUrl("https://app.super-productivity.com");
+                // wv.loadUrl("https://test-app.super-productivity.com");
+            }
+        }
     }
 
 //    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
--- app/src/main/java/com/superproductivity/superproductivity/FullscreenActivity.java
+++ app/src/main/java/com/superproductivity/superproductivity/FullscreenActivity.java
@@ -15,6 +15,8 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 /**
@@ -24,121 +26,22 @@ import android.widget.Toast;
 public class FullscreenActivity extends AppCompatActivity {
     CommonJavaScriptInterface jsi;
     WebView wv;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // if your build is in debug mode, enable inspecting of web views
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
-                WebView.setWebContentsDebuggingEnabled(true);
-            }
-        }
-
         Log.v("TW", "onCreate");
         if (savedInstanceState == null) {
             Log.v("TW", "onCreate reeeeeeeeeeeeeeeeeeload");
             // hide action bar
             getSupportActionBar().hide();
-
             setContentView(R.layout.activity_fullscreen);
 
             // init web view
-            wv = (WebView) findViewById(R.id.webview);
-
-            wv.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
-                        if (url.contains("super-productivity.com") || url.contains("localhost")) {
-                            return false;
-                        } else {
-                            view.getContext().startActivity(
-                                    new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                            return true;
-                        }
-                    } else {
-                        return false;
-                    }
-                }
-            });
-
-            wv.setWebChromeClient(new WebChromeClient());
-            wv.clearCache(true);
-            wv.clearHistory();
-
-            if (Build.VERSION.SDK_INT >= 19) {
-                wv.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-            } else {
-                wv.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-            }
-
-
-            // additional web view settings
-            WebSettings wSettings = wv.getSettings();
-
-            wSettings.setJavaScriptEnabled(true);
-            wSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-            wSettings.setLoadsImagesAutomatically(true);
-            if (Build.VERSION.SDK_INT < 18) {
-                wSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
-                wSettings.setDomStorageEnabled(true);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
-                wSettings.setLoadWithOverviewMode(true);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
-                wSettings.setDatabaseEnabled(true);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
-                wSettings.setGeolocationEnabled(true);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
-                wSettings.setAppCacheEnabled(true);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                wSettings.setMediaPlaybackRequiresUserGesture(false);
-            }
-            wSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-//            wSettings.setForceDark(FORCE_DARK_ON);
-
-
-            // allow google login
-            // @see https://stackoverflow.com/questions/45863004/how-some-apps-are-able-to-perform-google-login-successfully-in-android-webview
-            // Force links and redirects to open in the WebView instead of in a browser
-//            wv.setWebViewClient(new WebViewClient());
-            wSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-                wSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36");
-            }
-//            wSettings.setUserAgentString("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0");
-//        wSettings.setUserAgentString(wSettings.getUserAgentString().replace("; wv",""));
-
-            wSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-
-            boolean IS_DEBUG = 0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
-
-            jsi = new JavaScriptInterface(this, wv, IS_DEBUG);
-            wv.addJavascriptInterface(jsi, "SUPAndroid");
-
-            if (BuildConfig.FLAVOR.equals("fdroid")) {
-                wv.addJavascriptInterface(jsi, "SUPFDroid");
-            }
-
-            // needs to come last for some settings to take effect
-            if (IS_DEBUG) {
-//                String url = "https://test-app.super-productivity.com";
-                String url = "http://10.0.2.2:4200/#/config";
-//                String url = "https://app.super-productivity.com";
-                wv.loadUrl(url);
-                Toast.makeText(this, "DEBUG: " + url, Toast.LENGTH_SHORT).show();
-            } else {
-                wv.loadUrl("https://app.super-productivity.com");
-                //                wv.loadUrl("https://test-app.super-productivity.com");
-            }
+            wv = WebHelper.getWebView();
+            frameLayout = findViewById(R.id.webview_wrapper);
+            frameLayout.addView(wv);
         }
     }
 
