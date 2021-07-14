ed00e47a9c4d4420974f768039532dd63a828e23
Johannes Millan <johannes.millan@gmail.com>
2021-07-14 15:01:08 +0200

    build: update info about skip postinstall

--- tools/is-skip-postinstall.js
+++ tools/is-skip-postinstall.js
@@ -1,4 +1,5 @@
 console.log(process.env.SKIP_POST_INSTALL);
+// NOTE: this is just a script to make conditionally executing ngcc work via ||
 if (
   process.env.SKIP_POST_INSTALL &&
   process.env.SKIP_POST_INSTALL !== 'false' &&
@@ -7,5 +8,6 @@ if (
   console.log('\n\n!!! WARN: skipping postInstall !!!\n\n');
   process.exit(0);
 } else {
-  process.exit(1);
+  console.log('Exiting with error code on purpose! No worries :)');
+  process.exit(123456789);
 }
