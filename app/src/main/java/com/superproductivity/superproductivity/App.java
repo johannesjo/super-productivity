79fa3b196f962617149eb5eccef502636a769742
Johannes Millan <johannes.millan@gmail.com>
2021-09-03 17:03:46 +0200

    fix: remove TRIGGER_SYNC stuff completely for now

--- app/src/main/java/com/superproductivity/superproductivity/App.java
+++ app/src/main/java/com/superproductivity/superproductivity/App.java
@@ -46,26 +46,4 @@ public class App extends Application implements LifecycleObserver {
             }
         }
     }
-
-//    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-//    public void appInResumeState() {
-//        Toast.makeText(this, "In Foreground", Toast.LENGTH_LONG).show();
-//    }
-
-//    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-//    public void appInPauseState() {
-//        Toast.makeText(this, "In Background", Toast.LENGTH_LONG).show();
-//        // THIS LIKELY ONLY WORKS WITH BACKGROUND STUFF
-////                startActivity(new Intent("custom.actions.intent.TRIGGER_SYNC"));
-////        Intent intent = new Intent(getBaseContext(), FullscreenActivity.class);
-//        Intent intent = new Intent("custom.actions.intent.TRIGGER_SYNC");
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        startActivity(intent);
-//        //        Intent sendIntent = new Intent();
-////        sendIntent.setAction(Intent.ACTION_SEND);
-////
-////        if (sendIntent.resolveActivity(getPackageManager()) != null) {
-////            startActivity(sendIntent);
-////        }
-//    }
 }
