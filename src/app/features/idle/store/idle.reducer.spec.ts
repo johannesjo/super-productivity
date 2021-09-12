e0ad9255bc97282a2c576adee0d42f4769302319
Johannes Millan <johannes.millan@gmail.com>
2021-09-12 16:18:40 +0200

    test: deactivate trivial

--- src/app/features/idle/store/idle.reducer.spec.ts
+++ src/app/features/idle/store/idle.reducer.spec.ts
@@ -1,13 +1,13 @@
-import { initialIdleState, idleReducer } from './idle.reducer';
-
-describe('Idle Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
-
-      const result = idleReducer(initialIdleState, action);
-
-      expect(result).toBe(initialIdleState);
-    });
-  });
-});
+// import { initialIdleState, idleReducer } from './idle.reducer';
+//
+// describe('Idle Reducer', () => {
+//   describe('an unknown action', () => {
+//     it('should return the previous state', () => {
+//       const action = {} as any;
+//
+//       const result = idleReducer(initialIdleState, action);
+//
+//       expect(result).toBe(initialIdleState);
+//     });
+//   });
+// });
--- src/app/features/idle/store/idle.selectors.spec.ts
+++ src/app/features/idle/store/idle.selectors.spec.ts
@@ -1,12 +1,12 @@
-import * as fromIdle from './idle.reducer';
-import { selectIdleState } from './idle.selectors';
-
-describe('Idle Selectors', () => {
-  it('should select the feature state', () => {
-    const result = selectIdleState({
-      [fromIdle.IDLE_FEATURE_KEY]: {},
-    });
-
-    expect(result).toEqual({});
-  });
-});
+// import * as fromIdle from './idle.reducer';
+// import { selectIdleState } from './idle.selectors';
+//
+// describe('Idle Selectors', () => {
+//   it('should select the feature state', () => {
+//     const result = selectIdleState({
+//       [fromIdle.IDLE_FEATURE_KEY]: {},
+//     });
+//
+//     expect(result).toEqual({});
+//   });
+// });
