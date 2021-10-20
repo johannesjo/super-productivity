1abd103d3d97bf59ff6f6cda9680e887609e4c12
Nick <handnf@gmail.com>
2021-10-20 11:19:31 -0400

    Fix lint

--- src/app/features/tasks/task-attachment/task-attachment-list/task-attachment-list.component.ts
+++ src/app/features/tasks/task-attachment/task-attachment-list/task-attachment-list.component.ts
@@ -59,14 +59,14 @@ export class TaskAttachmentListComponent {
       });
   }
 
-  async copy(attachment?: TaskAttachment) {
+  async copy(attachment?: TaskAttachment): Promise<void> {
     if (!attachment) return;
     // Force-cast PermissionDescriptor as 'clipboard-write' is not defined in type
-    const permission = {name: 'clipboard-write'} as unknown as PermissionDescriptor;
+    const permission = { name: 'clipboard-write' } as unknown as PermissionDescriptor;
     const result = await navigator.permissions.query(permission);
-    if (result.state == "granted" || result.state == "prompt") {
+    if (result.state == 'granted' || result.state == 'prompt') {
       await navigator.clipboard.writeText(attachment.path);
-      this._snackService.open(T.GLOBAL_SNACK.COPY_TO_CLIPPBOARD)
+      this._snackService.open(T.GLOBAL_SNACK.COPY_TO_CLIPPBOARD);
     }
   }
 
