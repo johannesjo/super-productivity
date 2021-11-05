4dec2e4f0a1af4229a365069b7095743cd157b92
Johannes Millan <johannes.millan@gmail.com>
2021-11-05 11:31:54 +0100

    feat(notes): improve animation

--- src/app/features/tasks/task-additional-info/task-additional-info.ani.ts
+++ src/app/features/tasks/task-additional-info/task-additional-info.ani.ts
@@ -2,8 +2,8 @@ import { animate, style, transition, trigger } from '@angular/animations';
 import { ANI_ENTER_TIMING } from '../../../ui/animations/animation.const';
 
 const ANI = [
-  style({ opacity: 0, transform: 'scale(0.9)' }),
-  animate(ANI_ENTER_TIMING, style({ opacity: 1, transform: 'scale(1)' })),
+  style({ opacity: 0, transform: 'translateX(-100%)' }),
+  animate(ANI_ENTER_TIMING, style({ opacity: 1, transform: 'translateX(0)' })),
 ];
 
 export const taskAdditionalInfoTaskChangeAnimation = trigger(
