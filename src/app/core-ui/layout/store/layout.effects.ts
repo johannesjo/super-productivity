50152b5e6e60b729aec3f25baf3a02b16386650b
Johannes Millan <johannes.millan@gmail.com>
2021-11-05 16:13:32 +0100

    feat(notes): make note panel open work better

--- src/app/core-ui/layout/store/layout.effects.ts
+++ src/app/core-ui/layout/store/layout.effects.ts
@@ -3,7 +3,6 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { toggleShowNotes } from './layout.actions';
 import { concatMap, filter, withLatestFrom } from 'rxjs/operators';
 import { TaskService } from '../../../features/tasks/task.service';
-import { LayoutService } from '../layout.service';
 import { setSelectedTask } from '../../../features/tasks/store/task.actions';
 
 @Injectable()
@@ -11,18 +10,11 @@ export class LayoutEffects {
   showNotesWhenTaskIsSelected$ = createEffect(() =>
     this.actions$.pipe(
       ofType(toggleShowNotes),
-      withLatestFrom(this.taskService.selectedTaskId$, this.layoutService.isShowNotes$),
-      filter(
-        ([a, selectedTaskId, isShowNotes]) =>
-          isShowNotes === false && selectedTaskId !== null,
-      ),
+      withLatestFrom(this.taskService.selectedTaskId$),
+      filter(([a, selectedTaskId]) => selectedTaskId !== null),
       concatMap(() => [setSelectedTask({ id: null }), toggleShowNotes()]),
     ),
   );
 
-  constructor(
-    private actions$: Actions,
-    private taskService: TaskService,
-    private layoutService: LayoutService,
-  ) {}
+  constructor(private actions$: Actions, private taskService: TaskService) {}
 }
