598288e46a21c7337bbef7b8d329ed00e4f90a5c
Johannes Millan <johannes.millan@gmail.com>
2021-11-05 15:40:01 +0100

    feat(notes): improve panel ani

--- src/app/features/right-panel/right-panel.component.ts
+++ src/app/features/right-panel/right-panel.component.ts
@@ -6,13 +6,14 @@ import { TaskService } from '../tasks/task.service';
 import { LayoutService } from '../../core-ui/layout/layout.service';
 import { slideInFromTopAni } from '../../ui/animations/slide-in-from-top.ani';
 import { slideAdditionalInfoInFromLeftAni } from './slide-additional-info-in-from-left.ani';
+import { slideInFromRightAni } from '../../ui/animations/slide-in-from-right.ani';
 
 @Component({
   selector: 'right-panel',
   templateUrl: './right-panel.component.html',
   styleUrls: ['./right-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [slideAdditionalInfoInFromLeftAni, slideInFromTopAni],
+  animations: [slideAdditionalInfoInFromLeftAni, slideInFromTopAni, slideInFromRightAni],
 })
 export class RightPanelComponent {
   // NOTE: used for debugging
