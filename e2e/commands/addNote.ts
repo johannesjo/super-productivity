e0f2edaead053817905c093e300d44fa20f469dd
Johannes Millan <johannes.millan@gmail.com>
2021-11-06 12:32:36 +0100

    test: fix e2e

--- e2e/commands/addNote.ts
+++ e2e/commands/addNote.ts
@@ -1,13 +1,15 @@
 import { NightwatchBrowser } from 'nightwatch';
 
 const ADD_NOTE_BTN = '#add-note-btn';
+const NOTE_PANEL_BTN = '.toggle-notes-btn';
 const TEXTAREA = 'dialog-add-note textarea';
 const ADD_NOTE_SUBMIT_BTN = 'dialog-add-note button[type=submit]:enabled';
 const NOTES_WRAPPER = 'notes';
 
 module.exports = {
   async command(this: NightwatchBrowser, noteName: string) {
-    return this.moveToElement(NOTES_WRAPPER, 10, 50)
+    return this.waitForElementPresent(NOTE_PANEL_BTN)
+      .click(NOTE_PANEL_BTN)
       .waitForElementPresent(ADD_NOTE_BTN)
       .click(ADD_NOTE_BTN)
 
