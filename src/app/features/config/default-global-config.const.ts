d45714b2e0ee3f2aef8d4301d1352f86c75bd3aa
Gorka Kobeaga <gorka@kobeaga.eus>
2021-11-13 03:47:00 +0100

    Split take a break notification message into title and body

--- src/app/features/config/default-global-config.const.ts
+++ src/app/features/config/default-global-config.const.ts
@@ -48,7 +48,7 @@ export const DEFAULT_GLOBAL_CONFIG: GlobalConfigState = {
     isFocusWindow: false,
     /* eslint-disable-next-line */
     takeABreakMessage:
-      'Take a break! You have been working for ${duration} without one. Go away from the computer! Take a short walk! Makes you more productive in the long run!',
+      'You have been working for ${duration} without one. Go away from the computer! Take a short walk! Makes you more productive in the long run!',
     takeABreakMinWorkingTime: 60 * minute,
     takeABreakSnoozeTime: 15 * minute,
     motivationalImg: null,
