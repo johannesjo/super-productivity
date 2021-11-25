9c8f3bf4f656d9bca287cd8396a4e535a57141c1
Johannes Millan <johannes.millan@gmail.com>
2021-11-25 20:17:11 +0100

    refactor: move idle stuff to effect 3

--- src/app/features/idle/store/idle.actions.ts
+++ src/app/features/idle/store/idle.actions.ts
@@ -1,13 +1,24 @@
 import { createAction, props } from '@ngrx/store';
+import { TaskCopy } from '../../tasks/task.model';
 
 export const triggerIdle = createAction(
   '[Idle] Trigger Idle',
   props<{ idleTime: number }>(),
 );
 export const resetIdle = createAction('[Idle] Reset');
+export const openIdleDialog = createAction('[Idle] Open dialog');
 export const setIdleTime = createAction(
   '[Idle] Set idle time',
   props<{ idleTime: number }>(),
+);
+export const idleDialogResult = createAction(
+  '[Idle] dialog result',
+  props<{
+    timeSpent: number;
+    selectedTaskOrTitle: TaskCopy | string;
+    isResetBreakTimer: boolean;
+    isTrackAsBreak: boolean;
+  }>(),
 );
 
 // TODO better place would be the take a break module, if we ever add a store there
--- src/app/features/idle/store/idle.effects.ts
+++ src/app/features/idle/store/idle.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ChromeExtensionInterfaceService } from '../../../core/chrome-extension-interface/chrome-extension-interface.service';
 import { WorkContextService } from '../../work-context/work-context.service';
 import { ElectronService } from '../../../core/electron/electron.service';
@@ -10,20 +10,22 @@ import { Store } from '@ngrx/store';
 import { UiHelperService } from '../../ui-helper/ui-helper.service';
 import { IS_ELECTRON } from '../../../app.constants';
 import {
+  idleDialogResult,
   resetIdle,
   setIdleTime,
   triggerIdle,
   triggerResetBreakTimer,
 } from './idle.actions';
-import { DialogIdleComponent } from '../dialog-idle/dialog-idle.component';
 import { first, map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
-import { Task } from '../../tasks/task.model';
-import { getWorklogStr } from '../../../util/get-work-log-str';
 import { lazySetInterval } from '../../../../../electron/lazy-set-interval';
-import { fromEvent, Observable } from 'rxjs';
-import { selectIdleTime } from './idle.selectors';
+import { EMPTY, fromEvent, Observable, of } from 'rxjs';
 import { IpcRenderer } from 'electron';
 import { IPC } from '../../../../../electron/ipc-events.const';
+import { SimpleCounterService } from '../../simple-counter/simple-counter.service';
+import { DialogIdleComponent } from '../dialog-idle/dialog-idle.component';
+import { selectIdleTime } from './idle.selectors';
+import { Task } from '../../tasks/task.model';
+import { getWorklogStr } from '../../../util/get-work-log-str';
 
 const DEFAULT_MIN_IDLE_TIME = 60000;
 const IDLE_POLL_INTERVAL = 1000;
@@ -54,7 +56,129 @@ export class IdleEffects {
               });
             }),
           )
-      ).pipe(tap((idleTimeInMs) => this._handleIdle(idleTimeInMs as number))),
+      ).pipe(
+        switchMap((idleTimeInMs) => {
+          const idleTime = idleTimeInMs as number;
+          const gCfg = this._configService.cfg;
+          if (!gCfg) {
+            throw new Error();
+          }
+          const cfg = gCfg.idle;
+          const minIdleTime = cfg.minIdleTime || DEFAULT_MIN_IDLE_TIME;
+
+          // don't run if option is not enabled
+          if (
+            !cfg.isEnableIdleTimeTracking ||
+            (cfg.isOnlyOpenIdleWhenCurrentTask && !this._taskService.currentTaskId)
+          ) {
+            return of(resetIdle());
+          }
+          if (idleTime > minIdleTime) {
+            return of(triggerIdle({ idleTime }));
+          }
+          return EMPTY;
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  onTriggerIdle$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(triggerIdle),
+        tap(({ idleTime }) => {
+          if (!this.isIdleDialogOpen) {
+            this.isIdleDialogOpen = true;
+
+            if (IS_ELECTRON) {
+              this._uiHelperService.focusApp();
+            }
+
+            if (this._taskService.currentTaskId) {
+              // remove idle time already tracked
+              this._taskService.removeTimeSpent(
+                this._taskService.currentTaskId,
+                idleTime,
+              );
+              this.lastCurrentTaskId = this._taskService.currentTaskId;
+              this._taskService.setCurrentId(null);
+            } else {
+              this.lastCurrentTaskId = null;
+            }
+
+            this._initIdlePoll(idleTime);
+
+            this._matDialog
+              .open(DialogIdleComponent, {
+                restoreFocus: true,
+                disableClose: true,
+                data: {
+                  lastCurrentTaskId: this.lastCurrentTaskId,
+                  // todo get inside component instead maybe?
+                  idleTime$: this._store.select(selectIdleTime),
+                },
+              })
+              .afterClosed()
+              .pipe(withLatestFrom(this._store.select(selectIdleTime)))
+              .subscribe(
+                ([res, idleTimeI]: [
+                  {
+                    task: Task | string;
+                    isResetBreakTimer: boolean;
+                    isTrackAsBreak: boolean;
+                  },
+                  number,
+                ]) => {
+                  const { task, isResetBreakTimer, isTrackAsBreak } = res;
+                  this._store.dispatch(
+                    idleDialogResult({
+                      timeSpent: idleTimeI,
+                      selectedTaskOrTitle: task,
+                      isResetBreakTimer,
+                      isTrackAsBreak,
+                    }),
+                  );
+                },
+              );
+          }
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  onIdleDialogResult$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(idleDialogResult),
+        tap(({ timeSpent, selectedTaskOrTitle, isResetBreakTimer, isTrackAsBreak }) => {
+          if (isResetBreakTimer || isTrackAsBreak) {
+            this._store.dispatch(triggerResetBreakTimer());
+          }
+
+          if (isTrackAsBreak) {
+            this._workContextService.addToBreakTimeForActiveContext(undefined, timeSpent);
+          }
+
+          if (selectedTaskOrTitle) {
+            if (typeof selectedTaskOrTitle === 'string') {
+              const currId = this._taskService.add(selectedTaskOrTitle, false, {
+                timeSpent,
+                timeSpentOnDay: {
+                  [getWorklogStr()]: timeSpent,
+                },
+              });
+              this._taskService.setCurrentId(currId);
+            } else {
+              this._taskService.addTimeSpent(selectedTaskOrTitle, timeSpent);
+              this._taskService.setCurrentId(selectedTaskOrTitle.id);
+            }
+          }
+
+          this._cancelIdlePoll();
+          this._store.dispatch(resetIdle());
+          this.isIdleDialogOpen = false;
+        }),
+      ),
     { dispatch: false },
   );
 
@@ -64,107 +188,15 @@ export class IdleEffects {
     private _workContextService: WorkContextService,
     private _electronService: ElectronService,
     private _taskService: TaskService,
+    private _simpleCounterService: SimpleCounterService,
     private _configService: GlobalConfigService,
     private _matDialog: MatDialog,
     private _store: Store,
     private _uiHelperService: UiHelperService,
   ) {
     // window.setTimeout(() => {
-    //   this._handleIdle(800000);
+    //   this._store.dispatch(triggerIdle({ idleTime: 800000 }));
     // }, 700);
-  }
-
-  private _handleIdle(idleTime: number): void {
-    const gCfg = this._configService.cfg;
-    if (!gCfg) {
-      throw new Error();
-    }
-    const cfg = gCfg.idle;
-    const minIdleTime = cfg.minIdleTime || DEFAULT_MIN_IDLE_TIME;
-
-    // don't run if option is not enabled
-    if (
-      !cfg.isEnableIdleTimeTracking ||
-      (cfg.isOnlyOpenIdleWhenCurrentTask && !this._taskService.currentTaskId)
-    ) {
-      this._store.dispatch(resetIdle());
-      return;
-    }
-    if (idleTime > minIdleTime) {
-      this._store.dispatch(triggerIdle({ idleTime }));
-
-      if (!this.isIdleDialogOpen) {
-        if (IS_ELECTRON) {
-          this._uiHelperService.focusApp();
-        }
-
-        if (this._taskService.currentTaskId) {
-          // remove idle time already tracked
-          this._taskService.removeTimeSpent(this._taskService.currentTaskId, idleTime);
-          this.lastCurrentTaskId = this._taskService.currentTaskId;
-          this._taskService.setCurrentId(null);
-        } else {
-          this.lastCurrentTaskId = null;
-        }
-
-        this.isIdleDialogOpen = true;
-        this._initIdlePoll(idleTime);
-        this._matDialog
-          .open(DialogIdleComponent, {
-            restoreFocus: true,
-            disableClose: true,
-            data: {
-              lastCurrentTaskId: this.lastCurrentTaskId,
-              idleTime$: this._store.select(selectIdleTime),
-            },
-          })
-          .afterClosed()
-          .pipe(withLatestFrom(this._store.select(selectIdleTime)))
-          .subscribe(
-            ([res, idleTimeI]: [
-              {
-                task: Task | string;
-                isResetBreakTimer: boolean;
-                isTrackAsBreak: boolean;
-              },
-              number,
-            ]) => {
-              const { task, isResetBreakTimer, isTrackAsBreak } = res;
-              const timeSpent = idleTimeI;
-
-              if (isResetBreakTimer || isTrackAsBreak) {
-                this._store.dispatch(triggerResetBreakTimer());
-              }
-
-              if (isTrackAsBreak) {
-                this._workContextService.addToBreakTimeForActiveContext(
-                  undefined,
-                  timeSpent,
-                );
-              }
-
-              if (task) {
-                if (typeof task === 'string') {
-                  const currId = this._taskService.add(task, false, {
-                    timeSpent,
-                    timeSpentOnDay: {
-                      [getWorklogStr()]: timeSpent,
-                    },
-                  });
-                  this._taskService.setCurrentId(currId);
-                } else {
-                  this._taskService.addTimeSpent(task, timeSpent);
-                  this._taskService.setCurrentId(task.id);
-                }
-              }
-
-              this._cancelIdlePoll();
-              this._store.dispatch(resetIdle());
-              this.isIdleDialogOpen = false;
-            },
-          );
-      }
-    }
   }
 
   private _initIdlePoll(initialIdleTime: number): void {
