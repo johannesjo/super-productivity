441c300c49c11b3281c682ffe1e2c6c0cdcf034b
Johannes Millan <johannes.millan@gmail.com>
2021-11-26 18:36:34 +0100

    feat: improve update before import message 2

--- src/app/util/model-version.ts
+++ src/app/util/model-version.ts
@@ -17,7 +17,7 @@ export const isMigrateModel = (
       console.log(modelType, { importVersion, localVersion, modelData });
       alert(
         // eslint-disable-next-line max-len
-        `Cannot load model. Version to load (${importVersion}) is newer than the hard coded version (${localVersion}) for "${modelType}". Please close the app and update your local productivity version first, before importing the data.`,
+        `Cannot load model "${modelType}". Version to load (${importVersion}) is newer than the hard coded version (${localVersion}). Please close the app and update your local productivity version first, before importing the data.`,
       );
       throw new Error('Cannot load model. Version to load is newer than local');
     } else {
