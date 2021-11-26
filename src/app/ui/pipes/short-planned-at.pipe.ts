581343c6864e162855516021f42ce75dd4885ba5
Johannes Millan <johannes.millan@gmail.com>
2021-11-26 14:55:51 +0100

    feat: internationalize date for task schedule button

--- src/app/ui/pipes/short-planned-at.pipe.ts
+++ src/app/ui/pipes/short-planned-at.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { isToday } from '../../util/is-today.util';
 
@@ -6,17 +6,37 @@ import { isToday } from '../../util/is-today.util';
   name: 'shortPlannedAt',
 })
 export class ShortPlannedAtPipe implements PipeTransform {
-  constructor(private datePipe: DatePipe) {}
+  constructor(private datePipe: DatePipe, @Inject(LOCALE_ID) private locale: string) {}
 
   transform(value: number | null, ...args: unknown[]): string | null {
     if (typeof value !== 'number') {
       return null;
     }
 
+    const locale = this.locale;
+
     if (isToday(value)) {
-      return this.datePipe.transform(value, 'H:mm');
+      const str = `${new Date(value).toLocaleTimeString(locale, {
+        hour: 'numeric',
+        minute: 'numeric',
+      })}`;
+      // fallback as 12:00 PM is too long
+      if (str.length >= 7) {
+        return this.datePipe.transform(value, 'H:mm');
+      }
+      return str;
     } else {
-      return this.datePipe.transform(value, 'd.M');
+      const str = `${new Date(value).toLocaleDateString(locale, {
+        month: 'numeric',
+        day: 'numeric',
+      })}`;
+
+      const lastChar = str.slice(-1);
+
+      if (isNaN(lastChar as any)) {
+        return str.slice(0, -1);
+      }
+      return str;
     }
   }
 }
