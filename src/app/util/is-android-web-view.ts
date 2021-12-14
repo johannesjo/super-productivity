fb44ca0bc3d722a6f53f0b3e7a686300cb0343cd
Johannes Millan <johannes.millan@gmail.com>
2021-12-14 13:07:46 +0100

    feat: make reading android version work

--- src/app/util/is-android-web-view.ts
+++ src/app/util/is-android-web-view.ts
@@ -1,7 +1,8 @@
 export const IS_ANDROID_WEB_VIEW = !!(window as any).SUPAndroid;
 export const IS_F_DROID_APP = !!(window as any).SUPFDroid;
-export const ANDROID_APP_VERSION: string | undefined = (window as any)
-  .SUP_ANDROID_VERSION;
+export const ANDROID_APP_VERSION: string | null = new URL(
+  window.location.href,
+).searchParams.get('androidVersion');
 
 export const IS_ANDROID_NEW_DATA_INTERFACE: boolean =
   !!ANDROID_APP_VERSION && +(ANDROID_APP_VERSION as any) >= 16;
