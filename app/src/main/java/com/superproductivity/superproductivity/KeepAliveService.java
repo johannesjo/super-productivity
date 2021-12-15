5571a9057a5a8a9f2547c3dae4ffc0200a9c539e
Johannes Millan <johannes.millan@gmail.com>
2021-12-15 12:16:03 +0100

    feat(keepAlive): prepare updating the notification

--- app/src/main/java/com/superproductivity/superproductivity/KeepAliveService.java
+++ app/src/main/java/com/superproductivity/superproductivity/KeepAliveService.java
@@ -15,6 +15,10 @@ import androidx.core.app.NotificationCompat;
 public class KeepAliveService extends Service {
     private static final int NOTIFY_ID = 1;
     private static final String NOTIFY_CHANNEL_ID = "SUP_KeepAlive";
+    private  NotificationManager notificationManager;
+    private final NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+            NOTIFY_CHANNEL_ID);
+
 
     @Nullable
     @Override
@@ -23,17 +27,20 @@ public class KeepAliveService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
-
-        // do your jobs here
-
+    public int onStartCommand(Intent intent, int flags, int startId) {
         startForeground();
-
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void updateNotification(String message, int progress) {
+        builder.setContentText(message)
+                .setProgress(100, progress, true);
+        notificationManager.notify(NOTIFY_ID, builder.build());
     }
 
     private void startForeground() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // create notification channel
             CharSequence name = NOTIFY_CHANNEL_ID;
             String description = "Here to keep the app alive for syncing";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
@@ -41,24 +48,24 @@ public class KeepAliveService extends Service {
             NotificationChannel channel = null;
             channel = new NotificationChannel(NOTIFY_CHANNEL_ID, name, importance);
             channel.setDescription(description);
-            // Don't see these lines in your code...
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
 
+            // create notification
             Intent notificationIntent = new Intent(this, FullscreenActivity.class);
-
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                     notificationIntent, 0);
 
-            startForeground(NOTIFY_ID, new NotificationCompat.Builder(this,
-                    NOTIFY_CHANNEL_ID) // don't forget create a notification channel first
+            startForeground(NOTIFY_ID, builder
                     .setOngoing(true)
+                    .setOnlyAlertOnce(true)
                     .setSmallIcon(R.drawable.ic_sp)
                     .setContentTitle(getString(R.string.app_name))
                     .setContentText("Service is running background")
                     .setContentIntent(pendingIntent)
                     .build());
 
+            updateNotification("YEAH", 22);
         }
     }
 }
