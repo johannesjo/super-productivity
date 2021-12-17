fbbd757b54b42c4ab77ad0849ef90f9ccb00bef5
Johannes Millan <johannes.millan@gmail.com>
2021-12-17 21:32:43 +0100

    feat: finalize new promise interface

--- app/src/main/java/com/superproductivity/superproductivity/App.java
+++ app/src/main/java/com/superproductivity/superproductivity/App.java
@@ -34,14 +34,14 @@ public class App extends Application implements LifecycleObserver {
         if (wv != null) {
             // needs to come last for some settings to take effect
             if (IS_DEBUG) {
-                String url = "https://test-app.super-productivity.com?androidVersion=" + BuildConfig.VERSION_CODE;
-//                String url = "http://10.0.2.2:4200/?androidVersion=" + BuildConfig.VERSION_CODE;
-                // String url = "https://app.super-productivity.com?androidVersion=" + BuildConfig.VERSION_CODE;
+//                String url = "https://test-app.super-productivity.com";
+                String url = "http://10.0.2.2:4200/";
+                // String url = "https://app.super-productivity.com";
                 wv.loadUrl(url);
                 Toast.makeText(this, "DEBUG: " + url, Toast.LENGTH_SHORT).show();
             } else {
-                wv.loadUrl("https://app.super-productivity.com?androidVersion=" + BuildConfig.VERSION_CODE);
-                // wv.loadUrl("https://test-app.super-productivity.com?androidVersion=" + BuildConfig.VERSION_CODE);
+                wv.loadUrl("https://app.super-productivity.com");
+                // wv.loadUrl("https://test-app.super-productivity.com");
             }
         }
     }
