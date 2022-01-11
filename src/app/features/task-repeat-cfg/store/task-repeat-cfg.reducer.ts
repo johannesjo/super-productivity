041733d97ade67a8a8ad52556e4151810281fc31
Johannes Millan <johannes.millan@gmail.com>
2022-01-11 14:50:19 +0100

    refactor: cleanup

--- src/app/features/task-repeat-cfg/store/task-repeat-cfg.reducer.ts
+++ src/app/features/task-repeat-cfg/store/task-repeat-cfg.reducer.ts
@@ -84,7 +84,6 @@ export const selectTaskRepeatCfgsDueOnDay = createSelector(
             }
             const startDateDate = new Date(taskRepeatCfg.startDate);
             const diffInDays = getDiffInDays(startDateDate, dateToCheckDate);
-            console.log(diffInDays);
 
             return (
               // start date is not in the future
