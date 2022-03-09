7e36d32edbfa5255486f4c0e0648f5bdde68a135
EngelPika32 <41995155+EngelPika32@users.noreply.github.com>
2022-03-09 03:55:43 +0100

    test(repeatTask): add more detailed tests for WEEKLY
    ✅ Add 'multiple week' tests for repeated tasks
    🧪 Add 'incorrect weekday scheduling' tests for repeated tasks
    
    Both tests are well documented with comments.

--- src/app/features/task-repeat-cfg/store/task-repeat-cfg.reducer.spec.ts
+++ src/app/features/task-repeat-cfg/store/task-repeat-cfg.reducer.spec.ts
@@ -341,6 +341,142 @@ describe('selectTaskRepeatCfgsDueOnDay', () => {
       expect(resultIds).toEqual([]);
     });
 
+    // Multiple Weeks Test
+    // Please keep in mind, this test triggers every day of the week. Hence, it does not test the behaviour, if a day is skipped.
+    const repeatableTasksForMultiWeekTest = [
+      dummyRepeatable('R1', {
+        monday: true,
+        repeatCycle: 'WEEKLY',
+        startDate: '2022-01-10', // Monday
+        repeatEvery: 2,
+      }),
+      dummyRepeatable('R2', {
+        wednesday: true,
+        startDate: '2022-01-23', // Sunday
+        repeatCycle: 'WEEKLY',
+      }),
+      dummyRepeatable('R3', {
+        wednesday: true,
+        friday: true,
+        startDate: '2022-01-10', // Monday
+        repeatCycle: 'WEEKLY',
+        repeatEvery: 1,
+      }),
+      dummyRepeatable('R4', {
+        friday: true,
+        startDate: '2022-01-14', // Friday
+        repeatCycle: 'WEEKLY',
+        repeatEvery: 3,
+      }),
+    ];
+    const expectedMultipleTest = {
+      // R1 is every 2 weeks
+      // R3 repeats every week, (starts in the first week, at the first day)
+      0: [['R1'], [], ['R3'], [], ['R3', 'R4'], [], []], // R2 hasn't started // R4 starts this Friday and is scheduled Fridays
+      1: [[], [], ['R3'], [], ['R3'], [], []], // R2 starts this Sunday
+      2: [['R1'], [], ['R2', 'R3'], [], ['R3'], [], []],
+      3: [[], [], ['R2', 'R3'], [], ['R3', 'R4'], [], []], // R4 repeats in the third week
+      4: [['R1'], [], ['R2', 'R3'], [], ['R3'], [], []],
+    };
+    for (let w = 0; w < 5; w++) {
+      it('should work correctly for multiple weeks (week ' + w + ')', () => {
+        const week = w;
+        // prettier-ignore
+        // eslint-disable-next-line no-mixed-operators
+        const FULL_WEEK = [0, 1, 2, 3, 4, 5, 6].map((v) => FAKE_MONDAY_THE_10TH + (week * 7 * DAY) + v * DAY);
+        const results = FULL_WEEK.map((dayTimestamp) =>
+          selectTaskRepeatCfgsDueOnDay
+            .projector(repeatableTasksForMultiWeekTest, { dayDate: dayTimestamp })
+            .map((item) => item.id),
+        );
+        // prettier-ignore
+        // @ts-ignore
+        const printWeekStart = new Date(FULL_WEEK[0]).toLocaleTimeString('en-us', datePrintingOptions);
+        console.log('Week ' + week + ': ', results, '\tWeek-Start: ' + printWeekStart);
+        console.log('Should: ', expectedMultipleTest[week]);
+        expect(results).toEqual(expectedMultipleTest[week]);
+      });
+    }
+    // End of multiple test
+
+    // Start incorrect weekday scheduling test
+    // This test covers most use-cases, which aren't tested already.
+    // Though the main goal is to test correct handling of 'skipped' weekdays,
+    // it partly tests the correct handling of the startDate as well (hence the first 'empty' week).
+    // prettier-ignore
+    const lastTaskCreation = {
+      'R1': ['2022-01-10', '2022-01-10', '2022-01-17', '2022-01-24'], // Monday: created, Saturday not opened; Next Monday: Update, Next Monday: Update
+      'R2': [
+        // Monday   , Tuesday     , Wednesday   , Friday
+        '2022-01-14', '2022-01-14', '2022-01-14', '2022-01-14', // week 0 // "created repeatedTask"
+        '2022-01-14', '2022-01-17',  '2022-01-17', '2022-01-17', // week 1 // Monday repeated; Friday (catch up) repeated
+        '2022-01-21', '2022-01-24',  '2022-01-24', '2022-01-24', // week 2 // Monday repeated; Friday (catch up) repeated
+      ],
+    };
+    // prettier-ignore
+    const expectedIncorrectWeekdayTest = {
+       // Monday, Tuesday, Wednesday, Friday
+      0: [[], [], [], []], // R2 starts this Friday
+      1: [['R1', 'R2'], [], [], ['R2']], // R1 starts this Monday
+      2: [['R1', 'R2'], [], [], ['R2']],
+    };
+    for (let w = 0; w < 3; w++) {
+      it('should return cfg after missed day(s) (week ' + w + ')', () => {
+        const week = w;
+        // Monday, Tuesday, Wednesday, Friday
+        // prettier-ignore
+        // eslint-disable-next-line no-mixed-operators
+        const SOME_WEEK_DAYS = [0, 1, 2, 4].map((v) => FAKE_MONDAY_THE_10TH + (week * 7 * DAY) + v * DAY);
+        // We can dump every weekday into the function, as selectTaskRepeatCfgsDueOnDay is only filtering.
+        let c = w * 4;
+        const results = [] as any;
+        for (const dayTimestamp of SOME_WEEK_DAYS) {
+          results.push(
+            selectTaskRepeatCfgsDueOnDay
+              .projector(
+                [
+                  dummyRepeatable('R1', {
+                    saturday: true,
+                    repeatCycle: 'WEEKLY',
+                    startDate: '2022-01-17', // Monday
+                    repeatEvery: 1,
+                    lastTaskCreation: new Date(lastTaskCreation['R1'][week]).getTime(),
+                  }),
+                  dummyRepeatable('R2', {
+                    monday: true,
+                    thursday: true,
+                    startDate: '2022-01-14', // Friday
+                    repeatCycle: 'WEEKLY',
+                    repeatEvery: 1,
+                    lastTaskCreation: new Date(lastTaskCreation['R2'][c]).getTime(),
+                  }),
+                ],
+                { dayDate: dayTimestamp },
+              )
+              .map((item) => item.id),
+          );
+          c++;
+        }
+
+        // prettier-ignore
+        // @ts-ignore
+        const printWeekStart = new Date(SOME_WEEK_DAYS[0]).toLocaleTimeString('en-us', datePrintingOptions);
+        // prettier-ignore
+        // @ts-ignore
+        const printLastCreatedR1 = new Date(lastTaskCreation['R1'][week]).toLocaleTimeString('en-us', datePrintingOptions);
+        // prettier-ignore
+        // @ts-ignore
+        const printLastCreatedR2Monday = new Date(lastTaskCreation['R2'][week * 4]).toLocaleTimeString('en-us', datePrintingOptions);
+        console.log('Week ' + week + ': ', results, '\tWeek-Start: ' + printWeekStart);
+        console.log('Should: ', expectedIncorrectWeekdayTest[week]);
+        // prettier-ignore
+        console.log('LastCreated(R1): ' + printLastCreatedR1 + '\tLastCreated(R2)[Monday]: ' + printLastCreatedR2Monday)
+        expect(results).toEqual(expectedIncorrectWeekdayTest[week]);
+      });
+    }
+    // End incorrect weekday scheduling test
+
+    // End of "for WEEKLY"
   });
 
   describe('for MONTHLY', () => {
