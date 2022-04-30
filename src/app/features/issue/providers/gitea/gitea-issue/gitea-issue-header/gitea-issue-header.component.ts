61faa5421a27058898647f016ed758e60905066a
Hugaleno Bezerra <hugaleno@gmail.com>
2022-04-30 11:18:14 -0300

    Add issue content components

---
+++ src/app/features/issue/providers/gitea/gitea-issue/gitea-issue-header/gitea-issue-header.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { TaskWithSubTasks } from '../../../../../tasks/task.model';
+import { T } from '../../../../../../t.const';
+
+@Component({
+  selector: 'gitea-issue-header',
+  templateUrl: './gitea-issue-header.component.html',
+  styleUrls: ['./gitea-issue-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GiteaIssueHeaderComponent {
+  T: typeof T = T;
+  @Input() task?: TaskWithSubTasks;
+
+  constructor() {}
+}
