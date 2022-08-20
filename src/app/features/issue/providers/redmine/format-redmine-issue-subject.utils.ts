9d0bf27fd299917ae3b8ba95a65cc123cffd8981
Jonas Peterburs <j.peterburs@protonmail.com>
2022-08-20 09:27:22 +0000

    Add common interface implementation

---
+++ src/app/features/issue/providers/redmine/format-redmine-issue-subject.utils.ts
@@ -0,0 +1,10 @@
+import { RedmineIssue } from './redmine-issue/redmine-issue.model';
+import { truncate } from '../../../../util/truncate';
+
+export const formatRedmineIssueSubject = ({ id, subject }: RedmineIssue): string => {
+  return `#${id} ${subject}`;
+};
+
+export const formatRedmineIssueSubjectForSnack = (issue: RedmineIssue): string => {
+  return `${truncate(formatRedmineIssueSubject(issue))}`;
+};
