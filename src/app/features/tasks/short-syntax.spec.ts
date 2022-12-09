c9c56d68f395b4acae7f6a55504202fd8a7a0602
Trang Le <lethutrang101@gmail.com>
2022-12-09 23:54:06 +0700

    reformat

--- src/app/features/tasks/short-syntax.spec.ts
+++ src/app/features/tasks/short-syntax.spec.ts
@@ -242,6 +242,17 @@ describe('shortSyntax', () => {
     });
   });
 
+  describe('tags', () => {
+    it('should not trigger for tasks with starting # (e.g. github issues)', () => {
+      const t = {
+        ...TASK,
+        title: '#134 Fun title',
+      };
+      const r = shortSyntax(t, ALL_TAGS);
+
+      expect(r).toEqual(undefined);
+    });
+
     it('should not trigger for tasks with starting # (e.g. github issues) when adding tags', () => {
       const t = {
         ...TASK,
