d8007b8b4d7a95d657c24871a01e26e983b56a81
velvet <berhram@protonmail.com>
2022-12-11 04:36:56 +0300

    fix: activity crash after reopening

--- app/src/main/java/com/superproductivity/superproductivity/FullscreenActivity.kt
+++ app/src/main/java/com/superproductivity/superproductivity/FullscreenActivity.kt
@@ -12,6 +12,7 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,8 +21,9 @@ import androidx.appcompat.app.AppCompatActivity
  * status bar and navigation/system bar) with user interaction.
  */
 class FullscreenActivity : AppCompatActivity() {
-    private lateinit var jsi: CommonJavaScriptInterface
-    private lateinit var wv: WebView
+    private lateinit var javaScriptInterface: CommonJavaScriptInterface
+    private lateinit var webView: WebView
+    private lateinit var wvContainer: FrameLayout
     var isInForeground: Boolean = false
 
     @Suppress("ReplaceCallWithBinaryOperator")
@@ -32,112 +34,10 @@ class FullscreenActivity : AppCompatActivity() {
         } else {
             startService(Intent(this, KeepAliveNotificationService::class.java))
         }
-        wv = (application as App).wv
-        // if your build is in debug mode, enable inspecting of web views
-        if (BuildConfig.DEBUG) {
-            WebView.setWebContentsDebuggingEnabled(true)
-        }
-        if (BuildConfig.DEBUG) {
-            // val url = "https://test-app.super-productivity.com"
-            //TODO TODO TODO
-//          val url = "http://10.0.2.2:4200/"
-            val url = "https://app.super-productivity.com"
-            wv.loadUrl(url)
-            Toast.makeText(this, "DEBUG: $url", Toast.LENGTH_SHORT).show()
-        } else {
-            wv.loadUrl("https://app.super-productivity.com")
-            // wv.loadUrl("https://test-app.super-productivity.com")
-        }
-        // In case we want to make sure the most recent version is loaded
-        if (BuildConfig.DEBUG) {
-            wv.clearCache(true)
-            wv.clearHistory()
-        }
-        Log.v("TW", "FullScreenActivity: onCreate")
-        if (savedInstanceState == null) {
-            Log.v("TW", "FullScreenActivity: onCreate reload")
-        }
-        // hide action bar
-        supportActionBar?.hide()
-        // init web view
-        setContentView(wv)
-        // init JS here, as it needs an activity to work
-        jsi = JavaScriptInterface(this)
-        wv.addJavascriptInterface(jsi, INTERFACE_PROPERTY)
-        if (BuildConfig.FLAVOR.equals("fdroid")) {
-            wv.addJavascriptInterface(jsi, INTERFACE_PROPERTY_F_DROID)
-        }
-        // also needs to be done here, because new Intent otherwise will crash the app
-        wv.webViewClient = object : WebViewClient() {
-
-            @Deprecated("Deprecated in Java")
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                Log.v("TW", url)
-
-                return if (url.startsWith("http://") || url.startsWith("https://")) {
-                    if (url.contains("super-productivity.com") || url.contains("localhost") || url.contains(
-                            "10.0.2.2:4200"
-                        )
-                    ) {
-                        false
-                    } else {
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                        true
-                    }
-                } else {
-                    false
-                }
-            }
-
-
-            override fun shouldInterceptRequest(
-                view: WebView?,
-                request: WebResourceRequest?
-            ): WebResourceResponse? {
-                if (request?.isForMainFrame == false && request.url?.path?.contains("assets/icons/favicon") == true) {
-                    try {
-                        return WebResourceResponse("image/png", null, null)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                }
-                return null
-            }
-        }
-        wv.webChromeClient = object : WebChromeClient() {
-            override fun onJsAlert(
-                view: WebView,
-                url: String,
-                message: String,
-                result: JsResult
-            ): Boolean {
-                Log.v("TW", "onJsAlert")
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this@FullscreenActivity)
-                builder.setMessage(message)
-                    .setNeutralButton("OK") { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .create()
-                    .show()
-                result.cancel()
-                return super.onJsAlert(view, url, message, result)
-            }
-
-            override fun onJsConfirm(
-                view: WebView,
-                url: String,
-                message: String,
-                result: JsResult
-            ): Boolean {
-                AlertDialog.Builder(this@FullscreenActivity)
-                    .setMessage(message)
-                    .setPositiveButton(android.R.string.ok) { _, _ -> result.confirm() }
-                    .setNegativeButton(android.R.string.cancel) { _, _ -> result.cancel() }
-                    .create()
-                    .show()
-                return true
-            }
-        }
+        initWebView()
+        setContentView(R.layout.activity_fullscreen)
+        wvContainer = findViewById(R.id.webview_container)
+        wvContainer.addView(webView)
     }
 
     override fun onPause() {
@@ -178,13 +78,103 @@ class FullscreenActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (data != null) {
-            jsi.onActivityResult(requestCode, resultCode, data)
+    private fun initWebView() {
+        webView = (application as App).wv
+        val url: String
+        if (BuildConfig.DEBUG) {
+            url = "https://app.super-productivity.com"
+            Toast.makeText(this, "DEBUG: $url", Toast.LENGTH_SHORT).show()
+            webView.clearCache(true)
+            webView.clearHistory()
+        } else {
+            url = "https://app.super-productivity.com"
+        }
+        webView.loadUrl(url)
+        supportActionBar?.hide()
+        javaScriptInterface = JavaScriptInterface(this)
+        if (BuildConfig.FLAVOR.equals("fdroid")) {
+            webView.addJavascriptInterface(javaScriptInterface, INTERFACE_PROPERTY_F_DROID)
+        } else {
+            webView.addJavascriptInterface(javaScriptInterface, INTERFACE_PROPERTY)
+        }
+        webView.webViewClient = object : WebViewClient() {
+
+            @Deprecated("Deprecated in Java")
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                Log.v("TW", url)
+
+                return if (url.startsWith("http://") || url.startsWith("https://")) {
+                    if (url.contains("super-productivity.com") || url.contains("localhost") || url.contains(
+                            "10.0.2.2:4200"
+                        )
+                    ) {
+                        false
+                    } else {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                        true
+                    }
+                } else {
+                    false
+                }
+            }
+
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): WebResourceResponse? {
+                if (request?.isForMainFrame == false && request.url?.path?.contains("assets/icons/favicon") == true) {
+                    try {
+                        return WebResourceResponse("image/png", null, null)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                return null
+            }
+        }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                Log.v("TW", "onJsAlert")
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this@FullscreenActivity)
+                builder.setMessage(message)
+                    .setNeutralButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+                result.cancel()
+                return super.onJsAlert(view, url, message, result)
+            }
+
+            override fun onJsConfirm(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                AlertDialog.Builder(this@FullscreenActivity)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok) { _, _ -> result.confirm() }
+                    .setNegativeButton(android.R.string.cancel) { _, _ -> result.cancel() }
+                    .create()
+                    .show()
+                return true
+            }
         }
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            javaScriptInterface.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 
     private fun callJSInterfaceFunctionIfExists(fnName: String, objectPath: String) {
         val fnFullName = "window.$INTERFACE_PROPERTY.$objectPath.$fnName"
@@ -193,16 +183,21 @@ class FullscreenActivity : AppCompatActivity() {
     }
 
     fun callJavaScriptFunction(script: String) {
-        wv.post { wv.evaluateJavascript(script) { } }
+        webView.post { webView.evaluateJavascript(script) { } }
     }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
-        if (wv.canGoBack()) {
-            wv.goBack()
+        if (webView.canGoBack()) {
+            webView.goBack()
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun onDestroy() {
+        wvContainer.removeView(webView)
+        super.onDestroy()
     }
 
     companion object {
