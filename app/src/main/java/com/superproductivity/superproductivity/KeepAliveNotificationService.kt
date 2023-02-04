f19a1f6dd53e1bf1c031900d68ad32f2639b5abd
Stephen L <lrq3000@gmail.com>
2023-02-04 23:21:26 +0100

    add comments to avoid future regressions
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- app/src/main/java/com/superproductivity/superproductivity/KeepAliveNotificationService.kt
+++ app/src/main/java/com/superproductivity/superproductivity/KeepAliveNotificationService.kt
@@ -117,7 +117,7 @@ class KeepAliveNotificationService : Service() {
         if (message == "") {
             builder.setContentText(null)
         }
-        if (::notificationManager.isInitialized) {
+        if (::notificationManager.isInitialized) { // check if lateinit object is initialized, avoids Android <= 7.1 crashes
             notificationManager.notify(NOTIFY_ID, builder.build())
         }
     }
@@ -133,7 +133,7 @@ class KeepAliveNotificationService : Service() {
                 importance
             )
             channel.description = description
-            if (::notificationManager.isInitialized) {
+            if (::notificationManager.isInitialized) { // check if lateinit object is initialized, avoids Android <= 7.1 crashes
                 notificationManager = getSystemService(NotificationManager::class.java)
                 notificationManager.createNotificationChannel(channel)
             }
