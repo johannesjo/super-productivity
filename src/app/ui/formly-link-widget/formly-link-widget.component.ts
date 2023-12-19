e1f6dc19c61ce9f992a2ebe6dbce2599063ca589
Johannes Millan <johannes.millan@gmail.com>
2023-12-19 13:40:16 +0100

    refactor: fix lint

--- src/app/ui/formly-link-widget/formly-link-widget.component.ts
+++ src/app/ui/formly-link-widget/formly-link-widget.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 
 @Component({
