dec2eb702c4a303b2299126616a5a0c56f38ebe7
Johannes Millan <johannes.millan@gmail.com>
2023-12-29 13:04:35 +0100

    fix: wrong backup path being used #2910

--- electron/backup.ts
+++ electron/backup.ts
@@ -13,11 +13,7 @@ import * as path from 'path';
 import { error, log } from 'electron-log/main';
 import { AppDataComplete } from '../src/app/imex/sync/sync.model';
 
-let BACKUP_DIR =
-  `${app.getPath('userData')}` + process.platform == 'linux' ||
-  process.platform === 'darwin'
-    ? `/`
-    : `\\` + `backups`;
+let BACKUP_DIR = path.join(app.getPath('userData'), `backups`);
 
 // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
 export function initBackupAdapter(backupDir: string): void {
