127a5976be02971a6e110185b76ec84f62217786
Johannes Millan <johannes.millan@gmail.com>
2023-12-30 14:24:33 +0100

    fix: problematic ipcEvent$ case #2949

--- src/app/core/electron/exec-before-close.service.ts
+++ src/app/core/electron/exec-before-close.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { IPC } from '../../../../electron/shared-with-frontend/ipc-events.const';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IS_ELECTRON } from '../../app.constants';
-import { ipcEvent$ } from '../../util/ipc-event';
+import { ipcNotifyOnClose$ } from '../ipc-events';
 
 @Injectable({ providedIn: 'root' })
 export class ExecBeforeCloseService {
   onBeforeClose$: Observable<string[]> = IS_ELECTRON
-    ? ipcEvent$(IPC.NOTIFY_ON_CLOSE).pipe(map(([, ids]: any) => ids))
+    ? ipcNotifyOnClose$.pipe(map(([, ids]: any) => ids))
     : of([]);
 
   constructor() {}
--- src/app/util/ipc-event.ts
+++ src/app/util/ipc-event.ts
@@ -1,25 +1,35 @@
 import { Observable, Subject } from 'rxjs';
 import { IS_ELECTRON } from '../app.constants';
-import { finalize } from 'rxjs/operators';
+import { devError } from './dev-error';
+
+const handlerMap: { [key: string]: Observable<any> } = {};
 
 export const ipcEvent$ = (evName: string): Observable<unknown[]> => {
   if (!IS_ELECTRON) {
-    throw new Error('Not possible outside electron context');
+    devError(`ipcEvent$[${evName}] Not possible outside electron context`);
   }
 
   const subject = new Subject<unknown[]>();
+  if (handlerMap[evName]) {
+    console.log(handlerMap);
+    devError(`ipcEvent$[${evName}] should only ever be registered once`);
+    return handlerMap[evName];
+  }
+  handlerMap[evName] = subject;
 
   const handler: (...args: any[]) => void = (...args): void => {
+    console.log('ipcEvent$ trigger', evName);
     subject.next([...args]);
   };
   window.ea.on(evName, handler);
 
-  return subject.pipe(
-    finalize(() => {
-      console.log('FINALIZE', evName);
-      // NOTE doesn't work due to the different contexts
-      // window.ea.off(evName, handler);
-      throw new Error(`ipcEvent$[${evName}] observables live forever`);
-    }),
-  );
+  return subject;
+  // return subject.pipe(
+  //   // finalize(() => {
+  //   //   console.log('FINALIZE', evName);
+  //   //   // NOTE doesn't work due to the different contexts
+  //   //   // window.ea.off(evName, handler);
+  //   //   devError(`ipcEvent$[${evName}] observables live forever`);
+  //   // }),
+  // );
 };
