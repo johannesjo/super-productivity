b562471c2dd306ca465db18782d71441b33a67d1
Johannes Millan <johannes.millan@gmail.com>
2024-01-18 16:09:15 +0100

    fix(calendarIntegration): wrong component name

--- src/app/features/timeline/timeline-calendar-event/timeline-calendar-event.component.ts
+++ src/app/features/timeline/timeline-calendar-event/timeline-calendar-event.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TimelineFromCalendarEvent } from '../timeline.model';
 
 @Component({
-  selector: 'timeline-calendarIntegration-event',
+  selector: 'timeline-calendar-event',
   templateUrl: './timeline-calendar-event.component.html',
   styleUrls: ['./timeline-calendar-event.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
