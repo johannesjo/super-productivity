721aa86271c2aa7cb79b9631377f29874d857670
Johannes Millan <johannes.millan@gmail.com>
2024-01-24 15:30:27 +0100

    feat(tour): add basic steps2

--- src/app/features/shepherd/shepherd-steps.const.ts
+++ src/app/features/shepherd/shepherd-steps.const.ts
@@ -3,8 +3,11 @@ import { ShepherdService } from 'angular-shepherd';
 import { waitForEl, waitForObs } from './shepherd-helper';
 import { LayoutService } from '../../core-ui/layout/layout.service';
 import { TaskService } from '../tasks/task.service';
-import { distinctUntilChanged, filter, skip, tap } from 'rxjs/operators';
+import { filter, first } from 'rxjs/operators';
 import { promiseTimeout } from '../../util/promise-timeout';
+import { Actions, ofType } from '@ngrx/effects';
+import { deleteTask } from '../tasks/store/task.actions';
+import { GlobalConfigState } from '../config/global-config.model';
 
 const NEXT_BTN = {
   classes: 'shepherd-button-primary',
@@ -28,6 +31,8 @@ export const SHEPHERD_STANDARD_BTNS = [
 
 export const SHEPHERD_STEPS = (
   shepherdService: ShepherdService,
+  cfg: GlobalConfigState,
+  actions$: Actions,
   layoutService: LayoutService,
   taskService: TaskService,
 ): Array<Step.StepOptions> => [
@@ -44,157 +49,166 @@ export const SHEPHERD_STEPS = (
     },
     buttons: [],
   },
-  // {
-  //   title: 'Welcome to Super Productivity!!',
-  //   text: 'Super Productivity is a ToDo app that helps you to improve your personal workflows.',
-  //   buttons: SHEPHERD_STANDARD_BTNS,
-  // },
-  // {
-  //   attachTo: {
-  //     element: '.action-nav button',
-  //     on: 'bottom',
-  //   },
-  //   when: {
-  //     show: () => {
-  //       waitForEl('app-root > add-task-bar input', () => shepherdService.next());
-  //     },
-  //   },
-  //   scrollTo: false,
-  //   title: "Let's add your first task!",
-  //   text: 'Click on this button or press <kbd>Shift</kbd> + <kbd>a</kbd>',
-  //   advanceOn: {
-  //     selector: '.action-nav button',
-  //     event: 'click',
-  //   },
-  // },
-  //
-  // {
-  //   attachTo: {
-  //     element: 'add-task-bar',
-  //     on: 'bottom',
-  //   },
-  //   beforeShowPromise: () => promiseTimeout(1000),
-  //   when: {
-  //     show: () => {
-  //       waitForEl('task', () => {
-  //         layoutService.hideAddTaskBar();
-  //         shepherdService.next();
-  //       });
-  //     },
-  //   },
-  //   scrollTo: false,
-  //   title: 'Enter a title!',
-  //   text: 'Enter the title you want to give your task and hit the <kbd>Enter</kbd> key. After that you can press the <kbd>Escape</kbd> key or click anywhere on the grayed out backdrop to leave the add task bar.',
-  // },
-  // {
-  //   title: 'Congrats! This is your first task!',
-  //   text: 'Hover over it with your mouse',
-  //   attachTo: {
-  //     element: 'task',
-  //     on: 'bottom',
-  //   },
-  //   beforeShowPromise: () => promiseTimeout(1000),
-  //   when: {
-  //     show: () => {
-  //       setTimeout(() => {
-  //         waitForEl('task .hover-controls', () => shepherdService.next());
-  //       }, 1000);
-  //     },
-  //   },
-  // },
-  // {
-  //   title: 'Start Tracking',
-  //   attachTo: {
-  //     element: '.start-task-btn',
-  //     on: 'bottom',
-  //   },
-  //   text: 'Pressing the play button will start your first time tracking session. Time tracking is useful since it allows you to get a better idea on how you spend your time.',
-  //   //   attachTo: {
-  //   //     element: 'add-task-bar',
-  //   //     on: 'bottom',
-  //   //   },
-  //
-  //   ...waitForObs(taskService.currentTaskId$.pipe(filter((id) => !!id)), () =>
-  //     shepherdService.next(),
-  //   ),
-  //
-  // },
-  // {
-  //   title: 'Stop Tracking',
-  //   text: 'To stop tracking click on the pause button.',
-  //   attachTo: {
-  //     element: '.start-task-btn',
-  //     on: 'bottom',
-  //   },
-  //   ...waitForObs(taskService.currentTaskId$.pipe(filter((id) => !id)), () =>
-  //     shepherdService.next(),
-  //   ),
-  // },
-  // {
-  //   title: 'Edit Task Title',
-  //   text: 'You can edit the task title by clicking on it. Do this now and change the title to something else!',
-  //   attachTo: {
-  //     element: '.task-title',
-  //     on: 'bottom',
-  //   },
-  //   advanceOn: {
-  //     selector: '.task-title',
-  //     event: 'blur',
-  //   },
-  // },
-  // {
-  //   title: 'Task Side Panel',
-  //   text: 'There is more you you can do with task. Hover over the task you created with your mouse again.',
-  //   buttons: [],
-  //   attachTo: {
-  //     element: 'task',
-  //     on: 'bottom',
-  //   },
-  //   beforeShowPromise: () => promiseTimeout(500),
-  //   when: {
-  //     show: () => {
-  //       setTimeout(() => {
-  //         waitForEl('task .hover-controls', () => shepherdService.next());
-  //       }, 200);
-  //     },
-  //   },
-  // },
-  // {
-  //   title: 'Opening Task Side Panel',
-  //   attachTo: {
-  //     element: '.show-additional-info-btn',
-  //     on: 'bottom',
-  //   },
-  //   text: 'You can open a panel with additional controls by clicking on the button. Alternatively you can press the <kbd>➔</kbd> key when a task is focused.',
-  //   buttons: [],
-  //   ...waitForObs(
-  //     taskService.selectedTask$.pipe(filter((selectedTask) => !!selectedTask)),
-  //     () => shepherdService.next(),
-  //   ),
-  // },
-  // {
-  //   title: 'The Task Side Panel',
-  //   text: 'In the task side panel you can adjust estimates, schedule your task, add a description or attachments or configure your task to be repeated.',
-  //   buttons: [NEXT_BTN],
-  // },
-  // {
-  //   title: 'Closing the Task Side Panel',
-  //   text: 'You can close the panel by clicking the X or by pressing <kbd>←</kbd>. Do this now!',
-  //   attachTo: {
-  //     element: '.show-additional-info-btn',
-  //     on: 'bottom',
-  //   },
-  //   ...waitForObs(
-  //     taskService.selectedTask$.pipe(filter((selectedTask) => !selectedTask)),
-  //     () => shepherdService.next(),
-  //   ),
-  // },
   {
-    title: 'Deleting a Task',
-    text: 'To delete a task you need to open the task context menu.',
+    title: 'Welcome to Super Productivity!!',
+    text: 'Super Productivity is a ToDo app that helps you to improve your personal workflows.',
+    buttons: SHEPHERD_STANDARD_BTNS,
+  },
+  {
+    attachTo: {
+      element: '.action-nav button',
+      on: 'bottom',
+    },
     when: {
       show: () => {
-        waitForEl('mat-menu-panel', () => shepherdService.next());
+        waitForEl('app-root > add-task-bar input', () => shepherdService.next());
+      },
+    },
+    scrollTo: false,
+    title: "Let's add your first task!",
+    text: `Click on this button or press <kbd>${cfg.keyboard.addNewTask}</kbd>`,
+    advanceOn: {
+      selector: '.action-nav button',
+      event: 'click',
+    },
+  },
+
+  {
+    attachTo: {
+      element: 'add-task-bar',
+      on: 'bottom',
+    },
+    beforeShowPromise: () => promiseTimeout(1000),
+    when: {
+      show: () => {
+        waitForEl('task', () => {
+          layoutService.hideAddTaskBar();
+          shepherdService.next();
+        });
+      },
+    },
+    scrollTo: false,
+    title: 'Enter a title!',
+    text: 'Enter the title you want to give your task and hit the <kbd>Enter</kbd> key. After that you can press the <kbd>Escape</kbd> key or click anywhere on the grayed out backdrop to leave the add task bar.',
+  },
+  {
+    title: 'Congrats! This is your first task!',
+    text: 'Hover over it with your mouse',
+    attachTo: {
+      element: 'task',
+      on: 'bottom',
+    },
+    beforeShowPromise: () => promiseTimeout(1000),
+    when: {
+      show: () => {
+        setTimeout(() => {
+          waitForEl('task .hover-controls', () => shepherdService.next());
+        }, 1000);
+      },
+    },
+  },
+  {
+    title: 'Start Tracking',
+    attachTo: {
+      element: '.start-task-btn',
+      on: 'bottom',
+    },
+    text: 'Pressing the play button will start your first time tracking session. Time tracking is useful since it allows you to get a better idea on how you spend your time.',
+    //   attachTo: {
+    //     element: 'add-task-bar',
+    //     on: 'bottom',
+    //   },
+
+    ...waitForObs(taskService.currentTaskId$.pipe(filter((id) => !!id)), () =>
+      shepherdService.next(),
+    ),
+  },
+  {
+    title: 'Stop Tracking',
+    text: 'To stop tracking click on the pause button.',
+    attachTo: {
+      element: '.start-task-btn',
+      on: 'bottom',
+    },
+    ...waitForObs(taskService.currentTaskId$.pipe(filter((id) => !id)), () =>
+      shepherdService.next(),
+    ),
+  },
+  {
+    title: 'Edit Task Title',
+    text: 'You can edit the task title by clicking on it. Do this now and change the title to something else!',
+    attachTo: {
+      element: '.task-title',
+      on: 'bottom',
+    },
+    advanceOn: {
+      selector: '.task-title',
+      event: 'blur',
+    },
+  },
+  {
+    title: 'Task Side Panel',
+    text: 'There is more you you can do with task. Hover over the task you created with your mouse again.',
+    buttons: [],
+    attachTo: {
+      element: 'task',
+      on: 'bottom',
+    },
+    beforeShowPromise: () => promiseTimeout(500),
+    when: {
+      show: () => {
+        setTimeout(() => {
+          waitForEl('task .hover-controls', () => shepherdService.next());
+        }, 200);
+      },
+    },
+  },
+  {
+    title: 'Opening Task Side Panel',
+    attachTo: {
+      element: '.show-additional-info-btn',
+      on: 'bottom',
+    },
+    text: 'You can open a panel with additional controls by clicking on the button. Alternatively you can press the <kbd>➔</kbd> key when a task is focused.',
+    buttons: [],
+    ...waitForObs(
+      taskService.selectedTask$.pipe(filter((selectedTask) => !!selectedTask)),
+      () => shepherdService.next(),
+    ),
+  },
+  {
+    title: 'The Task Side Panel',
+    text: 'In the task side panel you can adjust estimates, schedule your task, add a description or attachments or configure your task to be repeated.',
+    buttons: [NEXT_BTN],
+  },
+  {
+    title: 'Closing the Task Side Panel',
+    text: 'You can close the panel by clicking the X or by pressing <kbd>←</kbd>. Do this now!',
+    attachTo: {
+      element: '.show-additional-info-btn',
+      on: 'bottom',
+    },
+    ...waitForObs(
+      taskService.selectedTask$.pipe(filter((selectedTask) => !selectedTask)),
+      () => shepherdService.next(),
+    ),
+  },
+  {
+    title: 'Deleting a Task',
+    // eslint-disable-next-line max-len
+    text: `To delete a task you need to open the task context menu. To do so right click (or long press on Mac and Mobile) and select "Delete Task". Alternatively you can focus the task by clicking on it and pressing the <kbd>${cfg.keyboard.taskDelete}</kbd> key`,
+    attachTo: {
+      element: 'task',
+      on: 'bottom',
+    },
+    when: {
+      show: () => {
+        waitForEl('.mat-menu-panel', () => {
+          shepherdService.hide();
+          actions$
+            .pipe(ofType(deleteTask), first())
+            .subscribe(() => shepherdService.next());
+        });
       },
     },
   },
--- src/app/features/shepherd/shepherd.component.ts
+++ src/app/features/shepherd/shepherd.component.ts
@@ -3,6 +3,9 @@ import { ShepherdService } from 'angular-shepherd';
 import { SHEPHERD_STANDARD_BTNS, SHEPHERD_STEPS } from './shepherd-steps.const';
 import { LayoutService } from '../../core-ui/layout/layout.service';
 import { TaskService } from '../tasks/task.service';
+import { Actions } from '@ngrx/effects';
+import { GlobalConfigService } from '../config/global-config.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'shepherd',
@@ -16,6 +19,8 @@ export class ShepherdComponent implements AfterViewInit {
     private shepherdService: ShepherdService,
     private layoutService: LayoutService,
     private taskService: TaskService,
+    private actions$: Actions,
+    private globalConfigService: GlobalConfigService,
   ) {}
 
   ngAfterViewInit(): void {
@@ -30,9 +35,18 @@ export class ShepherdComponent implements AfterViewInit {
     };
     // this.shepherdService.modal = true;
     // this.shepherdService.confirmCancel = false;
-    this.shepherdService.addSteps(
-      SHEPHERD_STEPS(this.shepherdService, this.layoutService, this.taskService) as any,
-    );
-    this.shepherdService.start();
+
+    this.globalConfigService.cfg$.pipe(first()).subscribe((cfg) => {
+      this.shepherdService.addSteps(
+        SHEPHERD_STEPS(
+          this.shepherdService,
+          cfg,
+          this.actions$,
+          this.layoutService,
+          this.taskService,
+        ) as any,
+      );
+      this.shepherdService.start();
+    });
   }
 }
