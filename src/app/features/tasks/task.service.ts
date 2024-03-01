b40397a7b5135e31c2fe65e5345e9d4852fdbdce
Johannes Millan <johannes.millan@gmail.com>
2024-03-01 13:30:07 +0100

    fix: issue when trying to remove last tag #3060

--- src/app/features/tasks/task.service.ts
+++ src/app/features/tasks/task.service.ts
@@ -101,6 +101,7 @@ import {
 } from '../project/store/project.actions';
 import { Update } from '@ngrx/entity';
 import { DateService } from 'src/app/core/date/date.service';
+import { T } from 'src/app/t.const';
 
 @Injectable({
   providedIn: 'root',
@@ -344,6 +345,14 @@ export class TaskService {
   }
 
   updateTags(task: Task, newTagIds: string[], oldTagIds: string[]): void {
+    if (!task.parentId && !task.projectId && newTagIds.length === 0) {
+      this._snackService.open({
+        type: 'ERROR',
+        msg: T.F.TASK.S.LAST_TAG_DELETION_WARNING,
+      });
+      return;
+    }
+
     this._store.dispatch(
       updateTaskTags({
         task,
