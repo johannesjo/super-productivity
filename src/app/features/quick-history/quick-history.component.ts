5d779a0345244d7fc62f63e0e1526cc0b6348476
Johannes Millan <johannes.millan@gmail.com>
2024-04-10 14:50:48 +0200

    refactor: cleanup

--- src/app/features/quick-history/quick-history.component.ts
+++ src/app/features/quick-history/quick-history.component.ts
@@ -1,17 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { expandAnimation, expandFadeAnimation } from '../../ui/animations/expand.ani';
 import { fadeAnimation } from '../../ui/animations/fade.ani';
 import { WorklogService } from '../worklog/worklog.service';
 import { SimpleCounterService } from '../simple-counter/simple-counter.service';
-import { MatDialog } from '@angular/material/dialog';
 import { TaskService } from '../tasks/task.service';
-import { getWeekNumber } from '../../util/get-week-number';
-import { getDateRangeForWeek } from '../../util/get-date-range-for-week';
-import { DialogWorklogExportComponent } from '../worklog/dialog-worklog-export/dialog-worklog-export.component';
 import { Task } from '../tasks/task.model';
 import { WorklogDataForDay, WorklogDay } from '../worklog/worklog.model';
 import { T } from 'src/app/t.const';
-import { DateAdapter } from '@angular/material/core';
 import { KeyValue } from '@angular/common';
 
 @Component({
@@ -29,36 +24,12 @@ export class QuickHistoryComponent {
   constructor(
     public readonly worklogService: WorklogService,
     public readonly simpleCounterService: SimpleCounterService,
-    private readonly _matDialog: MatDialog,
     private readonly _taskService: TaskService,
-    private _dateAdapter: DateAdapter<unknown>,
-  ) {
-    this.worklogService.quickHistoryWeeks$.subscribe((v) =>
-      console.log(`worklogService.quickHistoryWeeks$`, v),
-    );
-  }
+  ) {}
 
   sortDays(a: KeyValue<string, WorklogDay>, b: KeyValue<string, WorklogDay>): number {
     // avoid comparison by key (day) because a week may span across two months
     return a.value.dateStr.localeCompare(b.value.dateStr);
-  }
-
-  async exportData(): Promise<void> {
-    const now = new Date();
-    const year = now.getFullYear();
-    const weekNr = getWeekNumber(now, this._dateAdapter.getFirstDayOfWeek());
-
-    // get for whole week
-    const { rangeStart, rangeEnd } = getDateRangeForWeek(year, weekNr);
-
-    this._matDialog.open(DialogWorklogExportComponent, {
-      restoreFocus: true,
-      panelClass: 'big',
-      data: {
-        rangeStart,
-        rangeEnd,
-      },
-    });
   }
 
   async updateTimeSpentTodayForTask(
