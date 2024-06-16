0711a6018ad715fbecb2e423c4cf51afdc33dc5b
Johannes Millan <johannes.millan@gmail.com>
2024-06-16 12:07:42 +0200

    build: post 18 migrations

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -183,23 +183,24 @@ const OTHER_3RD_PARTY_MODS_WITHOUT_CFG = [
 const markedOptionsFactory = (): MarkedOptions => {
   const renderer = new MarkedRenderer();
 
-  renderer.checkbox = (isChecked: boolean) => {
-    return `<span class="checkbox material-icons">${
-      isChecked ? 'check_box ' : 'check_box_outline_blank '
-    }</span>`;
-  };
-  renderer.listitem = (text: string) => {
-    return text.includes('checkbox')
-      ? '<li class="checkbox-wrapper">' + text + '</li>'
-      : '<li>' + text + '</li>';
-  };
+  // TODO migrate properly
+  // renderer.checkbox = (isChecked: boolean) => {
+  //   return `<span class="checkbox material-icons">${
+  //     isChecked ? 'check_box ' : 'check_box_outline_blank '
+  //   }</span>`;
+  // };
+  // renderer.listitem = (text: string) => {
+  //   return text.includes('checkbox')
+  //     ? '<li class="checkbox-wrapper">' + text + '</li>'
+  //     : '<li>' + text + '</li>';
+  // };
   return {
     renderer: renderer,
     gfm: true,
     breaks: false,
     pedantic: false,
-    smartLists: true,
-    smartypants: false,
+    // smartLists: true,
+    // smartypants: false,
   };
 };
 
@@ -276,27 +277,27 @@ const markedOptionsFactory = (): MarkedOptions => {
 })
 export class UiModule {
   constructor(private _markdownService: MarkdownService) {
-    const linkRenderer = this._markdownService.renderer.link;
-    this._markdownService.renderer.link = (href, title, text) => {
-      const html = linkRenderer.call(this._markdownService.renderer, href, title, text);
-      return html.replace(/^<a /, '<a target="_blank" ');
-    };
-
-    this._markdownService.renderer.paragraph = (text) => {
-      const split = text.split('\n');
-      return split.reduce((acc, p, i) => {
-        const result = /h(\d)\./.exec(p);
-        if (result !== null) {
-          const h = `h${result[1]}`;
-          return acc + `<${h}>${p.replace(result[0], '')}</${h}>`;
-        }
-
-        if (split.length === 1) {
-          return `<p>` + p + `</p>`;
-        }
-
-        return acc ? (split.length - 1 === i ? acc + p + `</p>` : acc + p) : `<p>` + p;
-      }, '');
-    };
+    // TODO properly migrate
+    // const linkRenderer = this._markdownService.renderer.link;
+    // this._markdownService.renderer.link = (href, title, text) => {
+    //   const html = linkRenderer.call(this._markdownService.renderer, href, title, text);
+    //   return html.replace(/^<a /, '<a target="_blank" ');
+    // };
+    // this._markdownService.renderer.paragraph = (text) => {
+    //   const split = text.split('\n');
+    //   return split.reduce((acc, p, i) => {
+    //     const result = /h(\d)\./.exec(p);
+    //     if (result !== null) {
+    //       const h = `h${result[1]}`;
+    //       return acc + `<${h}>${p.replace(result[0], '')}</${h}>`;
+    //     }
+    //
+    //     if (split.length === 1) {
+    //       return `<p>` + p + `</p>`;
+    //     }
+    //
+    //     return acc ? (split.length - 1 === i ? acc + p + `</p>` : acc + p) : `<p>` + p;
+    //   }, '');
+    // };
   }
 }
