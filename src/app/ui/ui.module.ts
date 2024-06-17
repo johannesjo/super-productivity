ffc4a95785bfffaed1bde620b5108f16ad6099b3
Johannes Millan <johannes.millan@gmail.com>
2024-06-17 21:30:32 +0200

    migrate: mat hint

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -229,7 +229,7 @@ const OTHER_3RD_PARTY_MODS_WITHOUT_CFG = [
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { appearance: 'fill' },
+      useValue: { appearance: 'fill', subscriptSizing: 'dynamic' },
     },
     {
       provide: FORMLY_CONFIG,
