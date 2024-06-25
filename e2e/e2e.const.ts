fc49b9210b20a20dd9e62ff146205411bc60a137
Trang Le <trang.thule@zoho.com>
2024-06-25 23:13:00 +0700

    save common CSS selectors

--- e2e/e2e.const.ts
+++ e2e/e2e.const.ts
@@ -1,1 +1,17 @@
 export const BASE = 'http://localhost:4200';
+const SIDENAV = 'side-nav';
+const ADD_TASK_GLOBAL_SEL = 'add-task-bar.global input';
+const EXPAND_TAG_BTN = `${SIDENAV} .tags .expand-btn`;
+const ROUTER_WRAPPER = '.route-wrapper';
+const TAGS = `${SIDENAV} section.tags`;
+const READY_TO_WORK_BTN = '.ready-to-work-btn';
+
+export const WORK_VIEW_URL = `${BASE}/`;
+export const cssSelectors = {
+  READY_TO_WORK_BTN,
+  SIDENAV,
+  ADD_TASK_GLOBAL_SEL,
+  EXPAND_TAG_BTN,
+  ROUTER_WRAPPER,
+  TAGS,
+};
