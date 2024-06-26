352bd252dbb03a87d489cc3bb9b7989317b004de
Trang Le <trang.thule@zoho.com>
2024-06-26 21:02:55 +0700

    test: autocomplete dropdown for tag has at least one non-empty item

--- e2e/src/autocomplete-dropdown.e2e.ts
+++ e2e/src/autocomplete-dropdown.e2e.ts
@@ -2,7 +2,10 @@ import { NBrowser } from '../n-browser-interface';
 import { cssSelectors, WORK_VIEW_URL } from '../e2e.const';
 
 const AUTOCOMPLETE = 'mention-list';
-const { READY_TO_WORK_BTN } = cssSelectors;
+const AUTOCOMPLETE_ITEM = `${AUTOCOMPLETE} .mention-active`;
+const AUTOCOMPLETE_ITEM_TEXT = `${AUTOCOMPLETE_ITEM} .mention-item`;
+const { EXPAND_TAG_BTN, READY_TO_WORK_BTN, TAGS } = cssSelectors;
+const TAG = `${TAGS} div.tag`;
 
 module.exports = {
   '@tags': ['task', 'short-syntax', 'autocomplete'],
@@ -15,5 +18,35 @@ module.exports = {
       .waitForElementPresent(AUTOCOMPLETE)
       .assert.elementPresent(AUTOCOMPLETE)
       .end();
+  },
+  'should have at least one tag in the autocomplete dropdown': (browser: NBrowser) => {
+    const newTagTitle = 'angular';
+    browser
+      .loadAppAndClickAwayWelcomeDialog()
+      .waitForElementVisible(EXPAND_TAG_BTN)
+      .click(EXPAND_TAG_BTN)
+      .execute(
+        (tagSelector) => {
+          const tagElem = document.querySelector(tagSelector);
+          if (!tagElem) {
+            return false;
+          }
+          return true;
+        },
+        [TAG],
+        (result) => {
+          console.log('Has at least one tag', result.value);
+          if (!result.value) {
+            browser.addTaskWithNewTag(newTagTitle);
+          }
+        },
+      );
+    browser
+      .draftTask('Test the presence of tag in autcomplete #')
+      .waitForElementPresent(AUTOCOMPLETE)
+      .assert.visible(AUTOCOMPLETE_ITEM)
+      .expect.element(AUTOCOMPLETE_ITEM_TEXT)
+      .text.to.match(/.+/g);
+    browser.end();
   },
 };
