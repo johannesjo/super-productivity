29a71255fa701c424a1c7e829ccdf0563c1d530c
Trang Le <trang.thule@zoho.com>
2024-06-26 20:47:17 +0700

    test: display autocomplete when using short syntax for tags

--- e2e/e2e.const.ts
+++ e2e/e2e.const.ts
@@ -1,1 +1,18 @@
 export const BASE = 'http://localhost:4200';
+export const WORK_VIEW_URL = `${BASE}/`;
+
+const ADD_TASK_GLOBAL_SEL = 'add-task-bar.global input';
+const READY_TO_WORK_BTN = '.ready-to-work-btn';
+const ROUTER_WRAPPER = '.route-wrapper';
+const SIDENAV = 'side-nav';
+const EXPAND_TAG_BTN = `${SIDENAV} .tags .expand-btn`;
+const TAGS = `${SIDENAV} section.tags`;
+
+export const cssSelectors = {
+  ADD_TASK_GLOBAL_SEL,
+  EXPAND_TAG_BTN,
+  READY_TO_WORK_BTN,
+  ROUTER_WRAPPER,
+  SIDENAV,
+  TAGS,
+};
