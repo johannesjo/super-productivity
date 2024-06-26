0f2b5cf848d7400f92661162245f83e06cebc95f
Trang Le <trang.thule@zoho.com>
2024-06-26 16:16:29 +0700

    test: autocomplete has one item with non-empty string

--- e2e/src/autocomplete-dropdown.e2e.ts
+++ e2e/src/autocomplete-dropdown.e2e.ts
@@ -19,4 +19,34 @@ module.exports = {
       .assert.elementPresent(AUTOCOMPLETE)
       .end();
   },
+  'autocomplete dropdown should contain at least one tag': (browser: NBrowser) => {
+    let newTagTitle = 'angular';
+    browser
+      .loadAppAndClickAwayWelcomeDialog()
+      .waitForElementVisible(EXPAND_TAG_BTN)
+      .click(EXPAND_TAG_BTN)
+      .execute(
+        (tagSelector) => {
+          const tagElem = document.querySelector(tagSelector);
+          if (!tagElem) {
+            return false;
+          }
+          return true;
+        },
+        [TAG],
+        function (result) {
+          console.log('Has at least one tag', result.value);
+          if (!result.value) {
+            browser.addTaskWithNewTag(newTagTitle);
+          }
+        },
+      );
+    browser
+      .draftTask('Test the presence of tag in autcomplete #')
+      .waitForElementPresent(AUTOCOMPLETE)
+      .assert.visible(ACTIVE_AUTOCOMPLETE_ITEM)
+      .expect.element(ACTIVE_AUTOCOMPLETE_ITEM_TEXT)
+      .text.to.match(/.+/g);
+    browser.end();
+  },
 };
