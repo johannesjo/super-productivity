47d69930147f4d33555cec13e84aaeb57020672b
Johannes Millan <johannes.millan@gmail.com>
2024-07-08 19:56:04 +0200

    feat(weekPlanner): outline layout for add task

--- src/app/features/week-planner/week-planner.module.ts
+++ src/app/features/week-planner/week-planner.module.ts
@@ -4,9 +4,23 @@ import { WeekPlannerComponent } from './week-planner.component';
 import { WeekPlannerPlanViewComponent } from './week-planner-plan-view/week-planner-plan-view.component';
 import { UiModule } from '../../ui/ui.module';
 import { CdkDrag, CdkDropList, CdkDropListGroup } from '@angular/cdk/drag-drop';
+import { BetterDrawerModule } from '../../ui/better-drawer/better-drawer.module';
+import { NoteModule } from '../note/note.module';
+import { TasksModule } from '../tasks/tasks.module';
+import { WorkViewModule } from '../work-view/work-view.module';
 
 @NgModule({
   declarations: [WeekPlannerComponent, WeekPlannerPlanViewComponent],
-  imports: [CommonModule, UiModule, CdkDropList, CdkDropListGroup, CdkDrag],
+  imports: [
+    CommonModule,
+    UiModule,
+    CdkDropList,
+    CdkDropListGroup,
+    CdkDrag,
+    BetterDrawerModule,
+    NoteModule,
+    TasksModule,
+    WorkViewModule,
+  ],
 })
 export class WeekPlannerModule {}
