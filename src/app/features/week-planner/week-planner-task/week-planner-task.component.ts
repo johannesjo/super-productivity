7186afb5e7bd572381be716a979c05dd018c97e0
Johannes Millan <johannes.millan@gmail.com>
2024-07-10 14:46:39 +0200

    feat(weekPlanner): various next steps

--- src/app/features/week-planner/week-planner-task/week-planner-task.component.ts
+++ src/app/features/week-planner/week-planner-task/week-planner-task.component.ts
@@ -1,5 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { TaskCopy } from '../../tasks/task.model';
+import { Subscription } from 'rxjs';
+import { TaskService } from '../../tasks/task.service';
 
 @Component({
   selector: 'week-planner-task',
@@ -7,6 +16,31 @@ import { TaskCopy } from '../../tasks/task.model';
   styleUrl: './week-planner-task.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WeekPlannerTaskComponent {
+export class WeekPlannerTaskComponent implements OnInit, OnDestroy {
   @Input({ required: true }) task!: TaskCopy;
+
+  parentTitle: string | null = null;
+  private _subs = new Subscription();
+
+  constructor(
+    private _taskService: TaskService,
+    private _cd: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    console.log(this.task);
+    if (this.task.parentId) {
+      this._subs.add(
+        this._taskService.getByIdLive$(this.task.parentId).subscribe((parentTask) => {
+          this.parentTitle = parentTask && parentTask.title;
+          this._cd.markForCheck();
+          this._cd.detectChanges();
+        }),
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._subs.unsubscribe();
+  }
 }
