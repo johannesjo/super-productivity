7b8f1d212dce32704a5bd6772e4eb8fc83871005
Johannes Millan <johannes.millan@gmail.com>
2024-07-11 18:17:05 +0200

    feat(weekPlanner): show cal event projections

--- src/app/features/week-planner/store/week-planner.selectors.ts
+++ src/app/features/week-planner/store/week-planner.selectors.ts
@@ -3,6 +3,7 @@ import * as fromWeekPlanner from './week-planner.reducer';
 import { selectTaskFeatureState } from '../../tasks/store/task.selectors';
 import {
   NoStartTimeRepeatProjection,
+  ScheduleItemEvent,
   ScheduleItemRepeatProjection,
   ScheduleItemType,
   WeekPlannerDay,
@@ -12,6 +13,7 @@ import { TaskRepeatCfg } from '../../task-repeat-cfg/task-repeat-cfg.model';
 import { TimelineCalendarMapEntry } from '../../timeline/timeline.model';
 import { selectTaskRepeatCfgsDueOnDayOnly } from '../../task-repeat-cfg/store/task-repeat-cfg.reducer';
 import { getDateTimeFromClockString } from '../../../util/get-date-time-from-clock-string';
+import { isSameDay } from '../../../util/is-same-day';
 
 export const selectWeekPlannerState =
   createFeatureSelector<fromWeekPlanner.WeekPlannerState>(
@@ -30,11 +32,13 @@ export const selectWeekPlannerDays = (
     selectWeekPlannerState,
     (taskState, weekPlannerState): WeekPlannerDay[] => {
       return dayDates.map((dayDate, dayIndex) => {
+        const currentDayDate = new Date(dayDate);
+        const currentDayTimestamp = currentDayDate.getTime();
         const tIds = weekPlannerState.days[dayDate] || [];
         const normalTasks = tIds.map((id) => taskState.entities[id] as TaskCopy);
+        const icalEventsForDay: ScheduleItemEvent[] = [];
         const repeatProjectionsForDay: ScheduleItemRepeatProjection[] = [];
         const noStartTimeRepeatProjections: NoStartTimeRepeatProjection[] = [];
-        const currentDayTimestamp = new Date(dayDate).getTime();
 
         const allRepeatableTasksForDay = selectTaskRepeatCfgsDueOnDayOnly.projector(
           taskRepeatCfgs,
@@ -64,11 +68,34 @@ export const selectWeekPlannerDays = (
           }
         });
 
+        icalEvents.forEach((icalMapEntry) => {
+          icalMapEntry.items.forEach((calEv) => {
+            const start = calEv.start;
+            if (isSameDay(start, currentDayDate)) {
+              const end = calEv.start + calEv.duration;
+              icalEventsForDay.push({
+                id: calEv.id,
+                type: ScheduleItemType.CalEvent,
+                start,
+                end,
+                calendarEvent: {
+                  ico: icalMapEntry.icon,
+                  ...calEv,
+                },
+              });
+            }
+          });
+        });
+
+        // TODO get all scheduled tasks the days in question
+
         const day: WeekPlannerDay = {
           isToday: dayIndex === 0,
           dayDate,
           timeLimit: 0,
-          scheduledIItems: [...repeatProjectionsForDay],
+          scheduledIItems: [...repeatProjectionsForDay, ...icalEventsForDay].sort(
+            (a, b) => a.start - b.start,
+          ),
           tasks: normalTasks,
           noStartTimeRepeatProjections,
           // TODO calc total time from different function
