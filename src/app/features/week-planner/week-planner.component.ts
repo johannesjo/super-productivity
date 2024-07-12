40c0244748260b1d50f37145c5f834cc4c47f912
Johannes Millan <johannes.millan@gmail.com>
2024-07-12 12:05:29 +0200

    feat(weekPlanner): add new today tasks right away

--- src/app/features/week-planner/week-planner.component.ts
+++ src/app/features/week-planner/week-planner.component.ts
@@ -1,11 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BaseComponent } from '../../core/base-component/base.component';
-import { first, takeUntil, withLatestFrom } from 'rxjs/operators';
+import {
+  filter,
+  first,
+  startWith,
+  switchMap,
+  takeUntil,
+  withLatestFrom,
+} from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { selectTodayTasksWithPlannedAndDoneSeperated } from '../work-context/store/work-context.selectors';
 import { WeekPlannerActions } from './store/week-planner.actions';
 import { getWorklogStr } from '../../util/get-work-log-str';
 import { selectTaskFeatureState } from '../tasks/store/task.selectors';
+import { Actions, ofType } from '@ngrx/effects';
+import { addTask } from '../tasks/store/task.actions';
+import { TODAY_TAG } from '../tag/tag.const';
 
 @Component({
   selector: 'week-planner',
@@ -16,15 +26,28 @@ import { selectTaskFeatureState } from '../tasks/store/task.selectors';
 export class WeekPlannerComponent extends BaseComponent {
   isPanelOpen = true;
 
-  constructor(private _store: Store) {
+  constructor(
+    private _store: Store,
+    private _actions$: Actions,
+  ) {
     super();
-    // TODO optimize later
-    this._store
-      .select(selectTodayTasksWithPlannedAndDoneSeperated)
+
+    this._actions$
       .pipe(
-        takeUntil(this.onDestroy$),
-        withLatestFrom(this._store.select(selectTaskFeatureState)),
-        first(),
+        ofType(addTask),
+        filter((a) => a.task.tagIds.includes(TODAY_TAG.id)),
+        startWith(undefined),
+      )
+      .pipe(
+        switchMap(() =>
+          this._store
+            .select(selectTodayTasksWithPlannedAndDoneSeperated)
+            .pipe(
+              takeUntil(this.onDestroy$),
+              withLatestFrom(this._store.select(selectTaskFeatureState)),
+              first(),
+            ),
+        ),
       )
       .subscribe(([{ planned, done, normal }, taskState]) => {
         this._store.dispatch(
