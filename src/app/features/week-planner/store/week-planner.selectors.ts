a882d5b32c8d26958ce88823dbf75008154cec5f
Johannes Millan <johannes.millan@gmail.com>
2024-07-12 09:30:54 +0200

    refactor(weekPlanner): split selector functions

--- src/app/features/week-planner/store/week-planner.selectors.ts
+++ src/app/features/week-planner/store/week-planner.selectors.ts
@@ -38,71 +38,13 @@ export const selectWeekPlannerDays = (
         const currentDayTimestamp = currentDayDate.getTime();
         const tIds = weekPlannerState.days[dayDate] || [];
         const normalTasks = tIds.map((id) => taskState.entities[id] as TaskCopy);
-        const icalEventsForDay: ScheduleItemEvent[] = [];
-        const repeatProjectionsForDay: ScheduleItemRepeatProjection[] = [];
-        const scheduledTaskItems: ScheduleItemTask[] = [];
-        const noStartTimeRepeatProjections: NoStartTimeRepeatProjection[] = [];
 
-        const allRepeatableTasksForDay = selectTaskRepeatCfgsDueOnDayOnly.projector(
-          taskRepeatCfgs,
-          {
-            dayDate: currentDayTimestamp,
-          },
-        );
-        allRepeatableTasksForDay.forEach((repeatCfg) => {
-          if (repeatCfg.startTime) {
-            const start = getDateTimeFromClockString(
-              repeatCfg.startTime,
-              currentDayTimestamp,
-            );
-            const end = start + (repeatCfg.defaultEstimate || 0);
-            repeatProjectionsForDay.push({
-              id: repeatCfg.id,
-              type: ScheduleItemType.RepeatProjection,
-              start,
-              end,
-              repeatCfg,
-            });
-          } else {
-            noStartTimeRepeatProjections.push({
-              id: repeatCfg.id + dayIndex,
-              repeatCfg,
-            });
-          }
-        });
+        const { repeatProjectionsForDay, noStartTimeRepeatProjections } =
+          getAllRepeatableTasksForDay(taskRepeatCfgs, currentDayTimestamp);
 
-        icalEvents.forEach((icalMapEntry) => {
-          icalMapEntry.items.forEach((calEv) => {
-            const start = calEv.start;
-            if (isSameDay(start, currentDayDate)) {
-              const end = calEv.start + calEv.duration;
-              icalEventsForDay.push({
-                id: calEv.id,
-                type: ScheduleItemType.CalEvent,
-                start,
-                end,
-                calendarEvent: {
-                  ico: icalMapEntry.icon,
-                  ...calEv,
-                },
-              });
-            }
-          });
-        });
-
-        allPlannedTasks.forEach((task) => {
-          const start = task.plannedAt;
-          if (isSameDay(start, currentDayDate)) {
-            const end = start + Math.max(task.timeEstimate - task.timeSpent, 0);
-            scheduledTaskItems.push({
-              id: task.id,
-              type: ScheduleItemType.Task,
-              start,
-              end,
-              task,
-            });
-          }
-        });
+        // Inside selectWeekPlannerDays, replace the relevant sections with:
+        const scheduledTaskItems = getScheduledTaskItems(allPlannedTasks, currentDayDate);
+        const icalEventsForDay = getIcalEventsForDay(icalEvents, currentDayDate);
 
         const day: WeekPlannerDay = {
           isToday: dayIndex === 0,
@@ -125,4 +67,88 @@ export const selectWeekPlannerDays = (
       });
     },
   );
+};
+
+const getAllRepeatableTasksForDay = (
+  taskRepeatCfgs: TaskRepeatCfg[],
+  currentDayTimestamp: number,
+): {
+  repeatProjectionsForDay: ScheduleItemRepeatProjection[];
+  noStartTimeRepeatProjections: NoStartTimeRepeatProjection[];
+} => {
+  const repeatProjectionsForDay: ScheduleItemRepeatProjection[] = [];
+  const noStartTimeRepeatProjections: NoStartTimeRepeatProjection[] = [];
+  const allRepeatableTasksForDay = selectTaskRepeatCfgsDueOnDayOnly.projector(
+    taskRepeatCfgs,
+    {
+      dayDate: currentDayTimestamp,
+    },
+  );
+  allRepeatableTasksForDay.forEach((repeatCfg) => {
+    if (repeatCfg.startTime) {
+      const start = getDateTimeFromClockString(repeatCfg.startTime, currentDayTimestamp);
+      const end = start + (repeatCfg.defaultEstimate || 0);
+      repeatProjectionsForDay.push({
+        id: repeatCfg.id,
+        type: ScheduleItemType.RepeatProjection,
+        start,
+        end,
+        repeatCfg,
+      });
+    } else {
+      noStartTimeRepeatProjections.push({
+        id: repeatCfg.id,
+        repeatCfg,
+      });
+    }
+  });
+
+  return {
+    repeatProjectionsForDay,
+    noStartTimeRepeatProjections,
+  };
+};
+
+const getScheduledTaskItems = (
+  allPlannedTasks: TaskPlanned[],
+  currentDayDate: Date,
+): ScheduleItemTask[] =>
+  allPlannedTasks
+    .filter((task) => isSameDay(task.plannedAt, currentDayDate))
+    .map((task) => {
+      const start = task.plannedAt;
+      const end = start + Math.max(task.timeEstimate - task.timeSpent, 0);
+      return {
+        id: task.id,
+        type: ScheduleItemType.Task,
+        start,
+        end,
+        task,
+      };
+    });
+
+const getIcalEventsForDay = (
+  icalEvents: TimelineCalendarMapEntry[],
+  currentDayDate: Date,
+): ScheduleItemEvent[] => {
+  const icalEventsForDay: ScheduleItemEvent[] = [];
+  icalEvents.forEach((icalMapEntry) => {
+    icalMapEntry.items.forEach((calEv) => {
+      const start = calEv.start;
+      if (isSameDay(start, currentDayDate)) {
+        const end = calEv.start + calEv.duration;
+        icalEventsForDay.push({
+          id: calEv.id,
+          type: ScheduleItemType.CalEvent,
+          start,
+          end,
+          calendarEvent: {
+            ico: icalMapEntry.icon,
+            ...calEv,
+          },
+        });
+      }
+    });
+  });
+  return icalEventsForDay;
 };
