4ef8aaa38619824f5de4d5ae4ba0e0c6a4a4b3cb
Johannes Millan <johannes.millan@gmail.com>
2024-07-12 16:32:44 +0200

    feat(weekPlanner): reschedule or schedule tasks on drop 2

--- src/app/features/planner/planner-plan-view/planner-plan-view.component.ts
+++ src/app/features/planner/planner-plan-view/planner-plan-view.component.ts
@@ -16,6 +16,7 @@ import { TaskService } from '../../tasks/task.service';
 import { ReminderCopy } from '../../reminder/reminder.model';
 import { ReminderService } from '../../reminder/reminder.service';
 import { millisecondsDiffToRemindOption } from '../../tasks/util/remind-option-to-milliseconds';
+import { TODAY_TAG } from '../../tag/tag.const';
 
 @Component({
   selector: 'planner-plan-view',
@@ -46,46 +47,36 @@ export class PlannerPlanViewComponent {
     ev: CdkDragDrop<string, string, TaskCopy>,
   ): void {
     const newDay = ev.container.data;
+    const task = ev.item.data;
 
     if (targetList === 'SCHEDULED') {
       // TODO show schedule dialog
       if (ev.previousContainer !== ev.container) {
-        this.editTaskReminderOrReScheduleIfPossible(ev.item.data, ev.container.data);
+        this.editTaskReminderOrReScheduleIfPossible(task, ev.container.data);
       }
-
       return;
-    }
-
-    if (ev.previousContainer === ev.container) {
-      this._store.dispatch(
-        PlannerActions.moveInList({
-          targetDay: ev.container.data,
-          fromIndex: ev.previousIndex,
-          toIndex: ev.currentIndex,
-        }),
-      );
-    } else {
-      this._store.dispatch(
-        PlannerActions.transferTask({
-          task: ev.item.data,
-          prevDay: ev.previousContainer.data,
-          newDay: newDay,
-          targetIndex: ev.currentIndex,
-        }),
-      );
-      // transferArrayItem(
-      //   ev.previousContainer.data,
-      //   newDay,
-      //   ev.previousIndex,
-      //   ev.currentIndex,
-      // );
-      // if (targetList === 'TODO') {
-      //   const item = newDay[ev.currentIndex];
-      //   if (item.type) {
-      //     // TODO remove reminder
-      //     newDay[ev.currentIndex] = item.task;
-      //   }
-      // }
+    } else if (targetList === 'TODO') {
+      if (ev.previousContainer === ev.container) {
+        this._store.dispatch(
+          PlannerActions.moveInList({
+            targetDay: ev.container.data,
+            fromIndex: ev.previousIndex,
+            toIndex: ev.currentIndex,
+          }),
+        );
+      } else {
+        this._store.dispatch(
+          PlannerActions.transferTask({
+            task: task,
+            prevDay: ev.previousContainer.data,
+            newDay: newDay,
+            targetIndex: ev.currentIndex,
+          }),
+        );
+        if (task.reminderId) {
+          this._taskService.unScheduleTask(task.id, task.reminderId);
+        }
+      }
     }
   }
 
@@ -94,21 +85,7 @@ export class PlannerPlanViewComponent {
     if (newDay) {
       const newDate = new Date(newDay);
       if (task.plannedAt) {
-        const taskPlannedAtDate = new Date(task.plannedAt);
-        newDate.setHours(
-          taskPlannedAtDate.getHours(),
-          taskPlannedAtDate.getMinutes(),
-          0,
-          0,
-        );
-        const reminder: ReminderCopy | undefined = task.reminderId
-          ? this._reminderService.getById(task.reminderId) || undefined
-          : undefined;
-        const selectedReminderCfgId = millisecondsDiffToRemindOption(
-          task.plannedAt as number,
-          reminder?.remindAt,
-        );
-        this._taskService.scheduleTask(task, newDate.getTime(), selectedReminderCfgId);
+        this._rescheduleTask(task, newDate);
         return;
       } else {
         newDate.setHours(DAY_STARTS_AT_DEFAULT_H, 0, 0, 0);
@@ -123,5 +100,28 @@ export class PlannerPlanViewComponent {
         initialDateTime: initialDateTime,
       } as AddTaskReminderInterface,
     });
+  }
+
+  private _rescheduleTask(task: TaskCopy, newDate: Date): void {
+    const taskPlannedAtDate = new Date(task.plannedAt as number);
+    newDate.setHours(taskPlannedAtDate.getHours(), taskPlannedAtDate.getMinutes(), 0, 0);
+    const reminder: ReminderCopy | undefined = task.reminderId
+      ? this._reminderService.getById(task.reminderId) || undefined
+      : undefined;
+    const selectedReminderCfgId = millisecondsDiffToRemindOption(
+      task.plannedAt as number,
+      reminder?.remindAt,
+    );
+    const isToday = new Date().toDateString() === newDate.toDateString();
+    this._taskService.scheduleTask(task, newDate.getTime(), selectedReminderCfgId, false);
+    if (isToday) {
+      this._taskService.updateTags(task, [TODAY_TAG.id, ...task.tagIds], task.tagIds);
+    } else {
+      this._taskService.updateTags(
+        task,
+        task.tagIds.filter((tid) => tid !== TODAY_TAG.id),
+        task.tagIds,
+      );
+    }
   }
 }
