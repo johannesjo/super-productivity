272025bac21f062d68ec4fbe7890628c2d3786c4
Johannes Millan <johannes.millan@gmail.com>
2024-07-16 09:21:31 +0200

    fix(weekPlanner): order when dropping task on today

--- src/app/features/planner/store/planner.effects.ts
+++ src/app/features/planner/store/planner.effects.ts
@@ -7,6 +7,7 @@ import {
   filter,
   first,
   map,
+  mergeMap,
   switchMap,
   tap,
   withLatestFrom,
@@ -20,7 +21,7 @@ import {
   unScheduleTask,
   updateTaskTags,
 } from '../../tasks/store/task.actions';
-import { EMPTY, merge, of } from 'rxjs';
+import { EMPTY, merge } from 'rxjs';
 import { TODAY_TAG } from '../../tag/tag.const';
 import { unique } from '../../../util/unique';
 import { selectTodayTaskIds } from '../../work-context/store/work-context.selectors';
@@ -81,29 +82,46 @@ export class PlannerEffects {
   addOrRemoveTodayTag$ = createEffect(() => {
     return this._actions$.pipe(
       ofType(PlannerActions.transferTask),
-      switchMap(({ prevDay, newDay, task }) => {
+      filter(({ prevDay, newDay }) => {
+        const wls = this._dateService.todayStr();
+        return prevDay === wls || newDay === wls;
+      }),
+      withLatestFrom(
+        this._store.pipe(select(selectTagById, { id: TODAY_TAG.id })),
+        this._store.pipe(select(selectPlannerState)),
+      ),
+      mergeMap(([{ prevDay, newDay, task }, todayTag, plannerState]) => {
         const todayDayStr = this._dateService.todayStr();
         if (prevDay === todayDayStr && newDay !== todayDayStr) {
           const newTagIds = task.tagIds.filter((tagId) => tagId !== TODAY_TAG.id);
-          // NOTE: we need to prevent the NO tag NO project case
-          if (newTagIds.length > 0 || task.projectId) {
-            return of(
-              updateTaskTags({
-                task,
-                oldTagIds: task.tagIds,
-                newTagIds,
-              }),
-            );
-          }
+          return [
+            updateTaskTags({
+              task,
+              oldTagIds: task.tagIds,
+              newTagIds,
+            }),
+          ];
         }
         if (prevDay !== todayDayStr && newDay === todayDayStr) {
-          return of(
+          const plannedIds = plannerState.days[this._dateService.todayStr()] || [];
+          return [
             updateTaskTags({
               task,
               oldTagIds: task.tagIds,
               newTagIds: unique([TODAY_TAG.id, ...task.tagIds]),
             }),
-          );
+            updateTag({
+              tag: {
+                id: TODAY_TAG.id,
+                changes: {
+                  taskIds: [
+                    ...plannedIds,
+                    ...todayTag.taskIds.filter((id) => !plannedIds.includes(id)),
+                  ],
+                },
+              },
+            }),
+          ];
         }
         return EMPTY;
       }),
