1351d97aca709fbf120f3d9d319644dcd0ad8cf6
Johannes Millan <johannes.millan@gmail.com>
2024-07-23 15:56:53 +0200

    refactor(planner): cleanup

--- src/app/features/planner/planner-plan-view/planner-plan-view.component.ts
+++ src/app/features/planner/planner-plan-view/planner-plan-view.component.ts
@@ -1,13 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TASK_REMINDER_OPTIONS } from '../../tasks/dialog-add-task-reminder/task-reminder-options.const';
 import { T } from '../../../t.const';
-import { PlannerDay, ScheduleItemType } from '../planner.model';
-import { Store } from '@ngrx/store';
+import { PlannerDay } from '../planner.model';
 import { PlannerService } from '../planner.service';
-import { MatDialog } from '@angular/material/dialog';
-import { TaskService } from '../../tasks/task.service';
-import { ReminderService } from '../../reminder/reminder.service';
 
 @Component({
   selector: 'planner-plan-view',
@@ -16,19 +11,9 @@ import { ReminderService } from '../../reminder/reminder.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlannerPlanViewComponent {
-  SCHEDULE_ITEM_TYPE = ScheduleItemType;
-  // days$: Observable<PlannerDay[]> = of(PLANNER_DUMMY_DATA);
-
   days$: Observable<PlannerDay[]> = this._plannerService.days$;
 
-  protected readonly remindAvailableOptions = TASK_REMINDER_OPTIONS;
   protected readonly T = T;
 
-  constructor(
-    private _store: Store,
-    private _plannerService: PlannerService,
-    private _matDialog: MatDialog,
-    private _taskService: TaskService,
-    private _reminderService: ReminderService,
-  ) {}
+  constructor(private _plannerService: PlannerService) {}
 }
