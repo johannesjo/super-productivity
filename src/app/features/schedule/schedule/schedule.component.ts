aa6b88edfc4737f61d02a0f3aa2e000332225f76
Johannes Millan <johannes.millan@gmail.com>
2024-07-23 12:18:10 +0200

    feat(weekSchedule): make it more dynamic

--- src/app/features/schedule/schedule/schedule.component.ts
+++ src/app/features/schedule/schedule/schedule.component.ts
@@ -9,4 +9,25 @@ import { UiModule } from '../../../ui/ui.module';
   styleUrl: './schedule.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScheduleComponent {}
+export class ScheduleComponent {
+  hourFraction = 1;
+  numberOfRows = 24;
+  daysToShow = 7;
+  colByNr = Array.from({ length: this.daysToShow }, (_, index) => index);
+  rowsByNr = Array.from({ length: this.numberOfRows }, (_, index) => index);
+  times: string[] = this.rowsByNr.map((_, index) => {
+    // const hours = Math.floor(index / 2);
+    // const minutes = index % 2 === 0 ? '00' : '30';
+    // return `${hours}:${minutes}`;
+    return index.toString() + ':00';
+  });
+
+  events = [
+    {
+      title: 'Something',
+      col: 2,
+      row: 4,
+      span: 2,
+    },
+  ];
+}
