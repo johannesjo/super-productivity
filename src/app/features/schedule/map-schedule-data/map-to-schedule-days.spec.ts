87e5cf537e7458befa108aa05f80a495d2731a4b
Johannes Millan <johannes.millan@gmail.com>
2024-07-31 17:15:12 +0200

    test(schedule): outline next

--- src/app/features/schedule/map-schedule-data/map-to-schedule-days.spec.ts
+++ src/app/features/schedule/map-schedule-data/map-to-schedule-days.spec.ts
@@ -1,7 +1,7 @@
 import { mapToScheduleDays } from './map-to-schedule-days';
-import { TaskCopy, TaskPlanned } from '../../tasks/task.model';
+import { TaskCopy } from '../../tasks/task.model';
 
-const NDS = '1970-1-1';
+const NDS = '1970-01-01';
 // const N = new Date(`${NDS} 01:00`).getTime();
 const N = Date.UTC(1970, 0, 1, 0, 0, 0, 0);
 // 86400000 ==> 24h
@@ -15,7 +15,7 @@ const FAKE_TASK: Partial<TaskCopy> = {
 };
 
 // eslint-disable-next-line no-mixed-operators
-const minAfterNow = (min: number): Date => new Date(N + min * 60000);
+const minAfterNow = (min: number): Date => new Date(Date.UTC(1970, 0, 1, 0, min, 0, 0));
 
 console.log(N);
 console.log(minAfterNow(30));
@@ -28,17 +28,17 @@ const fakeTaskEntry = (id = 'XXX', add?: Partial<TaskCopy>): TaskCopy => {
   } as TaskCopy;
 };
 
-const fakePlannedTaskEntry = (
-  id = 'XXX',
-  planedAt: Date,
-  add?: Partial<TaskPlanned>,
-): TaskPlanned => {
-  return {
-    ...fakeTaskEntry(id, add),
-    plannedAt: planedAt.getUTCMilliseconds(),
-    reminderId: 'R_ID',
-  } as TaskPlanned;
-};
+// const fakePlannedTaskEntry = (
+//   id = 'XXX',
+//   planedAt: Date,
+//   add?: Partial<TaskPlanned>,
+// ): TaskPlanned => {
+//   return {
+//     ...fakeTaskEntry(id, add),
+//     plannedAt: planedAt.getUTCMilliseconds(),
+//     reminderId: 'R_ID',
+//   } as TaskPlanned;
+// };
 
 describe('mapToScheduleDays()', () => {
   it('should work for empty case', () => {
@@ -51,7 +51,7 @@ describe('mapToScheduleDays()', () => {
     expect(
       mapToScheduleDays(
         N,
-        ['1970-1-1', '2020-1-2'],
+        [NDS, '2020-1-2'],
         [
           fakeTaskEntry('1', { timeEstimate: H }),
           fakeTaskEntry('2', { timeEstimate: 2 * H }),
@@ -68,7 +68,7 @@ describe('mapToScheduleDays()', () => {
     ).toEqual([
       {
         beyondBudgetTasks: [],
-        dayDate: '1970-1-1',
+        dayDate: NDS,
         entries: [
           {
             data: {
@@ -101,24 +101,64 @@ describe('mapToScheduleDays()', () => {
     ] as any);
   });
 
-  xit('should work for case with one of each', () => {
-    expect(
-      mapToScheduleDays(
-        N,
-        [NDS, '2020-1-2'],
-        [
-          fakeTaskEntry('1', { timeEstimate: H }),
-          // fakeTaskEntry('2', { timeEstimate: 2 * H }),
-        ],
-        [fakePlannedTaskEntry('3', minAfterNow(30), { timeEstimate: H })],
-        [],
-        [],
-        [],
-        null,
-        {},
-        undefined,
-        undefined,
-      ),
-    ).toEqual([] as any);
-  });
+  // it('should work for simple scheduled case', () => {
+  //   // TODO fix this should result in a split task
+  //   expect(
+  //     mapToScheduleDays(
+  //       N,
+  //       [NDS, '1970-01-02'],
+  //       [
+  //         fakeTaskEntry('1', { timeEstimate: 2 * H }),
+  //         // fakeTaskEntry('2', { timeEstimate: 2 * H }),
+  //       ],
+  //       [fakePlannedTaskEntry('3', minAfterNow(15), { timeEstimate: H })],
+  //       [],
+  //       [],
+  //       [],
+  //       null,
+  //       {},
+  //       undefined,
+  //       undefined,
+  //     ),
+  //   ).toEqual([
+  //     {
+  //       beyondBudgetTasks: [],
+  //       dayDate: '1970-01-01',
+  //       entries: [
+  //         {
+  //           data: {
+  //             id: '3',
+  //             plannedAt: 0,
+  //             reminderId: 'R_ID',
+  //             subTaskIds: [],
+  //             tagIds: [],
+  //             timeEstimate: 3600000,
+  //             timeSpent: 0,
+  //           },
+  //           id: '3',
+  //           start: 0,
+  //           timeToGo: 3600000,
+  //           type: 'ScheduledTask',
+  //         },
+  //         {
+  //           data: {
+  //             id: '1',
+  //             subTaskIds: [],
+  //             tagIds: [],
+  //             timeEstimate: 3600000,
+  //             timeSpent: 0,
+  //           },
+  //           id: '1',
+  //           start: 3600000,
+  //           timeToGo: 3600000,
+  //           type: 'Task',
+  //         },
+  //       ],
+  //       isToday: true,
+  //     },
+  //     { beyondBudgetTasks: [], dayDate: '1970-01-02', entries: [], isToday: false },
+  //   ] as any);
+  // });
+
+  // fit('should work for case with one of each', () => {});
 });
