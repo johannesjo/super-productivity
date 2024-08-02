b33c201bb376e296ffe8f6b5ebf5de9a820caa36
Hendrik <badbossy@hotmail.de>
2024-08-02 13:47:26 +0200

    fix(config): removed unused imports

--- src/app/pages/config-page/config-page.module.ts
+++ src/app/pages/config-page/config-page.module.ts
@@ -4,26 +4,9 @@ import { ConfigPageComponent } from './config-page.component';
 import { ConfigModule } from '../../features/config/config.module';
 import { UiModule } from '../../ui/ui.module';
 import { JiraViewComponentsModule } from '../../features/issue/providers/jira/jira-view-components/jira-view-components.module';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ConfigModule,
-    UiModule,
-    JiraViewComponentsModule,
-    MatExpansionModule,
-    MatSliderModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FormsModule,
-    MatSlideToggleModule,
-  ],
+  imports: [CommonModule, ConfigModule, UiModule, JiraViewComponentsModule],
   declarations: [ConfigPageComponent],
 })
 export class ConfigPageModule {}
