f22fd43b9df9f7cc8314480e43785bd7e327ce4e
TheBadBossy <badbossy@hotmail.de>
2024-08-02 08:42:37 +0200

    feat(config): add new sound section into own component

--- src/app/pages/config-page/config-page.module.ts
+++ src/app/pages/config-page/config-page.module.ts
@@ -4,9 +4,24 @@ import { ConfigPageComponent } from './config-page.component';
 import { ConfigModule } from '../../features/config/config.module';
 import { UiModule } from '../../ui/ui.module';
 import { JiraViewComponentsModule } from '../../features/issue/providers/jira/jira-view-components/jira-view-components.module';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [CommonModule, ConfigModule, UiModule, JiraViewComponentsModule],
+  imports: [
+    CommonModule,
+    ConfigModule,
+    UiModule,
+    JiraViewComponentsModule,
+    MatExpansionModule,
+    MatSliderModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   declarations: [ConfigPageComponent],
 })
 export class ConfigPageModule {}
