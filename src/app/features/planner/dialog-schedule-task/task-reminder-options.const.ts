d97ae0cd84be5d81a1c691757ad288e7aa3a70ed
Johannes Millan <johannes.millan@gmail.com>
2024-08-18 14:22:53 +0200

    feat: add translations for new schedule dialog

--- src/app/features/planner/dialog-schedule-task/task-reminder-options.const.ts
+++ src/app/features/planner/dialog-schedule-task/task-reminder-options.const.ts
@@ -4,30 +4,30 @@ import { T } from '../../../t.const';
 export const TASK_REMINDER_OPTIONS: TaskReminderOption[] = [
   {
     value: TaskReminderOptionId.DoNotRemind,
-    label: T.F.TASK.D_REMINDER_ADD.RO_NEVER,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_NEVER,
   },
   {
     value: TaskReminderOptionId.AtStart,
-    label: T.F.TASK.D_REMINDER_ADD.RO_START,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_START,
   },
   {
     value: TaskReminderOptionId.m5,
-    label: T.F.TASK.D_REMINDER_ADD.RO_5M,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_5M,
   },
   {
     value: TaskReminderOptionId.m10,
-    label: T.F.TASK.D_REMINDER_ADD.RO_10M,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_10M,
   },
   {
     value: TaskReminderOptionId.m15,
-    label: T.F.TASK.D_REMINDER_ADD.RO_15M,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_15M,
   },
   {
     value: TaskReminderOptionId.m30,
-    label: T.F.TASK.D_REMINDER_ADD.RO_30M,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_30M,
   },
   {
     value: TaskReminderOptionId.h1,
-    label: T.F.TASK.D_REMINDER_ADD.RO_1H,
+    label: T.F.TASK.D_SCHEDULE_TASK.RO_1H,
   },
 ];
