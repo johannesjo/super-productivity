58109d41a22098adba421102d0f605a01bbc48b3
Johannes Millan <johannes.millan@gmail.com>
2024-09-09 12:47:56 +0200

    docs: add explanation for dateStrToUtcDate

--- src/app/util/date-str-to-utc-date.ts
+++ src/app/util/date-str-to-utc-date.ts
@@ -1,3 +1,14 @@
+//  since new Date("2021-01-01") will produce yesterday's date in timezones with negative timezone offset,
+// we need to convert date string to UTC date.
+// example use case:
+/*
+getDateTimeFromClockString(
+      workStartEndCfg.startTime,
+      dateStrToUtcDate(dayDate),
+)
+
+^ this would produce wrong day if the timezone offset is negative, if we were not using dateStrToUtcDate
+ */
 export const dateStrToUtcDate = (dateStr: string): Date => {
   if (!dateStr) {
     return new Date();
