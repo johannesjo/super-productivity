64a3e344329d63933da1b3318d44ee57ec2669c2
Johannes Millan <johannes.millan@gmail.com>
2024-09-16 21:54:14 +0200

    fix: build issues

--- src/app/imex/sync/local-file-sync/local-file-sync.module.ts
+++ src/app/imex/sync/local-file-sync/local-file-sync.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { LocalFileSyncElectronEffects } from './store/local-file-sync-electron.effects';
-import { IS_ELECTRON } from '../../../app.constants';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    IS_ELECTRON ? [EffectsModule.forFeature([LocalFileSyncElectronEffects])] : [],
-  ],
+  imports: [CommonModule, EffectsModule.forFeature([LocalFileSyncElectronEffects])],
 })
 export class LocalFileSyncModule {}
