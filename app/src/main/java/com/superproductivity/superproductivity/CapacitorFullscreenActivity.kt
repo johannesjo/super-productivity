c735e0e4ad4b7d61432142d10a9912d05ba2e1b4
Jiongxuan Zhang <jiongj.zhang@gmail.com>
2024-09-20 10:12:43 +0800

    chore: format code

--- app/src/main/java/com/superproductivity/superproductivity/CapacitorFullscreenActivity.kt
+++ app/src/main/java/com/superproductivity/superproductivity/CapacitorFullscreenActivity.kt
@@ -14,57 +14,59 @@ class CapacitorFullscreenActivity : BridgeActivity() {
         SimpleStorageHelper(this) // for scoped storage permission management on Android 10+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-      // Register Plugin
-      // TODO:
+        // Register Plugin
+        // TODO:
 
-      // Hide the action bar
-      supportActionBar?.hide()
+        // Hide the action bar
+        supportActionBar?.hide()
 
-      // Initialize JavaScriptInterface
-      javaScriptInterface = JavaScriptInterface(this, bridge.webView, storageHelper)
+        // Initialize JavaScriptInterface
+        javaScriptInterface = JavaScriptInterface(this, bridge.webView, storageHelper)
 
-      // Inject JavaScriptInterface into Capacitor's WebView
-      bridge.webView.addJavascriptInterface(javaScriptInterface,
-        WINDOW_INTERFACE_PROPERTY
-      )
-      if (BuildConfig.FLAVOR.equals("fdroid")) {
-        bridge.webView.addJavascriptInterface(javaScriptInterface,
-          WINDOW_PROPERTY_F_DROID
+        // Inject JavaScriptInterface into Capacitor's WebView
+        bridge.webView.addJavascriptInterface(
+            javaScriptInterface,
+            WINDOW_INTERFACE_PROPERTY
         )
-        // not ready in time, that's why we create a second JS interface just to fill the prop
-        // callJavaScriptFunction("window.$WINDOW_PROPERTY_F_DROID=true")
-      }
-
-      // Handle keyboard visibility changes
-      val rootView = findViewById<View>(android.R.id.content)
-      rootView.viewTreeObserver.addOnGlobalLayoutListener {
-        val rect = Rect()
-        rootView.getWindowVisibleDisplayFrame(rect)
-        val screenHeight = rootView.rootView.height
-
-        val keypadHeight = screenHeight - rect.bottom
-        if (keypadHeight > screenHeight * 0.15) {
-          // Keyboard is opened
-          callJavaScriptFunction("window.SUPAndroid.next.isKeyboardShown$('true')")
-        } else {
-          // Keyboard is closed
-          callJavaScriptFunction("window.SUPAndroid.next.isKeyboardShown$('false')")
+        if (BuildConfig.FLAVOR.equals("fdroid")) {
+            bridge.webView.addJavascriptInterface(
+                javaScriptInterface,
+                WINDOW_PROPERTY_F_DROID
+            )
+            // not ready in time, that's why we create a second JS interface just to fill the prop
+            // callJavaScriptFunction("window.$WINDOW_PROPERTY_F_DROID=true")
         }
-      }
+
+        // Handle keyboard visibility changes
+        val rootView = findViewById<View>(android.R.id.content)
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            rootView.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = rootView.rootView.height
+
+            val keypadHeight = screenHeight - rect.bottom
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is opened
+                callJavaScriptFunction("window.SUPAndroid.next.isKeyboardShown$('true')")
+            } else {
+                // Keyboard is closed
+                callJavaScriptFunction("window.SUPAndroid.next.isKeyboardShown$('false')")
+            }
+        }
     }
 
     override fun onPause() {
-      super.onPause()
-      Log.v("TW", "CapacitorFullscreenActivity: onPause")
-      callJavaScriptFunction("window.SUPAndroid.next.onPause$()")
+        super.onPause()
+        Log.v("TW", "CapacitorFullscreenActivity: onPause")
+        callJavaScriptFunction("window.SUPAndroid.next.onPause$()")
     }
 
     override fun onResume() {
-      super.onResume()
-      Log.v("TW", "CapacitorFullscreenActivity: onResume")
-      callJavaScriptFunction("window.SUPAndroid.next.onResume$()")
+        super.onResume()
+        Log.v("TW", "CapacitorFullscreenActivity: onResume")
+        callJavaScriptFunction("window.SUPAndroid.next.onResume$()")
     }
 
 //    override fun onBackPressed() {
@@ -74,13 +76,13 @@ class CapacitorFullscreenActivity : BridgeActivity() {
 //    }
 
     private fun callJavaScriptFunction(script: String) {
-      bridge?.webView?.post {
-        bridge?.webView?.evaluateJavascript(script, null)
-      }
+        bridge?.webView?.post {
+            bridge?.webView?.evaluateJavascript(script, null)
+        }
     }
 
-  companion object {
-    const val WINDOW_INTERFACE_PROPERTY: String = "SUPAndroid"
-    const val WINDOW_PROPERTY_F_DROID: String = "SUPFDroid"
-  }
+    companion object {
+        const val WINDOW_INTERFACE_PROPERTY: String = "SUPAndroid"
+        const val WINDOW_PROPERTY_F_DROID: String = "SUPFDroid"
+    }
 }
