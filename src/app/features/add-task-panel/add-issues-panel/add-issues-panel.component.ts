516391299f4d63dd0de08898a13b7a8f5c9cbd21
Johannes Millan <johannes.millan@gmail.com>
2024-11-18 16:39:07 +0100

    feat(issueProvider): draft 3

--- src/app/features/add-task-panel/add-issues-panel/add-issues-panel.component.ts
+++ src/app/features/add-task-panel/add-issues-panel/add-issues-panel.component.ts
@@ -1,21 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IssuePanelItemComponent } from './issue-panel-item/issue-panel-item.component';
-import { AsyncPipe } from '@angular/common';
-import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { UiModule } from '../../../ui/ui.module';
 
 @Component({
   selector: 'add-issues-panel',
   standalone: true,
-  imports: [
-    IssuePanelItemComponent,
-    AsyncPipe,
-    CdkDrag,
-    CdkDropList,
-    MatIcon,
-    MatIconButton,
-  ],
+  imports: [UiModule, IssuePanelItemComponent, MatIcon, MatFormField, MatLabel],
   templateUrl: './add-issues-panel.component.html',
   styleUrl: './add-issues-panel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/features/add-task-panel/add-issues-panel/issue-panel-item/issue-panel-item.component.ts
+++ src/app/features/add-task-panel/add-issues-panel/issue-panel-item/issue-panel-item.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconButton, MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'issue-panel-item',
   standalone: true,
-  imports: [],
+  imports: [MatIconButton, MatIcon, MatMiniFabButton],
   templateUrl: './issue-panel-item.component.html',
   styleUrl: './issue-panel-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
