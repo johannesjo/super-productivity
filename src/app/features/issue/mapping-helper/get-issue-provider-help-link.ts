9a97d50c96305ef07a234809c85e07b1de4acb1a
Johannes Millan <johannes.millan@gmail.com>
2024-11-23 16:27:19 +0100

    feat(issueProvider): improve link

--- src/app/features/issue/mapping-helper/get-issue-provider-help-link.ts
+++ src/app/features/issue/mapping-helper/get-issue-provider-help-link.ts
@@ -7,7 +7,7 @@ export const getIssueProviderHelpLink = (
     case 'JIRA':
       return 'https://support.atlassian.com/jira-service-management-cloud/docs/use-advanced-search-with-jira-query-language-jql/';
     case 'GITHUB':
-      return 'https://docs.github.com/en/issues/tracking-your-work-with-issues/using-issues/filtering-and-searching-issues-and-pull-requests';
+      return 'https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests';
     case 'GITLAB':
       return 'https://docs.gitlab.com/ee/user/search/advanced_search.html';
     // case 'GITEA':
