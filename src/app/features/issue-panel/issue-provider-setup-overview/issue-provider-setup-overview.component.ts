5414351d658b55257492f24a27ab8dc0a2264772
Johannes Millan <johannes.millan@gmail.com>
2024-12-06 21:03:47 +0100

    feat(issueProvider): add ui for making providers sortable

--- src/app/features/issue-panel/issue-provider-setup-overview/issue-provider-setup-overview.component.ts
+++ src/app/features/issue-panel/issue-provider-setup-overview/issue-provider-setup-overview.component.ts
@@ -1,18 +1,26 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
 import { T } from '../../../t.const';
 import { MatIcon } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
-import { IssueProviderKey } from '../../issue/issue.model';
+import { IssueProvider, IssueProviderKey } from '../../issue/issue.model';
 import { DialogEditIssueProviderComponent } from '../../issue/dialog-edit-issue-provider/dialog-edit-issue-provider.component';
 import { Store } from '@ngrx/store';
 import { MatDialog } from '@angular/material/dialog';
 import { UiModule } from '../../../ui/ui.module';
 import { CalendarContextInfoTarget } from '../../issue/providers/calendar/calendar.model';
+import { selectEnabledIssueProviders } from '../../issue/store/issue-provider.selectors';
+import { IssueModule } from '../../issue/issue.module';
+import {
+  getIssueProviderInitials,
+  getIssueProviderTooltip,
+} from '../../issue/mapping-helper/get-issue-provider-tooltip';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'issue-provider-setup-overview',
   standalone: true,
-  imports: [UiModule, MatIcon, TranslateModule],
+  imports: [UiModule, MatIcon, TranslateModule, IssueModule],
   templateUrl: './issue-provider-setup-overview.component.html',
   styleUrl: './issue-provider-setup-overview.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,6 +29,27 @@ export class IssueProviderSetupOverviewComponent {
   protected readonly T = T;
   private _store = inject(Store);
   private _matDialog = inject(MatDialog);
+
+  enabledProviders$ = this._store.select(selectEnabledIssueProviders);
+
+  issueProvidersMapped: Signal<
+    {
+      issueProvider: IssueProvider;
+      initials?: string | null;
+      tooltip: string;
+    }[]
+  > = toSignal(
+    this.enabledProviders$.pipe(
+      map((issueProviders) =>
+        issueProviders.map((p) => ({
+          issueProvider: p,
+          initials: getIssueProviderInitials(p),
+          tooltip: getIssueProviderTooltip(p),
+        })),
+      ),
+    ),
+    { initialValue: [] },
+  );
 
   openSetupDialog(
     issueProviderKey: IssueProviderKey,
