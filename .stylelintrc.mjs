19b57935ed28506098b59ad45f5cfa6dd2227eec
blyedev <adam.smielewski@blyedev.com>
2024-12-19 21:46:02 +0100

    build(stylelint): tweak stylelint settings
    Disable a few rules that should not apply to this repo

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -4,6 +4,10 @@ export default {
   rules: {
     'no-empty-source': null,
     'no-descending-specificity': null,
+    'selector-type-no-unknown': null, // stylelint doesn't recognize angular components
+    'scss/comment-no-empty': null, // allow for double slash multiline comments
+    'scss/load-no-partial-leading-underscore': null, // this project uses underscored files
+    'scss/operator-no-newline-after': null, // prettier conficts with this rule
     'selector-pseudo-element-no-unknown': [
       true,
       {
