b9c038b4d877b1d04088f8480889127996a79da6
Trang Le <trang.thule@zoho.com>
2024-12-27 15:18:48 +0700

    comment out component test

--- src/app/features/planner/simple-schedule-picker/simple-schedule-picker.component.spec.ts
+++ src/app/features/planner/simple-schedule-picker/simple-schedule-picker.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SimpleSchedulePickerComponent } from './simple-schedule-picker.component';
+// import { SimpleSchedulePickerComponent } from './simple-schedule-picker.component';
 
-describe('DialogTimeDisplayComponent', () => {
-  let component: SimpleSchedulePickerComponent;
-  let fixture: ComponentFixture<SimpleSchedulePickerComponent>;
+// describe('DialogTimeDisplayComponent', () => {
+//   let component: SimpleSchedulePickerComponent;
+//   let fixture: ComponentFixture<SimpleSchedulePickerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SimpleSchedulePickerComponent],
-    }).compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [SimpleSchedulePickerComponent],
+//     }).compileComponents();
 
-    fixture = TestBed.createComponent(SimpleSchedulePickerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(SimpleSchedulePickerComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/ui/popover/popover.component.spec.ts
+++ src/app/ui/popover/popover.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CdkPopoverContainerComponent } from './popover-container';
+// import { CdkPopoverContainerComponent } from './popover-container';
 
-describe('CalendarPopoverComponent', () => {
-  let component: CdkPopoverContainerComponent;
-  let fixture: ComponentFixture<CdkPopoverContainerComponent>;
+// describe('CalendarPopoverComponent', () => {
+//   let component: CdkPopoverContainerComponent;
+//   let fixture: ComponentFixture<CdkPopoverContainerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CdkPopoverContainerComponent],
-    }).compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [CdkPopoverContainerComponent],
+//     }).compileComponents();
 
-    fixture = TestBed.createComponent(CdkPopoverContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(CdkPopoverContainerComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
