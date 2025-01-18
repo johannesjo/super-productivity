fda4ba796bc01a23b6b101f9a540c816660e7de2
Johannes Millan <johannes.millan@gmail.com>
2025-01-18 19:41:55 +0100

    feat(simpleCounter): add chart to simple counter edit dialog

--- src/app/features/simple-counter/dialog-simple-counter-edit/dialog-simple-counter-edit.component.ts
+++ src/app/features/simple-counter/dialog-simple-counter-edit/dialog-simple-counter-edit.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -7,7 +7,7 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { T } from '../../../t.const';
-import { SimpleCounter, SimpleCounterType } from '../simple-counter.model';
+import { SimpleCounterCopy, SimpleCounterType } from '../simple-counter.model';
 import { SimpleCounterService } from '../simple-counter.service';
 import { DateService } from 'src/app/core/date/date.service';
 import { FormsModule } from '@angular/forms';
@@ -17,6 +17,9 @@ import { InputDurationDirective } from '../../../ui/duration/input-duration.dire
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { TranslatePipe } from '@ngx-translate/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
+import { LineChartData } from '../../metric/metric.model';
 
 @Component({
   selector: 'dialog-simple-counter-edit',
@@ -35,6 +38,7 @@ import { TranslatePipe } from '@ngx-translate/core';
     MatDialogActions,
     MatButton,
     TranslatePipe,
+    BaseChartDirective,
   ],
 })
 export class DialogSimpleCounterEditComponent {
@@ -42,15 +46,51 @@ export class DialogSimpleCounterEditComponent {
     inject<MatDialogRef<DialogSimpleCounterEditComponent>>(MatDialogRef);
   private _simpleCounterService = inject(SimpleCounterService);
   private _dateService = inject(DateService);
+
   data = inject<{
-    simpleCounter: SimpleCounter;
+    simpleCounter: SimpleCounterCopy;
   }>(MAT_DIALOG_DATA);
+
+  stats = computed(() => {
+    const countOnDay = this.data.simpleCounter.countOnDay;
+    const labels = Object.keys(countOnDay);
+    const data =
+      this.data.simpleCounter.type === SimpleCounterType.StopWatch
+        ? labels.map((date) => Math.round(countOnDay[date] / 60000))
+        : labels.map((date) => countOnDay[date]);
+    const chartData: LineChartData = {
+      labels,
+      datasets: [
+        {
+          data,
+          label: 'Count',
+          fill: false,
+          borderColor: '#4bc0c0',
+        },
+      ],
+    };
+    return chartData;
+  });
 
   T: typeof T = T;
   SimpleCounterType: typeof SimpleCounterType = SimpleCounterType;
 
   todayStr: string = this._dateService.todayStr();
   val: number = this.data.simpleCounter.countOnDay[this.todayStr];
+  lineChartOptions: ChartConfiguration<
+    'line',
+    (number | undefined)[],
+    string
+  >['options'] = {
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          precision: 0,
+        },
+      },
+    },
+  };
 
   submit(): void {
     this._simpleCounterService.setCounterToday(this.data.simpleCounter.id, this.val);
