59c28caac86970b6c4e127f72ba8876c9d302c56
Johannes Millan <johannes.millan@gmail.com>
2025-02-20 12:37:11 +0100

    feat: add animation for stopwatch

--- src/app/ui/progress-circle/progress-circle.component.ts
+++ src/app/ui/progress-circle/progress-circle.component.ts
@@ -7,7 +7,12 @@ import {
   effect,
   Renderer2,
   viewChild,
+  linkedSignal,
+  OnDestroy,
 } from '@angular/core';
+import { lazySetInterval } from '../../../../electron/shared-with-frontend/lazy-set-interval';
+
+const TICK = 1000;
 
 @Component({
   selector: 'progress-circle',
@@ -15,18 +20,38 @@ import {
   styleUrls: ['./progress-circle.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProgressCircleComponent {
+export class ProgressCircleComponent implements OnDestroy {
   private readonly _renderer = inject(Renderer2);
 
   progress = input<number>();
+  progressIntermediary = linkedSignal(() => this.progress() || 0);
+  autoRotationDuration = input<number | undefined>();
 
   readonly progressCircle = viewChild<ElementRef>('progressCircle');
 
+  private _timeOutCancelFn?: () => void;
+
   constructor() {
+    effect(() => {
+      const autoRotation = this.autoRotationDuration();
+      if (autoRotation) {
+        const step = autoRotation / TICK;
+        const percentagePerStep = 100 / step;
+
+        this._timeOutCancelFn = lazySetInterval(() => {
+          const newProgress = this.progressIntermediary() + percentagePerStep;
+
+          this.progressIntermediary.set(
+            newProgress >= 100 ? newProgress - 100 : newProgress,
+          );
+        }, TICK);
+      }
+    });
+
     effect(() => {
       const progressCircle = this.progressCircle();
       if (progressCircle) {
-        let progress = this.progress() || 0;
+        let progress = this.progressIntermediary() || 0;
         if (progress > 100) {
           progress = 100;
         }
@@ -38,5 +63,11 @@ export class ProgressCircleComponent {
         );
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this._timeOutCancelFn) {
+      this._timeOutCancelFn();
+    }
   }
 }
