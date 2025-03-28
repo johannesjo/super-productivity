b7fb14f089bcbfac3550f9ea687904b1fe7498f1
Johannes Millan <johannes.millan@gmail.com>
2025-03-28 20:48:08 +0100

    fix: invalid translation variables #4179 {{}}

--- src/app/features/domina-mode/store/domina-mode.effects.ts
+++ src/app/features/domina-mode/store/domina-mode.effects.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect } from '@ngrx/effects';
 import { EMPTY, Observable, timer } from 'rxjs';
 import { distinctUntilChanged, switchMap, tap, withLatestFrom } from 'rxjs/operators';
