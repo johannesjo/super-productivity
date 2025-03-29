96172deb6d347b49384c108b99c3cc0718ae709b
Johannes Millan <johannes.millan@gmail.com>
2025-03-29 16:14:44 +0100

    feat(pfapi): outline new time tracking model

---
+++ src/app/imex/sync/time-tracking.model.ts
@@ -0,0 +1,29 @@
+export interface TimeTrackingMapByEntryId<T> {
+  [modelEntryId: string]: T;
+}
+
+export interface TimeTrackingForDay<T> {
+  [date: string]: T;
+}
+
+export interface TimeTrackingWorkStartEnd {
+  start: number;
+  end: number;
+}
+
+export interface TimeTrackingBreakCount {
+  nr: number;
+  time: number;
+}
+
+export interface TimeTracking {
+  project: TimeTrackingMapByEntryId<TimeTrackingForDay<TimeTrackingWorkStartEnd>>;
+  tag: TimeTrackingMapByEntryId<TimeTrackingForDay<TimeTrackingWorkStartEnd>>;
+  task: TimeTrackingMapByEntryId<TimeTrackingForDay<number>>;
+  break: TimeTrackingMapByEntryId<TimeTrackingForDay<TimeTrackingBreakCount>>;
+  lastFlush: number;
+}
+
+export interface TimeTrackingArchive extends TimeTracking {
+  newestEntry: number;
+}
