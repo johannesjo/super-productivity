cb2c5565e8cd659a4fecad1362e9b9d7e7ee29a7
Johannes Millan <johannes.millan@gmail.com>
2025-03-31 14:39:06 +0200

    feat(pfapi): improve migration experience for electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,6 +58,7 @@ import { PfapiService } from './pfapi/pfapi.service';
 import { PersistenceLegacyService } from './core/persistence/persistence-legacy.service';
 import { download } from './util/download';
 import { PersistenceLocalService } from './core/persistence/persistence-local.service';
+import { SyncStatus } from './pfapi/api';
 
 const w = window as any;
 const productivityTip: string[] = w.productivityTips && w.productivityTips[w.randomIndex];
@@ -148,24 +149,38 @@ export class AppComponent implements OnDestroy {
         if (isImportingBackupOrNone) {
           return;
         }
-        const MIGRATED_VAL = 0;
+        const MIGRATED_VAL = 37;
         const lastLocalSyncModelChange =
           await this._persistenceLocalService.loadLastSyncModelChange();
         if (
           typeof lastLocalSyncModelChange === 'number' &&
           lastLocalSyncModelChange > MIGRATED_VAL
         ) {
+          // disable sync until reload
+          this._pfapiService.sync = () => Promise.resolve({ status: SyncStatus.InSync });
+          this.imexMetaService.setDataImportInProgress(true);
+
           const legacyData = await this._persistenceLegacyService.loadComplete();
           console.log({ legacyData: legacyData });
+
           alert('Detected legacy data. We will migrate it for you! ');
-          if (confirm('Do you want to download a backup of your legacy data?')) {
+          if (
+            confirm(
+              'Do you want to download a backup of your legacy data (can be used with older versions of Super Productivity)?',
+            )
+          ) {
             download('sp-legacy-backup.json', JSON.stringify(legacyData));
           }
           try {
             await this._pfapiService.importCompleteBackup(legacyData, true, true);
+            this.imexMetaService.setDataImportInProgress(true);
             await this._persistenceLocalService.updateLastSyncModelChange(MIGRATED_VAL);
             alert('Migration all done! Restarting app now...');
+            if (IS_ELECTRON) {
+              window.ea.relaunch();
+            }
             window.location.reload();
+            window.setTimeout(() => window.location.reload());
           } catch (error) {
             console.error(error);
             alert('Migration failed! with Error: ' + error?.toString());
