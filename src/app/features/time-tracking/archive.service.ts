c7439c65496b03aec885bae864ffe4f377ca1b81
Johannes Millan <johannes.millan@gmail.com>
2025-04-04 20:52:58 +0200

    feat(timeTracking): adjust flushing logic once more

--- src/app/features/time-tracking/archive.service.ts
+++ src/app/features/time-tracking/archive.service.ts
@@ -27,18 +27,17 @@ timeTracking:
 * (currently) there seems to be no writing of archiveYoung or archiveOld like there is for archive tasks, when editing tasks in worklog
 => archiveOld.timeTracking and archiveYoung.timeTracking can be read-only
 * data for today should never be in the archive and always be in the store to avoid problems when doing partial updates
-=> timeTracking should always retain some data, at least for today, probably better for the current week
+=> timeTracking should always retain some data, at least for today (or  maybe later for the whole current week, if we want to make it editable)
 
 taskArchive:
 * data in archiveYoung should be faster to access and write
 * when updating some old data, we need to upload archiveOld regardless of flushing
 => makes sense to retain data in archiveYoung that is likely to be accessed more often
-=> 31 days is maybe a good middle ground for this, since it allows us to access and write data from the last month
+=> 21 days is maybe a good middle ground for this, since it allows us to write data from the last month
  */
 
-export const ARCHIVE_CURRENT_TO_YOUNG_THRESHOLD = 1000 * 60 * 60 * 24 * 14; // two weeks
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const ARCHIVE_YOUNG_TO_OLD_THRESHOLD = 1000 * 60 * 60 * 24 * 31; // 31 days
+export const ARCHIVE_ALL_YOUNG_TO_OLD_THRESHOLD = 1000 * 60 * 60 * 24 * 14;
+export const ARCHIVE_TASK_YOUNG_TO_OLD_THRESHOLD = 1000 * 60 * 60 * 24 * 21;
 
 @Injectable({
   providedIn: 'root',
@@ -75,27 +74,10 @@ export class ArchiveService {
       taskArchiveState,
     );
 
-    const isFlushTimeTracking =
-      now - archiveYoung.lastTimeTrackingFlush > ARCHIVE_CURRENT_TO_YOUNG_THRESHOLD;
-
-    // Result A: Just move tasks to archive
-    if (!isFlushTimeTracking) {
-      await this._pfapiService.m.archiveYoung.save(
-        {
-          timeTracking: archiveYoung.timeTracking,
-          lastTimeTrackingFlush: archiveYoung.lastTimeTrackingFlush,
-          task: newTaskArchive,
-        },
-        {
-          isUpdateRevAndLastUpdate: true,
-        },
-      );
-      return;
-    }
-
     // ------------------------------------------------
-    // Result B:
-    // Also move timeTracking data to archiveYoung
+    // Result A:
+    // Move all archived tasks to archiveYoung
+    // Move timeTracking data to archiveYoung
     const newSorted1 = sortTimeTrackingDataToArchiveYoung({
       // TODO think if it is better to get this from store as it is fresher potentially
       timeTracking: await this._pfapiService.m.timeTracking.load(),
@@ -119,18 +101,21 @@ export class ArchiveService {
     // ------------------------------------------------
     const archiveOld = await this._pfapiService.m.archiveOld.load();
     const isFlushArchiveOld =
-      now - archiveOld.lastTimeTrackingFlush > ARCHIVE_YOUNG_TO_OLD_THRESHOLD;
+      now - archiveOld.lastTimeTrackingFlush > ARCHIVE_ALL_YOUNG_TO_OLD_THRESHOLD;
+
+    alert('FLUSHED TIME TRACKING TO ARCHIVE YOUNG');
+
     if (!isFlushArchiveOld) {
       return;
     }
 
     // ------------------------------------------------
-    // Result C:
+    // Result B:
     // Also sort timeTracking and task data from archiveYoung to archiveOld
     const newSorted2 = sortTimeTrackingAndTasksFromArchiveYoungToOld({
       archiveYoung: newArchiveYoung,
       archiveOld,
-      threshold: ARCHIVE_YOUNG_TO_OLD_THRESHOLD,
+      threshold: ARCHIVE_TASK_YOUNG_TO_OLD_THRESHOLD,
       now,
     });
     await this._pfapiService.m.archiveYoung.save(
@@ -151,5 +136,6 @@ export class ArchiveService {
         isUpdateRevAndLastUpdate: true,
       },
     );
+    alert('FLUSHED ALL FROM ARCHIVE YOUNG TO OLD');
   }
 }
