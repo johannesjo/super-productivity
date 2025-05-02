0af26aed4dc70a362e526fec37b855bc1d672cc4
Johannes Millan <johannes.millan@gmail.com>
2025-05-02 18:55:29 +0200

    feat(dueDate): deactivate logger again

--- src/app/root-store/meta/action-logger.reducer.ts
+++ src/app/root-store/meta/action-logger.reducer.ts
@@ -1,15 +1,17 @@
 import { RootState } from '../root-state';
 import { actionLogger } from '../../util/action-logger';
 import { ActionReducer } from '@ngrx/store/src/models';
+import { IS_ELECTRON } from '../../app.constants';
+import { environment } from '../../../environments/environment';
 
 export const actionLoggerReducer = (
   reducer: ActionReducer<any, any>,
 ): ActionReducer<any, any> => {
   return (state: RootState, action: any) => {
     // if (environment.production) {
-    // if (environment.production || IS_ELECTRON) {
-    console.log(action.type, (action as any)?.payload || action);
-    // }
+    if (environment.production || IS_ELECTRON) {
+      console.log(action.type, (action as any)?.payload || action);
+    }
     actionLogger(action);
     return reducer(state, action);
   };
