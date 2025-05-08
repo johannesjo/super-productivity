d0f3bbbd560d07c65219018198603f965433f015
Johannes Millan <johannes.millan@gmail.com>
2025-05-08 17:29:56 +0200

    feat: improve db saving

--- src/app/root-store/shared/save-to-db.effects.ts
+++ src/app/root-store/shared/save-to-db.effects.ts
@@ -1,8 +1,7 @@
 import { inject, Injectable } from '@angular/core';
-import { createEffect } from '@ngrx/effects';
-import { Observable } from 'rxjs';
+import { Actions, createEffect } from '@ngrx/effects';
 import { skip, switchMap } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
+import { MemoizedSelector, Store } from '@ngrx/store';
 import { PfapiService } from '../../pfapi/pfapi.service';
 import { selectBoardsState } from '../../features/boards/store/boards.selectors';
 import { selectConfigFeatureState } from '../../features/config/store/global-config.reducer';
@@ -17,6 +16,7 @@ import { selectObstructionFeatureState } from '../../features/metric/obstruction
 import { selectTaskRepeatCfgFeatureState } from '../../features/task-repeat-cfg/store/task-repeat-cfg.reducer';
 import { selectMetricFeatureState } from '../../features/metric/store/metric.selectors';
 import { DataInitStateService } from '../../core/data-init/data-init-state.service';
+import { RootState } from '../root-state';
 
 // NOTE task state is a bit special, since we have ui only actions and we throttle addTimeSpent
 // NOTE TimeTrackingState is also a bit special, since we throttle addTimeSpent
@@ -24,173 +24,51 @@ import { DataInitStateService } from '../../core/data-init/data-init-state.servi
 
 @Injectable()
 export class SaveToDbEffects {
-  private _store = inject(Store);
+  private _store = inject(Store<RootState>);
+  private _actions = inject(Actions);
   private _pfapiService = inject(PfapiService);
   private _dataInitStateService = inject(DataInitStateService);
 
-  taskRepeatCfg$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectTaskRepeatCfgFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.taskRepeatCfg.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
+  // Infer the correct type for each modelKey from PfapiService['m']
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  private createSaveEffect<K extends keyof typeof this._pfapiService.m>(
+    selector: MemoizedSelector<
+      RootState,
+      Parameters<(typeof this._pfapiService.m)[K]['save']>[0]
+    >,
+    modelKey: K,
+  ) {
+    return createEffect(
+      () =>
+        this._dataInitStateService.isAllDataLoadedInitially$.pipe(
+          switchMap(() => this._store.select(selector)),
+          skip(1),
+          switchMap((state) =>
+            this._pfapiService.m[modelKey].save(state, {
+              isUpdateRevAndLastUpdate: true,
+            }),
+          ),
         ),
-      ),
-    { dispatch: false },
-  );
+      { dispatch: false },
+    );
+  }
 
-  tag$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectTagFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.tag.save(state, { isUpdateRevAndLastUpdate: true }),
-        ),
-      ),
-    { dispatch: false },
+  tag$ = this.createSaveEffect(selectTagFeatureState, 'tag');
+  project$ = this.createSaveEffect(selectProjectFeatureState, 'project');
+  globalCfg$ = this.createSaveEffect(selectConfigFeatureState, 'globalConfig');
+  planner$ = this.createSaveEffect(selectPlannerState, 'planner');
+  boards$ = this.createSaveEffect(selectBoardsState, 'boards');
+  issueProvider$ = this.createSaveEffect(selectIssueProviderState, 'issueProvider');
+  note$ = this.createSaveEffect(selectNoteFeatureState, 'note');
+  metric$ = this.createSaveEffect(selectMetricFeatureState, 'metric');
+  improvement$ = this.createSaveEffect(selectImprovementFeatureState, 'improvement');
+  obstruction$ = this.createSaveEffect(selectObstructionFeatureState, 'obstruction');
+  simpleCounter$ = this.createSaveEffect(
+    selectSimpleCounterFeatureState,
+    'simpleCounter',
   );
-
-  project$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectProjectFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.project.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  globalCfg$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectConfigFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.globalConfig.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  planner$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectPlannerState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.planner.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  boards$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectBoardsState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.boards.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  simpleCounter$: Observable<unknown> = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectSimpleCounterFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.simpleCounter.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  issueProvider$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectIssueProviderState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.issueProvider.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  note$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectNoteFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.note.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  metric$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectMetricFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.metric.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  improvement$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectImprovementFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.improvement.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  obstruction$ = createEffect(
-    () =>
-      this._dataInitStateService.isAllDataLoadedInitially$.pipe(
-        switchMap(() => this._store.select(selectObstructionFeatureState)),
-        skip(1),
-        switchMap((state) =>
-          this._pfapiService.m.obstruction.save(state, {
-            isUpdateRevAndLastUpdate: true,
-          }),
-        ),
-      ),
-    { dispatch: false },
+  taskRepeatCfg$ = this.createSaveEffect(
+    selectTaskRepeatCfgFeatureState,
+    'taskRepeatCfg',
   );
 }
