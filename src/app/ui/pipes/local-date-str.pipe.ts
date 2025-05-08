a1c4d527d2a5134276b6d618d7e06a681a217572
Johannes Millan <johannes.millan@gmail.com>
2025-05-08 13:09:43 +0200

    feat(dueDate): improve day changes 1

--- src/app/ui/pipes/local-date-str.pipe.ts
+++ src/app/ui/pipes/local-date-str.pipe.ts
@@ -1,22 +1,22 @@
 import { inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { dateStrToUtcDate } from '../../util/date-str-to-utc-date';
-import { getWorklogStr } from '../../util/get-work-log-str';
 import { TranslateService } from '@ngx-translate/core';
 import { T } from 'src/app/t.const';
+import { getWorklogStr } from '../../util/get-work-log-str';
 
 @Pipe({ name: 'localDateStr', standalone: true })
 export class LocalDateStrPipe implements PipeTransform {
-  private datePipe = inject(DatePipe);
   private locale = inject(LOCALE_ID);
   private translateService = inject(TranslateService);
-  // TODO add today case
 
-  transform(value: string | null, ...args: unknown[]): string | null {
+  transform(
+    value: string | null | undefined,
+    todayStr: string = getWorklogStr(),
+  ): string | null {
     if (typeof value !== 'string') {
       return null;
     }
-    if (value === getWorklogStr()) {
+    if (value === todayStr) {
       const translation = this.translateService.instant(T.G.TODAY_TAG_TITLE);
       if (translation.length <= 7) {
         return translation;
