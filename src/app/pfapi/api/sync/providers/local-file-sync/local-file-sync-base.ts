4d8fdf59da8969c2cd9225cbfcc43d12f4c9757d
Johannes Millan <johannes.millan@gmail.com>
2025-06-09 09:00:36 +0200

    fix(sync): make android error handling more robust

--- src/app/pfapi/api/sync/providers/local-file-sync/local-file-sync-base.ts
+++ src/app/pfapi/api/sync/providers/local-file-sync/local-file-sync-base.ts
@@ -80,7 +80,7 @@ export abstract class LocalFileSyncBase
       // TODO move to file adapters
       // Handle common file not found errors
       if (
-        e?.toString?.().includes('File does not exist') ||
+        e?.toString?.().includes('does not exist') ||
         e?.toString?.().includes('ENOENT')
       ) {
         throw new RemoteFileNotFoundAPIError(targetPath);
