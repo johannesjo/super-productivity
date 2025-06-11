5936f538134875d286c5277aa7a24988cab81265
Johannes Millan <johannes.millan@gmail.com>
2025-06-11 17:21:17 +0200

    feat(plugin): add more stuff to bridges

--- src/app/plugins/plugin-hooks.effects.ts
+++ src/app/plugins/plugin-hooks.effects.ts
@@ -8,11 +8,14 @@ import {
   deleteTask,
   deleteTasks,
   updateTask,
+  addTask,
 } from '../features/tasks/store/task.actions';
 import { selectTaskById } from '../features/tasks/store/task.selectors';
 import { Task } from '../features/tasks/task.model';
 import { PluginService } from './plugin.service';
 import { PluginHooks } from './plugin-api.model';
+import { updateGlobalConfigSection } from '../features/config/store/global-config.actions';
+import { setActiveWorkContext } from '../features/work-context/store/work-context.actions';
 
 @Injectable()
 export class PluginHooksEffects {
@@ -81,6 +84,74 @@ export class PluginHooksEffects {
         tap((action) => {
           this.pluginService.dispatchHook(PluginHooks.TASK_DELETE, {
             taskIds: action.taskIds,
+          });
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  // Effect for task creation
+  taskAdd$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(addTask),
+        switchMap((action) =>
+          this.store.pipe(
+            select(selectTaskById, { id: action.task.id }),
+            take(1),
+            filter((task) => !!task),
+            tap((task: Task | undefined) => {
+              if (task) {
+                this.pluginService.dispatchHook(PluginHooks.TASK_UPDATE, task);
+              }
+            }),
+            map(() => EMPTY),
+          ),
+        ),
+      ),
+    { dispatch: false },
+  );
+
+  // Effect for configuration changes
+  configChange$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(updateGlobalConfigSection),
+        tap((action) => {
+          this.pluginService.dispatchHook(PluginHooks.CFG_CHANGE, {
+            sectionKey: action.sectionKey,
+            sectionCfg: action.sectionCfg,
+          });
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  // Effect for work context changes
+  workContextChange$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(setActiveWorkContext),
+        tap((action) => {
+          this.pluginService.dispatchHook(PluginHooks.LANGUAGE_CHANGE, {
+            activeId: action.activeId,
+            activeType: action.activeType,
+          });
+        }),
+      ),
+    { dispatch: false },
+  );
+
+  // Effect for when the day ends (finish day)
+  finishDay$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        // This would need to be connected to a finish day action
+        // For now, we'll leave it as a placeholder
+        filter(() => false), // Never triggers for now
+        tap(() => {
+          this.pluginService.dispatchHook(PluginHooks.FINISH_DAY, {
+            timestamp: Date.now(),
           });
         }),
       ),
