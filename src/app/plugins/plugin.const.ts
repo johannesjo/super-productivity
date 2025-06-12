839cb3dbd64a50405c879f54384ccea473102740
Johannes Millan <johannes.millan@gmail.com>
2025-06-12 15:43:30 +0200

    feat(plugin): add max sizes

---
+++ src/app/plugins/plugin.const.ts
@@ -0,0 +1,6 @@
+/**
+ * Plugin system constants
+ */
+export const MAX_PLUGIN_ZIP_SIZE = 1024 * 1024;
+export const MAX_PLUGIN_MANIFEST_SIZE = 10 * 1024;
+export const MAX_PLUGIN_CODE_SIZE = 512 * 1024;
