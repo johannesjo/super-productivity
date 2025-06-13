1255301b0b05bd2949d1baf798782315f0b88521
Johannes Millan <johannes.millan@gmail.com>
2025-06-13 17:17:08 +0200

    feat(plugin): prepare api package

---
+++ packages/plugin-api/dist/index.d.ts
@@ -0,0 +1,20 @@
+export * from './types';
+export type {
+  PluginAPI,
+  PluginManifest,
+  PluginHooks,
+  Hooks,
+  TaskData,
+  ProjectData,
+  TagData,
+  PluginCreateTaskData,
+  PluginBaseCfg,
+  DialogCfg,
+  DialogButtonCfg,
+  SnackCfg,
+  NotifyCfg,
+  PluginMenuEntryCfg,
+  PluginShortcutCfg,
+  PluginHeaderBtnCfg,
+} from './types';
+//# sourceMappingURL=index.d.ts.map
