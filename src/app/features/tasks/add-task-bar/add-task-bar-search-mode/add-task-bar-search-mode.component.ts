3194fd2616af5499c2eba1cb93ee0efa63bbf154
Johannes Millan <johannes.millan@gmail.com>
2025-08-08 16:39:09 +0200

    feat(addTaskBar): first draft

---
+++ src/app/features/tasks/add-task-bar/add-task-bar-search-mode/add-task-bar-search-mode.component.ts
@@ -0,0 +1,231 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  output,
+  input,
+  signal,
+  viewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import {
+  MatAutocomplete,
+  MatAutocompleteTrigger,
+  MatOption,
+} from '@angular/material/autocomplete';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { AsyncPipe } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { TaskService } from '../../task.service';
+import { WorkContextService } from '../../../work-context/work-context.service';
+import { IssueService } from '../../../issue/issue.service';
+import { SnackService } from '../../../../core/snack/snack.service';
+import { T } from '../../../../t.const';
+import { AddTaskSuggestion } from '../add-task-suggestions.model';
+import { AddTaskBarService } from '../add-task-bar.service';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { debounceTime, switchMap, map, tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { IssueIconPipe } from '../../../issue/issue-icon/issue-icon.pipe';
+import { TagComponent } from '../../../tag/tag/tag.component';
+import { IS_ANDROID_WEB_VIEW } from '../../../../util/is-android-web-view';
+import { truncate } from '../../../../util/truncate';
+import { WorkContextType } from '../../../work-context/work-context.model';
+
+@Component({
+  selector: 'add-task-bar-search-mode',
+  templateUrl: './add-task-bar-search-mode.component.html',
+  styleUrls: ['./add-task-bar-search-mode.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    MatTooltip,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatOption,
+    MatProgressSpinner,
+    AsyncPipe,
+    TranslatePipe,
+    IssueIconPipe,
+    TagComponent,
+  ],
+})
+export class AddTaskBarSearchModeComponent implements AfterViewInit {
+  private _taskService = inject(TaskService);
+  private _workContextService = inject(WorkContextService);
+  private _issueService = inject(IssueService);
+  private _snackService = inject(SnackService);
+  private _addTaskBarService = inject(AddTaskBarService);
+
+  tabindex = input<number>(0);
+  isElevated = input<boolean>(false);
+  isDisableAutoFocus = input<boolean>(false);
+  isAddToBacklog = input<boolean>(false);
+  taskIdsToExclude = input<string[]>();
+
+  afterTaskAdd = output<{ taskId: string; isAddToBottom: boolean }>();
+  blurred = output<void>();
+  switchToAddMode = output<void>();
+
+  T = T;
+
+  inputEl = viewChild<ElementRef>('inputEl');
+  searchControl = new FormControl<string>('');
+
+  isLoading = signal(false);
+  searchType = signal<'tasks' | 'issues'>('tasks');
+
+  activatedSuggestion$ = new BehaviorSubject<AddTaskSuggestion | null>(null);
+
+  suggestions$: Observable<AddTaskSuggestion[]> = this.searchControl.valueChanges.pipe(
+    tap(() => this.isLoading.set(true)),
+    debounceTime(300),
+    switchMap((searchTerm) => {
+      if (!searchTerm || typeof searchTerm !== 'string') {
+        this.isLoading.set(false);
+        return of([]);
+      }
+
+      if (this.searchType() === 'issues') {
+        return this._issueService.searchAllEnabledIssueProviders$(searchTerm).pipe(
+          map((issueSuggestions) =>
+            issueSuggestions.map(
+              (issueSuggestion) =>
+                ({
+                  title: issueSuggestion.title,
+                  titleHighlighted: issueSuggestion.titleHighlighted,
+                  issueData: issueSuggestion.issueData,
+                  issueType: issueSuggestion.issueType,
+                  issueProviderId: issueSuggestion.issueProviderId,
+                }) as AddTaskSuggestion,
+            ),
+          ),
+          catchError(() => of([])),
+        );
+      } else {
+        return this._workContextService.activeWorkContextTypeAndId$.pipe(
+          switchMap(({ activeType, activeId }) => {
+            return this._addTaskBarService.getFilteredIssueSuggestions$(
+              this.searchControl,
+              of(false),
+              this.isLoading,
+            );
+          }),
+        );
+      }
+    }),
+    tap(() => this.isLoading.set(false)),
+    map((suggestions) => {
+      const taskIdsToExclude = this.taskIdsToExclude() || [];
+      return suggestions.filter((s) => {
+        if (s.taskId) {
+          return !taskIdsToExclude.includes(s.taskId);
+        }
+        return true;
+      });
+    }),
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.isDisableAutoFocus()) {
+      this._focusInput();
+    }
+
+    const inputElement = (this.inputEl() as ElementRef).nativeElement;
+    inputElement.addEventListener('keydown', (ev: KeyboardEvent) => {
+      if (ev.key === 'Escape') {
+        if (this.searchControl.value) {
+          this.searchControl.setValue('');
+        } else {
+          this.switchToAddMode.emit();
+        }
+      }
+    });
+  }
+
+  onOptionActivated(suggestion: AddTaskSuggestion | null): void {
+    this.activatedSuggestion$.next(suggestion);
+  }
+
+  async onOptionSelected(suggestion: AddTaskSuggestion): Promise<void> {
+    if (!suggestion) return;
+
+    let taskId: string | undefined;
+
+    if (suggestion.taskId && suggestion.isFromOtherContextAndTagOnlySearch) {
+      if (this._workContextService.activeWorkContextType === WorkContextType.TAG) {
+        const task = await this._taskService.getByIdOnce$(suggestion.taskId).toPromise();
+        this._taskService.moveToCurrentWorkContext(task);
+      }
+      this._snackService.open({
+        ico: 'playlist_add',
+        msg: T.F.TASK.S.FOUND_MOVE_FROM_OTHER_LIST,
+        translateParams: {
+          title: truncate(suggestion.title),
+          contextTitle: suggestion.ctx?.title
+            ? truncate(suggestion.ctx.title)
+            : '~the void~',
+        },
+      });
+      taskId = suggestion.taskId;
+    } else if (suggestion.taskId) {
+      if (suggestion.projectId) {
+        this._taskService.getByIdOnce$(suggestion.taskId).subscribe((task) => {
+          this._taskService.moveToCurrentWorkContext(task);
+        });
+        this._snackService.open({
+          ico: 'arrow_upward',
+          msg: T.F.TASK.S.FOUND_MOVE_FROM_BACKLOG,
+          translateParams: { title: suggestion.title },
+        });
+        taskId = suggestion.taskId;
+      }
+    } else if (suggestion.issueType && suggestion.issueData) {
+      taskId = await this._addTaskBarService.addTaskFromExistingTaskOrIssue(
+        suggestion,
+        this.isAddToBacklog(),
+        true,
+      );
+    }
+
+    if (taskId) {
+      this.afterTaskAdd.emit({
+        taskId,
+        isAddToBottom: false,
+      });
+      this.searchControl.setValue('');
+      this.switchToAddMode.emit();
+    }
+  }
+
+  toggleSearchType(): void {
+    this.searchType.set(this.searchType() === 'tasks' ? 'issues' : 'tasks');
+    this.searchControl.setValue('');
+    this._focusInput();
+  }
+
+  private _focusInput(): void {
+    if (IS_ANDROID_WEB_VIEW) {
+      document.body.focus();
+      (this.inputEl() as ElementRef).nativeElement.focus();
+      setTimeout(() => {
+        document.body.focus();
+        (this.inputEl() as ElementRef).nativeElement.focus();
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        (this.inputEl() as ElementRef).nativeElement.focus();
+      });
+    }
+  }
+}
--- src/app/features/tasks/add-task-bar/add-task-bar.component.ts
+++ src/app/features/tasks/add-task-bar/add-task-bar.component.ts
@@ -1,377 +1,64 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  inject,
-  input,
-  OnDestroy,
-  output,
-  signal,
-  viewChild,
-} from '@angular/core';
-import { FormsModule, ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
-import { TaskService } from '../task.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { T } from '../../../t.const';
-import { AddTaskSuggestion } from './add-task-suggestions.model';
-import { WorkContextService } from '../../work-context/work-context.service';
-import { ShortSyntaxTag } from './short-syntax-to-tags';
-import { slideAnimation } from '../../../ui/animations/slide.ani';
+import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import { LS } from '../../../core/persistence/storage-keys.const';
 import { blendInOutAnimation } from 'src/app/ui/animations/blend-in-out.ani';
 import { fadeAnimation } from '../../../ui/animations/fade.ani';
-import { LS, SS } from '../../../core/persistence/storage-keys.const';
-import { IS_ANDROID_WEB_VIEW } from '../../../util/is-android-web-view';
-import { Store } from '@ngrx/store';
-import { PlannerActions } from '../../planner/store/planner.actions';
-import { getDbDateStr } from '../../../util/get-db-date-str';
-import { MentionConfig } from 'angular-mentions/lib/mention-config';
-import { AddTaskBarService } from './add-task-bar.service';
-import { map } from 'rxjs/operators';
-import { selectEnabledIssueProviders } from '../../issue/store/issue-provider.selectors';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import {
-  MatAutocomplete,
-  MatAutocompleteOrigin,
-  MatAutocompleteTrigger,
-} from '@angular/material/autocomplete';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { MatInput } from '@angular/material/input';
-import { MentionModule } from 'angular-mentions';
-import { MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { MatTooltip } from '@angular/material/tooltip';
-import { MatOption } from '@angular/material/core';
-import { AsyncPipe } from '@angular/common';
-import { TranslatePipe } from '@ngx-translate/core';
-import { IssueIconPipe } from '../../issue/issue-icon/issue-icon.pipe';
-import { TagComponent } from '../../tag/tag/tag.component';
 import { TaskCopy } from '../task.model';
-import { TaskLog } from '../../../core/log';
+import { AddTaskBarAddModeComponent } from './add-task-bar-add-mode/add-task-bar-add-mode.component';
+import { AddTaskBarSearchModeComponent } from './add-task-bar-search-mode/add-task-bar-search-mode.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'add-task-bar',
   templateUrl: './add-task-bar.component.html',
   styleUrls: ['./add-task-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [blendInOutAnimation, slideAnimation, fadeAnimation],
-  imports: [
-    FormsModule,
-    MatAutocompleteOrigin,
-    MatProgressSpinner,
-    MatInput,
-    MatAutocompleteTrigger,
-    ReactiveFormsModule,
-    MentionModule,
-    MatIconButton,
-    MatIcon,
-    MatTooltip,
-    MatAutocomplete,
-    MatOption,
-    AsyncPipe,
-    TranslatePipe,
-    IssueIconPipe,
-    TagComponent,
-  ],
+  animations: [blendInOutAnimation, fadeAnimation],
+  imports: [AddTaskBarAddModeComponent, AddTaskBarSearchModeComponent, AsyncPipe],
 })
-export class AddTaskBarComponent implements AfterViewInit, OnDestroy {
-  private _taskService = inject(TaskService);
-  private _workContextService = inject(WorkContextService);
-  private _store = inject(Store);
-  private _addTaskBarService = inject(AddTaskBarService);
-
+export class AddTaskBarComponent {
   tabindex = input<number>(0);
-  isDoubleEnterMode = input<boolean>(false);
   isElevated = input<boolean>(false);
-  isHideTagTitles = input<boolean>(false);
   isDisableAutoFocus = input<boolean>(false);
   planForDay = input<string | undefined>(undefined);
-  tagsToRemove = input<string[]>();
   additionalFields = input<Partial<TaskCopy>>();
-  isSkipAddingCurrentTag = input<boolean>(false);
   taskIdsToExclude = input<string[]>();
 
   afterTaskAdd = output<{ taskId: string; isAddToBottom: boolean }>();
   blurred = output<void>();
   done = output<void>();
 
-  isLoading = signal(false);
-  doubleEnterCount = signal(0);
   isAddToBottom = signal(
     JSON.parse(localStorage.getItem(LS.IS_ADD_TO_BOTTOM) || 'false'),
   );
   isAddToBacklog = signal(false);
-  isSearchIssueProviders = signal(false);
-  isSearchIssueProviders$ = toObservable(this.isSearchIssueProviders);
 
-  inputEl = viewChild<ElementRef>('inputEl');
+  mode = signal<'add' | 'search'>('add');
 
-  T: typeof T = T;
+  switchToSearchMode(): void {
+    this.mode.set('search');
+  }
 
-  taskSuggestionsCtrl: UntypedFormControl = new UntypedFormControl();
+  switchToAddMode(): void {
+    this.mode.set('add');
+  }
 
-  filteredIssueSuggestions$: Observable<AddTaskSuggestion[]> = this._addTaskBarService
-    .getFilteredIssueSuggestions$(
-      this.taskSuggestionsCtrl,
-      this.isSearchIssueProviders$,
-      this.isLoading,
-    )
-    .pipe(
-      map((suggestions) => {
-        const taskIdsToExclude = this.taskIdsToExclude() || [];
-        return suggestions.filter((s) => {
-          if (s.taskId) {
-            return !taskIdsToExclude.includes(s.taskId);
-          }
-          return true;
-        });
-      }),
-    );
+  onAfterTaskAdd(event: { taskId: string; isAddToBottom: boolean }): void {
+    this.afterTaskAdd.emit(event);
+    if (this.mode() === 'search') {
+      this.mode.set('add');
+    }
+  }
 
-  activatedIssueTask$: BehaviorSubject<AddTaskSuggestion | null> =
-    new BehaviorSubject<AddTaskSuggestion | null>(null);
-
-  shortSyntaxTags$: Observable<ShortSyntaxTag[]> =
-    this._addTaskBarService.getShortSyntaxTags$(this.taskSuggestionsCtrl);
-
-  inputVal$: Observable<string> = this.taskSuggestionsCtrl.valueChanges;
-
-  mentionConfig$: Observable<MentionConfig> = this._addTaskBarService.getMentionConfig$();
-
-  isAddToBacklogAvailable = toSignal(
-    this._workContextService.activeWorkContext$.pipe(map((ctx) => !!ctx.isEnableBacklog)),
-    { initialValue: false },
-  );
-
-  isSearchIssueProvidersAvailable = toSignal(
-    this._store
-      .select(selectEnabledIssueProviders)
-      .pipe(map((issueProviders) => issueProviders.length > 0)),
-    { initialValue: false },
-  );
-
-  private _isAddInProgress?: boolean;
-  private _lastAddedTaskId?: string;
-
-  private _delayBlurTimeout?: number;
-  private _autofocusTimeout?: number;
-  private _attachKeyDownHandlerTimeout?: number;
-  private _saveTmpTodoTimeout?: number;
+  onBlurred(): void {
+    this.blurred.emit();
+  }
 
   toggleIsAddToBottom(): void {
     this.isAddToBottom.set(!this.isAddToBottom());
     localStorage.setItem(LS.IS_ADD_TO_BOTTOM, JSON.stringify(this.isAddToBottom()));
   }
 
-  ngAfterViewInit(): void {
-    this.isAddToBottom.set(!!this.planForDay() || this.isAddToBottom());
-    if (!this.isDisableAutoFocus()) {
-      this._focusInput();
-    }
-
-    this._attachKeyDownHandlerTimeout = window.setTimeout(() => {
-      (this.inputEl() as ElementRef).nativeElement.addEventListener(
-        'keydown',
-        (ev: KeyboardEvent) => {
-          if (ev.key === 'Escape') {
-            this.blurred.emit();
-            this.activatedIssueTask$.next(null);
-          } else if (ev.key === '1' && ev.ctrlKey) {
-            this.toggleIsAddToBottom();
-            ev.preventDefault();
-          } else if (ev.key === '2' && ev.ctrlKey) {
-            this.isSearchIssueProviders.set(!this.isSearchIssueProviders());
-            ev.preventDefault();
-          } else if (ev.key === '3' && ev.ctrlKey) {
-            this.isAddToBacklog.set(!this.isAddToBacklog());
-            ev.preventDefault();
-          }
-        },
-      );
-    });
-
-    const savedTodo = sessionStorage.getItem(SS.TODO_TMP) || '';
-    if (savedTodo) {
-      sessionStorage.setItem(SS.TODO_TMP, '');
-      this.taskSuggestionsCtrl.setValue(savedTodo);
-      this._saveTmpTodoTimeout = window.setTimeout(() => {
-        (this.inputEl() as ElementRef).nativeElement.value = savedTodo;
-        (this.inputEl() as ElementRef).nativeElement.select();
-      });
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this._delayBlurTimeout) {
-      window.clearTimeout(this._delayBlurTimeout);
-    }
-    if (this._attachKeyDownHandlerTimeout) {
-      window.clearTimeout(this._attachKeyDownHandlerTimeout);
-    }
-    if (this._autofocusTimeout) {
-      window.clearTimeout(this._autofocusTimeout);
-    }
-    if (this._saveTmpTodoTimeout) {
-      window.clearTimeout(this._saveTmpTodoTimeout);
-    }
-
-    if (this._lastAddedTaskId) {
-      this._taskService.focusTaskIfPossible(this._lastAddedTaskId);
-    }
-  }
-
-  onOptionActivated(val: any): void {
-    this.activatedIssueTask$.next(val);
-  }
-
-  onBlur(ev: FocusEvent): void {
-    const relatedTarget: HTMLElement = ev.relatedTarget as HTMLElement;
-    let isUIelement = false;
-
-    // NOTE: related target is null for all elements that are not focusable (e.g. items without tabindex, non-buttons, non-inputs etc.)
-    if (relatedTarget) {
-      const { className } = relatedTarget;
-      isUIelement =
-        className.includes('switch-add-to-btn') ||
-        className.includes('switch-add-to-bot-btn') ||
-        className.includes('shepherd-enabled');
-    }
-
-    const inputEl = this.inputEl();
-    if (!relatedTarget || (relatedTarget && !isUIelement)) {
-      sessionStorage.setItem(SS.TODO_TMP, (inputEl as ElementRef).nativeElement.value);
-    }
-
-    if (relatedTarget && isUIelement) {
-      (inputEl as ElementRef).nativeElement.focus();
-    } else {
-      // we need to wait since otherwise addTask is not working
-      window.clearTimeout(this._delayBlurTimeout);
-      this._delayBlurTimeout = window.setTimeout(() => {
-        if (this._isAddInProgress) {
-          window.clearTimeout(this._delayBlurTimeout);
-          this._delayBlurTimeout = window.setTimeout(() => {
-            this.blurred.emit();
-          }, 300);
-        } else {
-          this.blurred.emit();
-        }
-      }, 220);
-    }
-  }
-
-  async addTask(ev?: Event): Promise<void> {
-    // Prevent form submission if event is provided and has preventDefault method
-    // This handles cases where event might not be a standard Event object (e.g., in Android WebView)
-    if (ev && typeof ev.preventDefault === 'function') {
-      ev.preventDefault();
-    }
-
-    this._isAddInProgress = true;
-    const item: AddTaskSuggestion | string = this.taskSuggestionsCtrl.value;
-
-    if (!item) {
-      return;
-    } else if (typeof item === 'string') {
-      const newTaskStr = item as string;
-      if (newTaskStr.length > 0) {
-        this.doubleEnterCount.set(0);
-        this._lastAddedTaskId = this._taskService.add(
-          newTaskStr,
-          this.isAddToBacklog(),
-          {
-            ...this.additionalFields(),
-          },
-          this.isAddToBottom(),
-        );
-      } else if (this.doubleEnterCount() > 0) {
-        this.blurred.emit();
-        this.done.emit();
-      } else if (this.isDoubleEnterMode()) {
-        this.doubleEnterCount.set(this.doubleEnterCount() + 1);
-      }
-    } else if (
-      (item.taskId && item.isFromOtherContextAndTagOnlySearch) ||
-      !!item.issueData
-    ) {
-      this._lastAddedTaskId =
-        await this._addTaskBarService.addTaskFromExistingTaskOrIssue(
-          item,
-          this.isAddToBacklog(),
-          !this.isSkipAddingCurrentTag(),
-        );
-
-      const additionalFields = this.additionalFields();
-      const tagsToRemove = this.tagsToRemove();
-
-      if (this._lastAddedTaskId && (additionalFields || tagsToRemove)) {
-        const { tagIds, ...otherAdditionalFields } = additionalFields || { tagIds: [] };
-
-        this._taskService.update(this._lastAddedTaskId, otherAdditionalFields);
-        if (tagIds || tagsToRemove) {
-          const task = await this._taskService
-            .getByIdOnce$(this._lastAddedTaskId)
-            .toPromise();
-          TaskLog.log(additionalFields, tagsToRemove, task);
-
-          this._taskService.updateTags(
-            task,
-            [...task.tagIds, ...(tagIds || [])].filter(
-              (tagId) => !tagsToRemove || !tagsToRemove.includes(tagId),
-            ),
-          );
-        }
-      }
-    }
-    if (this._lastAddedTaskId) {
-      this.afterTaskAdd.emit({
-        taskId: this._lastAddedTaskId,
-        isAddToBottom: this.isAddToBottom(),
-      });
-    }
-
-    const planForDay = this.planForDay();
-    if (planForDay) {
-      this.blurred.emit();
-      if (this._lastAddedTaskId) {
-        this._planTaskForDay(this._lastAddedTaskId, planForDay);
-      }
-    } else {
-      this._focusInput();
-    }
-
-    this.taskSuggestionsCtrl.setValue('');
-    this._isAddInProgress = false;
-    sessionStorage.setItem(SS.TODO_TMP, '');
-    this._isAddInProgress = false;
-  }
-
-  private _planTaskForDay(taskId: string, day: string): void {
-    this._taskService.getByIdOnce$(taskId).subscribe((task) => {
-      if (getDbDateStr() !== day) {
-        this._store.dispatch(
-          PlannerActions.planTaskForDay({
-            task: task,
-            day,
-            isAddToTop: !this.isAddToBottom(),
-          }),
-        );
-      }
-    });
-  }
-
-  private _focusInput(): void {
-    if (IS_ANDROID_WEB_VIEW) {
-      document.body.focus();
-      (this.inputEl() as ElementRef).nativeElement.focus();
-      this._autofocusTimeout = window.setTimeout(() => {
-        document.body.focus();
-        (this.inputEl() as ElementRef).nativeElement.focus();
-      }, 1000);
-    } else {
-      this._autofocusTimeout = window.setTimeout(() => {
-        (this.inputEl() as ElementRef).nativeElement.focus();
-      });
-    }
+  toggleIsAddToBacklog(): void {
+    this.isAddToBacklog.set(!this.isAddToBacklog());
   }
 }
