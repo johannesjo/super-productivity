b0ad0e1322692d4a9528ee474d1ba46c8cf445bc
Johannes Millan <johannes.millan@gmail.com>
2025-08-24 11:13:31 +0200

    feat(addTaskBar): don't add today tag when adding from boards

--- src/app/features/planner/add-task-inline/add-task-inline.component.ts
+++ src/app/features/planner/add-task-inline/add-task-inline.component.ts
@@ -20,8 +20,9 @@ export class AddTaskInlineComponent {
 
   readonly planForDay = input<string>();
   readonly additionalFields = input<Partial<TaskCopy>>();
-  readonly tagsToRemove = input<string[]>();
+  readonly tagsToRemove = input<string[]>([]);
   readonly taskIdsToExclude = input<string[]>();
+  readonly isNoDefaults = input<boolean>(false);
   readonly afterTaskAdd = output<{ taskId: string; isAddToBottom: boolean }>();
 
   isShowAddTask = false;
