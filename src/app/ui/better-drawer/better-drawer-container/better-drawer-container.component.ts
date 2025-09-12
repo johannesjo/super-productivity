cbead5c96f7e4c56045144b3568ffed15940d9e7
Johannes Millan <johannes.millan@gmail.com>
2025-09-12 14:27:54 +0200

    refactor(rightPanel): remove more stuff from better-drawer-container.component.html

--- src/app/ui/better-drawer/better-drawer-container/better-drawer-container.component.ts
+++ src/app/ui/better-drawer/better-drawer-container/better-drawer-container.component.ts
@@ -2,18 +2,12 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
-  effect,
-  ElementRef,
   inject,
   input,
-  OnDestroy,
   output,
-  signal,
-  viewChild,
 } from '@angular/core';
 import { fadeAnimation } from '../../animations/fade.ani';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
-import { MainContainerClass } from '../../../app.constants';
 import { LanguageService } from '../../../core/language/language.service';
 import { IS_TOUCH_PRIMARY } from '../../../util/is-mouse-primary';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -22,9 +16,6 @@ import { filter, map, switchMap, startWith } from 'rxjs/operators';
 import { of, timer } from 'rxjs';
 import { SwipeDirective } from '../../swipe-gesture/swipe.directive';
 import { CssString, StyleObject, StyleObjectToString } from '../../../util/styles';
-
-const SMALL_CONTAINER_WIDTH = 620;
-const VERY_SMALL_CONTAINER_WIDTH = 450;
 
 @Component({
   selector: 'better-drawer-container',
@@ -39,8 +30,7 @@ const VERY_SMALL_CONTAINER_WIDTH = 450;
   imports: [SwipeDirective],
   standalone: true,
 })
-export class BetterDrawerContainerComponent implements OnDestroy {
-  private _elementRef = inject(ElementRef);
+export class BetterDrawerContainerComponent {
   private _domSanitizer = inject(DomSanitizer);
   private _languageService = inject(LanguageService);
   private _router = inject(Router);
@@ -51,20 +41,7 @@ export class BetterDrawerContainerComponent implements OnDestroy {
   readonly isOpen = input<boolean>(false);
   readonly wasClosed = output<void>();
 
-  readonly contentElRef = viewChild<ElementRef>('contentElRef');
-
-  private _containerWidth = signal<number>(0);
-  private _resizeObserver: ResizeObserver | null = null;
-
   readonly isRTL = this._languageService.isLangRTL;
-
-  readonly isSmallMainContainer = computed(
-    () => this._containerWidth() < SMALL_CONTAINER_WIDTH,
-  );
-
-  readonly isVerySmallMainContainer = computed(
-    () => this._containerWidth() < VERY_SMALL_CONTAINER_WIDTH,
-  );
 
   readonly sideStyle = computed<SafeStyle>(() => {
     const styles =
@@ -92,39 +69,6 @@ export class BetterDrawerContainerComponent implements OnDestroy {
 
   // Computed signal that determines if animations should be skipped
   private readonly _shouldSkipAnimation = computed(() => this._skipAnimationDuringNav());
-
-  // Effect to set up resize observer when content element is available
-  private _contentElEffect = effect(() => {
-    const elRef = this.contentElRef();
-    if (elRef) {
-      this._setupResizeObserver(elRef.nativeElement);
-    }
-  });
-
-  // Effects to handle class toggling
-  private _containerClassEffect = effect(() => {
-    const containerEl = this._elementRef.nativeElement;
-    const isSmall = this.isSmallMainContainer();
-    const isVerySmall = this.isVerySmallMainContainer();
-
-    if (isSmall) {
-      containerEl.classList.add(MainContainerClass.isSmallMainContainer);
-    } else {
-      containerEl.classList.remove(MainContainerClass.isSmallMainContainer);
-    }
-
-    if (isVerySmall) {
-      containerEl.classList.add(MainContainerClass.isVerySmallMainContainer);
-    } else {
-      containerEl.classList.remove(MainContainerClass.isVerySmallMainContainer);
-    }
-  });
-
-  ngOnDestroy(): void {
-    if (this._resizeObserver) {
-      this._resizeObserver.disconnect();
-    }
-  }
 
   updateStyleAfterTransition(): void {
     // Handle visibility after transition ends
@@ -155,20 +99,6 @@ export class BetterDrawerContainerComponent implements OnDestroy {
     const styles: StyleObject = { width: isOpen ? widthVal : '0' };
 
     return StyleObjectToString(styles);
-  }
-
-  private _setupResizeObserver(element: HTMLElement): void {
-    if (this._resizeObserver) {
-      this._resizeObserver.disconnect();
-    }
-
-    this._resizeObserver = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        this._containerWidth.set(entry.contentRect.width);
-      }
-    });
-
-    this._resizeObserver.observe(element);
   }
 
   protected readonly IS_TOUCH_PRIMARY = IS_TOUCH_PRIMARY;
