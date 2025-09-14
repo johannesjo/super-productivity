0a5b71e067afd0e787c122ce9b36275001c8635d
Johannes Millan <johannes.millan@gmail.com>
2025-09-14 16:14:21 +0200

    feat(sideNav): make mobile nav ani work

--- src/app/core-ui/magic-side-nav/magic-side-nav.animations.ts
+++ src/app/core-ui/magic-side-nav/magic-side-nav.animations.ts
@@ -1,27 +1,21 @@
 import { animate, style, transition, trigger } from '@angular/animations';
+import { ANI_ENTER_TIMING, ANI_LEAVE_TIMING } from '../../ui/animations/animation.const';
 
 export const magicSideNavAnimations = [
   trigger('mobileNav', [
     transition(':enter', [
-      style({ transform: 'translateX(-100%)' }),
-      animate(
-        '225ms cubic-bezier(0.4, 0, 0.2, 1)',
-        style({ transform: 'translateX(0)' }),
-      ),
+      style({ transform: 'translateX(-100%)', opacity: 0 }),
+      animate(ANI_ENTER_TIMING, style({ transform: 'translateX(0)', opacity: 1 })),
     ]),
     transition(':leave', [
-      style({ transform: 'translateX(0)' }),
-      animate(
-        '225ms cubic-bezier(0.4, 0, 0.2, 1)',
-        style({ transform: 'translateX(-100%)' }),
-      ),
+      animate(ANI_LEAVE_TIMING, style({ transform: 'translateX(-100%)' })),
     ]),
   ]),
   trigger('mobileBackdrop', [
     transition(':enter', [
       style({ opacity: 0 }),
-      animate('225ms ease-in-out', style({ opacity: 1 })),
+      animate(ANI_ENTER_TIMING, style({ opacity: 1 })),
     ]),
-    transition(':leave', [animate('225ms ease-in-out', style({ opacity: 0 }))]),
+    transition(':leave', [animate(ANI_LEAVE_TIMING, style({ opacity: 0 }))]),
   ]),
 ];
