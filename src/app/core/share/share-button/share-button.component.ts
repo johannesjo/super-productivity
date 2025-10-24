c357531fbbd29a8a1763dd2ad8785a21e2fd8176
Johannes Millan <johannes.millan@gmail.com>
2025-10-24 17:26:17 +0200

    fix: prevent chart component download chart image on enter press

--- src/app/core/share/share-button/share-button.component.ts
+++ src/app/core/share/share-button/share-button.component.ts
@@ -14,6 +14,7 @@ import { SharePayload } from '../share.model';
   template: `
     <button
       mat-icon-button
+      type="button"
       [matTooltip]="tooltip()"
       (click)="share()"
       [disabled]="disabled()"
