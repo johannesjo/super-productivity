f69a7ac5f66e4c6a8d8b4d522cb4f1017a20f70a
Johannes Millan <johannes.millan@gmail.com>
2025-11-13 13:32:21 +0100

    fix: inconsistent state on dragging tasks onto project #5489

--- src/app/root-store/meta/task-shared-meta-reducers/project-shared.reducer.ts
+++ src/app/root-store/meta/task-shared-meta-reducers/project-shared.reducer.ts
@@ -29,8 +29,11 @@ const handleMoveToOtherProject = (
   task: TaskWithSubTasks,
   targetProjectId: string,
 ): RootState => {
-  const currentProjectId = task.projectId;
-  const allTaskIds = [task.id, ...task.subTaskIds];
+  const taskState = state[TASK_FEATURE_NAME];
+  const canonicalTask = taskState.entities[task.id] as Task | undefined;
+  const canonicalSubTaskIds = canonicalTask?.subTaskIds ?? task.subTaskIds;
+  const currentProjectId = canonicalTask?.projectId ?? task.projectId;
+  const allTaskIds = unique([task.id, ...canonicalSubTaskIds]);
 
   let updatedState = state;
 
