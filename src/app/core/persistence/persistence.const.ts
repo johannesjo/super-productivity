de40826573dcd6c8841bf534762218257318ae15
Johannes Millan <johannes.millan@gmail.com>
2025-12-05 15:24:11 +0100

    refactor(persistence): remove legacy model configs and unused interfaces
    Remove unused legacy persistence code that was superseded by the
    Operation Log system:
    
    - Remove BASE_MODEL_CFGS with unused modelVersion/migrateFn
    - Remove ENTITY_MODEL_CFGS with unused reducerFn/modelVersion/migrateFn
    - Remove unused interfaces: PersistenceLegacyBaseModel,
      PersistenceBaseEntityModel, EntityModelHelpers, PersistenceBaseModelCfg,
      PersistenceEntityModelCfg
    - Simplify ALL_ENTITY_MODEL_KEYS to a direct array (still used by data-repair)

--- src/app/core/persistence/persistence.const.ts
+++ src/app/core/persistence/persistence.const.ts
@@ -1,142 +1,18 @@
-import { PersistenceBaseModelCfg, PersistenceEntityModelCfg } from './persistence.model';
-import { GlobalConfigState } from '../../features/config/global-config.model';
-import { Project, ProjectState } from '../../features/project/project.model';
-import { projectReducer } from '../../features/project/store/project.reducer';
-import { Tag, TagState } from '../../features/tag/tag.model';
-import { tagReducer } from '../../features/tag/store/tag.reducer';
-import {
-  SimpleCounter,
-  SimpleCounterState,
-} from '../../features/simple-counter/simple-counter.model';
-import { simpleCounterReducer } from '../../features/simple-counter/store/simple-counter.reducer';
-import { Note, NoteState } from '../../features/note/note.model';
-import { noteReducer } from '../../features/note/store/note.reducer';
-import { Metric, MetricState } from '../../features/metric/metric.model';
-import { metricReducer } from '../../features/metric/store/metric.reducer';
-import {
-  ArchiveTask,
-  Task,
-  TaskArchive,
-  TaskState,
-} from '../../features/tasks/task.model';
-import { taskReducer } from '../../features/tasks/store/task.reducer';
-import {
-  TaskRepeatCfg,
-  TaskRepeatCfgState,
-} from '../../features/task-repeat-cfg/task-repeat-cfg.model';
-import { taskRepeatCfgReducer } from '../../features/task-repeat-cfg/store/task-repeat-cfg.reducer';
 import { AppBaseData } from '../../imex/sync/sync.model';
-import { PlannerState } from '../../features/planner/store/planner.reducer';
-import { IssueProvider, IssueProviderState } from '../../features/issue/issue.model';
-import { issueProviderReducer } from '../../features/issue/store/issue-provider.reducer';
-import { BoardsState } from '../../features/boards/store/boards.reducer';
 
-interface PersistenceBaseModelCfgs {
-  // [key: string]: PersistenceBaseModelCfg<any>;
-  globalConfig: PersistenceBaseModelCfg<GlobalConfigState>;
-  planner: PersistenceBaseModelCfg<PlannerState>;
-  boards: PersistenceBaseModelCfg<BoardsState>;
-}
-
-interface PersistenceEntityModelCfgs {
-  project: PersistenceEntityModelCfg<ProjectState, Project>;
-  issueProvider: PersistenceEntityModelCfg<IssueProviderState, IssueProvider>;
-  tag: PersistenceEntityModelCfg<TagState, Tag>;
-  simpleCounter: PersistenceEntityModelCfg<SimpleCounterState, SimpleCounter>;
-  note: PersistenceEntityModelCfg<NoteState, Note>;
-
-  metric: PersistenceEntityModelCfg<MetricState, Metric>;
-
-  // MAIN TASK MODELS
-  task: PersistenceEntityModelCfg<TaskState, Task>;
-  taskArchive: PersistenceEntityModelCfg<TaskArchive, ArchiveTask>;
-  taskRepeatCfg: PersistenceEntityModelCfg<TaskRepeatCfgState, TaskRepeatCfg>;
-}
-
-export const BASE_MODEL_CFGS: PersistenceBaseModelCfgs = {
-  globalConfig: {
-    appDataKey: 'globalConfig',
-    // TODO remove this everywhere
-    modelVersion: 99,
-    migrateFn: (v) => v,
-  },
-  planner: {
-    appDataKey: 'planner',
-    modelVersion: 99,
-    // no migrations needed yet
-    migrateFn: (s: PlannerState): PlannerState => s,
-  },
-  boards: {
-    appDataKey: 'boards',
-    modelVersion: 99,
-    // no migrations needed yet
-    migrateFn: (s: BoardsState): BoardsState => s,
-  },
-};
-
-export const ENTITY_MODEL_CFGS: PersistenceEntityModelCfgs = {
-  project: {
-    appDataKey: 'project',
-    modelVersion: 99,
-    reducerFn: projectReducer as any,
-    migrateFn: (v) => v,
-  },
-  issueProvider: {
-    appDataKey: 'issueProvider',
-    modelVersion: 99,
-    reducerFn: issueProviderReducer,
-    migrateFn: (v) => v,
-  },
-  tag: {
-    appDataKey: 'tag',
-    modelVersion: 99,
-    reducerFn: tagReducer,
-    migrateFn: (v) => v,
-  },
-  simpleCounter: {
-    appDataKey: 'simpleCounter',
-    modelVersion: 99,
-    reducerFn: simpleCounterReducer,
-    migrateFn: (v) => v,
-  },
-  note: {
-    appDataKey: 'note',
-    modelVersion: 99,
-    reducerFn: noteReducer,
-    migrateFn: (v) => v,
-  },
-
-  metric: {
-    appDataKey: 'metric',
-    modelVersion: 99,
-    reducerFn: metricReducer as any,
-    migrateFn: (v) => v,
-  },
-
-  // MAIN TASK MODELS
-  task: {
-    appDataKey: 'task',
-    modelVersion: 99,
-    reducerFn: taskReducer,
-    migrateFn: (v) => v,
-  },
-  taskArchive: {
-    appDataKey: 'taskArchive',
-    modelVersion: 99,
-    reducerFn: taskReducer as any,
-    migrateFn: (v) => v,
-  },
-  taskRepeatCfg: {
-    appDataKey: 'taskRepeatCfg',
-    modelVersion: 99,
-    reducerFn: taskRepeatCfgReducer as any,
-    migrateFn: (v) => v,
-  },
-};
-
-// TODO remove later
-export const ALL_ENTITY_MODEL_KEYS: (keyof AppBaseData)[] = Object.entries(
-  ENTITY_MODEL_CFGS,
-)
-  .map(([, entry]) => entry.appDataKey)
-  .filter((key) => key !== 'taskArchive');
+/**
+ * List of entity model keys used for data repair.
+ * These correspond to the entity states that have ids/entities structure.
+ *
+ * Note: taskArchive is excluded as it's handled separately in the archive models.
+ */
+export const ALL_ENTITY_MODEL_KEYS: (keyof AppBaseData)[] = [
+  'project',
+  'issueProvider',
+  'tag',
+  'simpleCounter',
+  'note',
+  'metric',
+  'task',
+  'taskRepeatCfg',
+];
