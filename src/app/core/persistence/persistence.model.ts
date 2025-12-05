419706edff1dcfd7e89692ccadd295740ee48535
Johannes Millan <johannes.millan@gmail.com>
2025-12-05 15:24:11 +0100

    refactor(persistence): remove legacy model configs and unused interfaces
    Remove unused legacy persistence code that was superseded by the
    Operation Log system:
    
    - Remove BASE_MODEL_CFGS with unused modelVersion/migrateFn
    - Remove ENTITY_MODEL_CFGS with unused reducerFn/modelVersion/migrateFn
    - Remove unused interfaces: PersistenceLegacyBaseModel,
      PersistenceBaseEntityModel, EntityModelHelpers, PersistenceBaseModelCfg,
      PersistenceEntityModelCfg
    - Simplify ALL_ENTITY_MODEL_KEYS to a direct array (still used by data-repair)

--- src/app/core/persistence/persistence.model.ts
+++ src/app/core/persistence/persistence.model.ts
@@ -1,44 +1,10 @@
 import { AppBaseData } from '../../imex/sync/sync.model';
-import { Action } from '@ngrx/store';
-import { ActionReducer } from '@ngrx/store/src/models';
 
-export interface PersistenceLegacyBaseModel<T> {
+/**
+ * Configuration for entity models used in data repair.
+ * This is a simplified version - the full config with reducerFn/migrateFn
+ * was removed as part of the migration to the Operation Log system.
+ */
+export interface EntityModelKey {
   appDataKey: keyof AppBaseData;
-
-  loadState(isSkipMigration?: boolean): Promise<T>;
-
-  saveState(
-    state: T,
-    flags: { isDataImport?: boolean; isSyncModelChange?: boolean },
-  ): Promise<unknown>;
-}
-
-export interface PersistenceBaseEntityModel<S, M> extends PersistenceLegacyBaseModel<S> {
-  getById(id: string): Promise<M>;
-
-  // NOTE: side effects are not executed!!!
-  execAction(action: Action, isSyncModelChange?: boolean): Promise<S>;
-
-  // NOTE: side effects are not executed!!!
-  execActions(actions: Action[], isSyncModelChange?: boolean): Promise<S>;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export interface EntityModelHelpers<S, M> {
-  getById(projectId: string, id: string): Promise<M>;
-}
-
-export interface PersistenceBaseModelCfg<S> {
-  appDataKey: keyof AppBaseData;
-  modelVersion: number;
-  migrateFn: (state: S) => S;
-  isSkipPush?: boolean;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export interface PersistenceEntityModelCfg<S, M> {
-  appDataKey: keyof AppBaseData;
-  modelVersion: number;
-  reducerFn: ActionReducer<S, { type: string; payload?: any }>;
-  migrateFn: (state: S) => S;
 }
