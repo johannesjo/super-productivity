4a07ecfcdd65a85c93f3748cce7b0611a5f48fba
Johannes Millan <johannes.millan@gmail.com>
2025-12-10 15:29:03 +0100

    refactor: cleanup

--- e2e/pages/side-nav.page.ts
+++ e2e/pages/side-nav.page.ts
@@ -1,4 +1,4 @@
-import { expect, Locator, Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { BasePage } from './base.page';
 
 export class SideNavPage extends BasePage {
